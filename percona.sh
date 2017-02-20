8096b6a74a0b635cd706db0ae95a5021c73e7189
Chris Gray <christophermjgray@gmail.com>
2017-02-20 09:29:04 -0500

    Add files via upload

--- percona.sh
+++ percona.sh
@@ -1,10 +1,8 @@
 sudo apt-key adv --keyserver keys.gnupg.net --recv-keys 8507EFA5
 wait
-
-echo "deb http://repo.percona.com/apt trusty main" >> /etc/apt/sources.list
-echo "deb-src http://repo.percona.com/apt trusty main" >> /etc/apt/sources.list
+sudo echo "deb http://repo.percona.com/apt trusty main" >> /etc/apt/sources.list
+sudo echo "deb-src http://repo.percona.com/apt trusty main" >> /etc/apt/sources.list
 wait
-
 sudo apt-get update -y
 wait
 
@@ -18,7 +16,7 @@ wait
 echo "Downloading Config"
 wget "my.cnf" "https://raw.githubusercontent.com/c2theg/srvBuilds/master/my.cnf"
 wait
-cp "my.cnf" "/etc/mysql/my.cnf"
+sudo cp "my.cnf" "/etc/mysql/my.cnf"
 wait
 echo "Percona Config Download Complete"
 sudo /etc/init.d/mysql restart
