127ad3e95b1a96f2832851e26c6d10b06d03acb0
Chris Gray <christophermjgray@gmail.com>
2017-02-20 01:32:02 -0500

    initial
    initial upload of files

---
+++ nginxphp.sh
@@ -0,0 +1,14 @@
+sudo apt-get -y update 
+wait
+sudo apt-get -y upgrade
+wait
+sudo apt-get -y dist-upgrade
+wait
+sudo apt-get install -y ntp ntpdate ssh openssh-server libicu-dev python-software-properties autossh python python-pip python-dev screen python3-setuptools whois 
+wait 
+sudo apt-get install -y traceroute htop sysstat iptraf iftop slurm tcptrack bmon nethogs speedometer zmap ncdu
+
+#--------------------------------------------
+
+
+
---
+++ percona.sh
@@ -0,0 +1,3 @@
+sudo apt-key adv --keyserver keys.gnupg.net --recv-keys 8507EFA5
+wait
+
