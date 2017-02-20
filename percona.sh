3caa9b55d3bbe994059a00c1a3b949411d823a2e
Chris Gray <christophermjgray@gmail.com>
2017-02-20 08:16:30 -0500

    update

--- percona.sh
+++ percona.sh
@@ -1,3 +1,19 @@
 sudo apt-key adv --keyserver keys.gnupg.net --recv-keys 8507EFA5
 wait
 
+#Add To
+#nano /etc/apt/sources.list
+#deb http://repo.percona.com/apt trusty main
+#deb-src http://repo.percona.com/apt trusty main
+echo "deb http://repo.percona.com/apt trusty main" >> /etc/apt/sources.list
+echo "deb-src http://repo.percona.com/apt trusty main" >> /etc/apt/sources.list
+wait
+
+sudo apt-get update -y
+wait
+
+sudo apt-get install -y percona-server-server-5.6 percona-server-client-5.6
+wait
+
+#sudo apt-get install -y php5-mysqlnd 
+sudo apt-get install -y php7.0-mysql
