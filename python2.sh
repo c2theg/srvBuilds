a66910e6248c6c27fe08afab5977de0ea2e42d71
Chris Gray <christophermjgray@gmail.com>
2017-02-21 17:58:57 -0500

    init

---
+++ python2.sh
@@ -0,0 +1,18 @@
+# -- add newer python 2.7.x repo --
+sudo -E add-apt-repository -y ppa:fkrull/deadsnakes-python2.7
+wait
+sudo -E apt-get update
+wait
+sudo -E apt-get -y upgrade
+wait
+#-- Upgrade to latest Kernal --
+sudo -E apt-get -y dist-upgrade
+wait
+sudo -E apt-get install -y ssh openssh-server openssl libssl-dev libssl1.0.0 whois traceroute htop
+wait
+sudo -E apt-get install -y python-software-properties python python-pip python-dev python2.7
+wait
+#---- install python dependancies ----
+#-- suds --
+#sudo pip install --upgrade pip
+sudo pip install --upgrade virtualenv
