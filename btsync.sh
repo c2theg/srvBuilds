1b41bcaeff546d2bb9d31a7ad797189f87cd88e2
Chris Gray <christophermjgray@gmail.com>
2017-03-14 12:59:24 -0400

    update1
    update

--- btsync.sh
+++ btsync.sh
@@ -1,3 +1,23 @@
+clear
+echo " "
+echo " Creating directories... "
+echo " "
+echo " "
+
+mkdir /media/data
+mkdir /media/data/btsync
+sudo chmod -R 755 /media/data/btsync/ && sudo chown -R www-data:www-data /media/data/btsync/
+
+echo "Made directory: /media/data/btsync  and gave rights to user/group: www-data:www-data"
+echo " set your files to this directory during the btsync wizard setup "
+
+echo " "
+echo " "
+echo " "
+echo " "
+
+
+
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D294A752
 wait
 sudo add-apt-repository -y ppa:tuxpoldo/btsync
@@ -5,3 +25,5 @@ wait
 sudo apt-get -y update
 wait
 sudo apt-get install -y btsync
+
+
