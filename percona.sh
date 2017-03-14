2ecf8c98b8501073631ba3b1854d07c8a5256c29
Chris Gray <christophermjgray@gmail.com>
2017-03-13 23:30:10 -0400

    update1
    updates

--- percona.sh
+++ percona.sh
@@ -1,3 +1,7 @@
+clear
+echo "Install Percona 5.6"
+echo " "
+echo " "
 sudo apt-key adv --keyserver keys.gnupg.net --recv-keys 8507EFA5
 wait
 sudo echo "deb http://repo.percona.com/apt trusty main" >> /etc/apt/sources.list
@@ -8,11 +12,11 @@ wait
 
 sudo apt-get install -y percona-server-server-5.6 percona-server-client-5.6
 wait
-
+echo " "
 #sudo apt-get install -y php5-mysqlnd 
 sudo apt-get install -y php7.0-mysql
 wait
-
+echo " "
 echo "Downloading Config"
 wget "my.cnf" "https://raw.githubusercontent.com/c2theg/srvBuilds/master/my.cnf"
 wait
@@ -20,4 +24,9 @@ sudo cp "my.cnf" "/etc/mysql/my.cnf"
 wait
 echo "Percona Config Download Complete"
 sudo /etc/init.d/mysql restart
+echo " "
+echo " "
+echo " "
+echo " "
+echo " Done! "
 
