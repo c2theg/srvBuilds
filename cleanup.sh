193c4dbf565828bfd4d97ab74301aff21e0c78f8
Chris Gray <christophermjgray@gmail.com>
2017-03-13 23:08:01 -0400

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,6 @@
+echo "\r\n \r\n --- Running System cleanup... \r\n "
+sudo df -h
+echo "\r\n"
 sudo apt-get remove --purge $(dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d')
 wait
 sudo apt-get -f install
@@ -15,9 +18,19 @@ wait
 sudo update-grub2
 wait
 sudo apt-get upgrade && sudo apt-get upgrade
+echo "\r\n"
+echo "\r\n"
+echo "\r\n"
 echo "\r\n -------------- Done Cleaning system -------- \r\n"
+echo "\r\n"
+echo "\r\n"
+echo "\r\n"
 echo "But Just incase you still dont have space... \r\n"
+echo "\r\n"
 sudo uname -r
 sudo dpkg --list | grep linux-image
+echo "\r\n"
+sudo df -h
+echo "\r\n"
 echo "Then issue the following: sudo apt-get purge linux-image-x.x.x.x-generic"
 echo "\r\n \r\n"
