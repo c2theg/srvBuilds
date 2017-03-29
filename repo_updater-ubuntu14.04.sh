f65043e1535fe10753752a885b60f995f07c263a
Chris Gray <christophermjgray@gmail.com>
2017-03-29 16:25:58 -0400

    Update repo_updater-ubuntu14.04.sh

--- repo_updater-ubuntu14.04.sh
+++ repo_updater-ubuntu14.04.sh
@@ -5,18 +5,18 @@ sudo apt-get -y install unattended-upgrades
 wait
 sudo apt-get -y autoremove
 wait
-
 apt-get dist-upgrade -y
 wait
-
 apt-get update && apt-get -y upgrade
-
 wait
-
 echo "-----------------------------------------------------------------------"
 echo " "
 echo " "
 echo "Downloading latest custom config's "
-wait
 wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/50unattended-upgrades
-
+wait
+cp 50unattended-upgrades /etc/apt/apt.conf.d/50unattended-upgrades
+echo "Done setting up AutoUpdates!"
+echo " "
+echo " "
+echo " "
