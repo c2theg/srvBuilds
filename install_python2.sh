efe004df5f8cd47eb07d06cfc68c8560eba6850d
Chris Gray <christophermjgray@gmail.com>
2017-05-05 23:22:24 -0400

    Update install_python2.sh

--- install_python2.sh
+++ install_python2.sh
@@ -1,3 +1,12 @@
+#!/bin/sh
+#    If you update this from Windows, using Notepad ++, do the following:
+#       apt-get install dos2unix
+#       dos2unix install_redis.sh
+#       chmod u+x install_redis.sh
+#
+clear
+
+echo "Installing Python 2.7 latest.... "
 # -- add newer python 2.7.x repo --
 sudo -E add-apt-repository -y ppa:fkrull/deadsnakes-python2.7
 wait
@@ -7,12 +16,12 @@ sudo -E apt-get -y upgrade
 wait
 #-- Upgrade to latest Kernal --
 sudo -E apt-get -y dist-upgrade
-#wait
-#sudo -E apt-get install -y ssh openssh-server openssl libssl-dev libssl1.0.0 whois traceroute htop
 wait
-sudo -E apt-get install -y python-software-properties python python-pip python-dev python2.7
+sudo -E apt-get install -y python-software-properties python python-pip python-dev python2.7 python2-virtualenv
 wait
 #---- install python dependancies ----
 #-- suds --
 #sudo pip install --upgrade pip
 sudo pip install --upgrade virtualenv
+
+echo "DONE installing Python 2.7 - latest \r\n \r\n "
