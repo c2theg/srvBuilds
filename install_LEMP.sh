4feccc903093e41c9f4b8ac6d615a25959c58b05
Chris Gray <christophermjgray@gmail.com>
2017-05-07 20:29:52 -0400

    Update install_LEMP.sh

--- install_LEMP.sh
+++ install_LEMP.sh
@@ -1,7 +1,33 @@
-sudo apt-get -y update 
+#!/bin/sh
+#    If you update this from Windows, using Notepad ++, do the following:
+#       sudo apt-get -y install dos2unix
+#       dos2unix <FILE>
+#       chmod u+x <FILE>
+#
+clear
+echo "
+ _____             _         _    _          _                                   
+|     |___ ___ ___| |_ ___ _| |  | |_ _ _   |_|                                  
+|   --|  _| -_| .'|  _| -_| . |  | . | | |   _                                   
+|_____|_| |___|__,|_| |___|___|  |___|_  |  |_|                                  
+                                     |___|                                       
+                                                                                 
+ _____ _       _     _           _              _____    __    _____             
+|     | |_ ___|_|___| |_ ___ ___| |_ ___ ___   |     |__|  |  |   __|___ ___ _ _ 
+|   --|   |  _| |_ -|  _| . | . |   | -_|  _|  | | | |  |  |  |  |  |  _| .'| | |
+|_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
+                            |_|                                             |___|
+
+\r\n \r\n
+Version:  2.4                             \r\n
+Last Updated:  5/7/2017
+\r\n \r\n
+Updating system first..."
+apt-get update && apt-get upgrade -y
 wait
-sudo apt-get -y upgrade
-wait
+
+echo "Downloading required dependencies...\r\n\r\n"
+#--------------------------------------------------------------------------------------------
 sudo apt-get -y dist-upgrade
 wait
 sudo apt-get install -y ntp ntpdate ssh openssh-server screen whois traceroute htop sysstat iptraf iftop speedometer ncdu
