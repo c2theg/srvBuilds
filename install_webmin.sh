58554eb2cc1d4983ec87e981d50ba4f13601ff7d
Chris Gray <christophermjgray@gmail.com>
2017-05-07 20:52:04 -0400

    Update install_webmin.sh

--- install_webmin.sh
+++ install_webmin.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 #    If you update this from Windows, using Notepad ++, do the following:
-#       apt-get install dos2unix
-#       dos2unix install_redis.sh
-#       chmod u+x install_redis.sh
+#       sudo apt-get -y install dos2unix
+#       dos2unix <FILE>
+#       chmod u+x <FILE>
 #
 clear
-echo "                                                                                 
+echo "
  _____             _         _    _          _                                   
 |     |___ ___ ___| |_ ___ _| |  | |_ _ _   |_|                                  
 |   --|  _| -_| .'|  _| -_| . |  | . | | |   _                                   
@@ -17,23 +17,21 @@ echo "
 |   --|   |  _| |_ -|  _| . | . |   | -_|  _|  | | | |  |  |  |  |  |  _| .'| | |
 |_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
                             |_|                                             |___|
-"
-echo "\r\n \r\n"
 
-echo "Updating System first.... \r\n "
-sudo apt-get -y update 
+\r\n \r\n
+Version:  1.0                             \r\n
+Last Updated:  5/7/2017
+\r\n \r\n
+Updating system first..."
+sudo -E apt-get update
 wait
-sudo apt-get -y upgrade
+sudo -E apt-get upgrade -y
 wait
-
-echo "Installing dependencies... \r\n "
+echo "Downloading required dependencies...\r\n\r\n"
+#--------------------------------------------------------------------------------------------
 sudo apt-get -y install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python
 wait
-
 sudo wget http://prdownloads.sourceforge.net/webadmin/webmin_1.831_all.deb 
 wait
-
 sudo dpkg --install webmin_1.831_all.deb
-
-
 echo "Done! \r\n \r\n"
