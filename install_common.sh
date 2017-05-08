c4ce02221d001c028b7bd241166debf65a167e7e
Chris Gray <christophermjgray@gmail.com>
2017-05-07 20:34:35 -0400

    Update install_common.sh

--- install_common.sh
+++ install_common.sh
@@ -1,15 +1,33 @@
 #!/bin/sh
 #    If you update this from Windows, using Notepad ++, do the following:
-#       apt-get install dos2unix
-#       dos2unix install_redis.sh
-#       chmod u+x install_redis.sh
+#       sudo apt-get -y install dos2unix
+#       dos2unix <FILE>
+#       chmod u+x <FILE>
 #
 clear
+echo "
+ _____             _         _    _          _                                   
+|     |___ ___ ___| |_ ___ _| |  | |_ _ _   |_|                                  
+|   --|  _| -_| .'|  _| -_| . |  | . | | |   _                                   
+|_____|_| |___|__,|_| |___|___|  |___|_  |  |_|                                  
+                                     |___|                                       
+                                                                                 
+ _____ _       _     _           _              _____    __    _____             
+|     | |_ ___|_|___| |_ ___ ___| |_ ___ ___   |     |__|  |  |   __|___ ___ _ _ 
+|   --|   |  _| |_ -|  _| . | . |   | -_|  _|  | | | |  |  |  |  |  |  _| .'| | |
+|_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
+                            |_|                                             |___|
 
-sudo apt-get -y update 
+\r\n \r\n
+Version:  1.0                             \r\n
+Last Updated:  5/7/2017
+\r\n \r\n
+Updating system first..."
+apt-get update && apt-get upgrade -y
 wait
-sudo apt-get -y upgrade
-wait
+
+echo "Downloading required dependencies...\r\n\r\n"
+#--------------------------------------------------------------------------------------------
 
 sudo -E apt-get install -y ssh openssh-server openssl libssl-dev libssl1.0.0 whois traceroute htop
 wait
