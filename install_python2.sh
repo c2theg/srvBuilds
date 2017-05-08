ed1058625b89f8bc6a4438a2ba0edd623eb7312e
Chris Gray <christophermjgray@gmail.com>
2017-05-07 20:45:16 -0400

    Update install_python2.sh

--- install_python2.sh
+++ install_python2.sh
@@ -1,27 +1,43 @@
 #!/bin/sh
 #    If you update this from Windows, using Notepad ++, do the following:
-#       apt-get install dos2unix
-#       dos2unix install_redis.sh
-#       chmod u+x install_redis.sh
+#       sudo apt-get -y install dos2unix
+#       dos2unix <FILE>
+#       chmod u+x <FILE>
 #
 clear
+echo "
+ _____             _         _    _          _                                   
+|     |___ ___ ___| |_ ___ _| |  | |_ _ _   |_|                                  
+|   --|  _| -_| .'|  _| -_| . |  | . | | |   _                                   
+|_____|_| |___|__,|_| |___|___|  |___|_  |  |_|                                  
+                                     |___|                                       
+                                                                                 
+ _____ _       _     _           _              _____    __    _____             
+|     | |_ ___|_|___| |_ ___ ___| |_ ___ ___   |     |__|  |  |   __|___ ___ _ _ 
+|   --|   |  _| |_ -|  _| . | . |   | -_|  _|  | | | |  |  |  |  |  |  _| .'| | |
+|_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
+                            |_|                                             |___|
 
+\r\n \r\n
+Version:  1.0                             \r\n
+Last Updated:  5/7/2017
+\r\n \r\n
+Updating system first..."
+sudo -E apt-get update
+wait
+sudo -E apt-get upgrade -y
+wait
+echo "Downloading required dependencies...\r\n\r\n"
+#--------------------------------------------------------------------------------------------
 echo "Installing Python 2.7 latest.... "
 # -- add newer python 2.7.x repo --
 sudo -E add-apt-repository -y ppa:fkrull/deadsnakes-python2.7
 wait
 sudo -E apt-get update
 wait
-sudo -E apt-get -y upgrade
-wait
-#-- Upgrade to latest Kernal --
-sudo -E apt-get -y dist-upgrade
-wait
 sudo -E apt-get install -y python-software-properties python python-pip python-dev python2.7 python2-virtualenv
 wait
 #---- install python dependancies ----
-#-- suds --
 #sudo pip install --upgrade pip
 sudo pip install --upgrade virtualenv
-
 echo "DONE installing Python 2.7 - latest \r\n \r\n "
