e29fd62b323717d7e8cda2cf37083244839128b7
Chris Gray <christophermjgray@gmail.com>
2017-05-07 20:16:31 -0400

    Update update_ubuntu14.04.sh

--- update_ubuntu14.04.sh
+++ update_ubuntu14.04.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
+#    If you update this from Windows, using Notepad ++, do the following:
+#       sudo apt-get -y install dos2unix
+#       dos2unix <FILE>
+#       chmod u+x <FILE>
+#
 clear
+echo "                                                                                 
+ _____             _         _    _          _                                   
+|     |___ ___ ___| |_ ___ _| |  | |_ _ _   |_|                                  
+|   --|  _| -_| .'|  _| -_| . |  | . | | |   _                                   
+|_____|_| |___|__,|_| |___|___|  |___|_  |  |_|                                  
+                                     |___|                                       
+                                                                                 
+ _____ _       _     _           _              _____    __    _____             
+|     | |_ ___|_|___| |_ ___ ___| |_ ___ ___   |     |__|  |  |   __|___ ___ _ _ 
+|   --|   |  _| |_ -|  _| . | . |   | -_|  _|  | | | |  |  |  |  |  |  _| .'| | |
+|_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
+                            |_|                                             |___|
+"
+echo "\r\n \r\n"
 echo "Downloading required files.."
 sudo apt-get -y install unattended-upgrades
 wait
