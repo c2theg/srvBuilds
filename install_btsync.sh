745df2e75b34286bf7384e7054259189b74d3cea
Chris Gray <christophermjgray@gmail.com>
2017-05-07 20:30:56 -0400

    Update install_btsync.sh

--- install_btsync.sh
+++ install_btsync.sh
@@ -1,6 +1,34 @@
+#!/bin/sh
+#    If you update this from Windows, using Notepad ++, do the following:
+#       sudo apt-get -y install dos2unix
+#       dos2unix <FILE>
+#       chmod u+x <FILE>
+#
 clear
-echo " "
-echo " Creating directories... "
+echo "
+ _____             _         _    _          _                                   
+|     |___ ___ ___| |_ ___ _| |  | |_ _ _   |_|                                  
+|   --|  _| -_| .'|  _| -_| . |  | . | | |   _                                   
+|_____|_| |___|__,|_| |___|___|  |___|_  |  |_|                                  
+                                     |___|                                       
+                                                                                 
+ _____ _       _     _           _              _____    __    _____             
+|     | |_ ___|_|___| |_ ___ ___| |_ ___ ___   |     |__|  |  |   __|___ ___ _ _ 
+|   --|   |  _| |_ -|  _| . | . |   | -_|  _|  | | | |  |  |  |  |  |  _| .'| | |
+|_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
+                            |_|                                             |___|
+
+\r\n \r\n
+Version:  1.4                             \r\n
+Last Updated:  5/7/2017
+\r\n \r\n
+Updating system first..."
+apt-get update && apt-get upgrade -y
+wait
+
+#echo "Downloading required dependencies...\r\n\r\n"
+#--------------------------------------------------------------------------------------------
+echo " Creating directories... \r\n"
 echo " "
 echo " "
 
@@ -13,11 +41,6 @@ echo " set your files to this directory during the btsync wizard setup "
 
 echo " "
 echo " "
-echo " "
-echo " "
-
-
-
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D294A752
 wait
 sudo add-apt-repository -y ppa:tuxpoldo/btsync
