c229afe8c79f93127134d72e41e8e2b26e424182
Chris Gray <christophermjgray@gmail.com>
2017-05-09 21:20:33 -0400

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -23,55 +23,48 @@ Version:  1.0                             \r\n
 Last Updated:  5/7/2017
 \r\n \r\n
 Updating system first..."
-sudo -E apt-get update
-wait
-sudo -E apt-get upgrade -y
-wait
+#sudo -E apt-get update
+#wait
+#sudo -E apt-get upgrade -y
+#wait
 echo "Downloading required dependencies...\r\n\r\n"
 #--------------------------------------------------------------------------------------------
 echo "This installs docker to your ubuntu 14.04.5+ box..."
 # - from:  https://docs.docker.com/engine/installation/linux/ubuntu/#os-requirements
 
-echo "Removing any old versions... \r\n"
-sudo apt-get -y remove docker docker-engine
-wait
-
+#echo "Removing any old versions... \r\n"
+#sudo apt-get -y remove docker docker-engine
+#wait
 echo "DONE. Updating dependencies"
 sudo apt-get -y update && sudo apt-get -y install linux-image-extra-$(uname -r) linux-image-extra-virtual
 wait
 
 echo "\r\n \r\n"
-echo "Installing Docker... \r\n \r\n"
-sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
-wait
-
 echo "\r\n Downloading keys... "
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key -y add -
 wait
 sudo apt-key -y fingerprint 0EBFCD88
 wait
-
+apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+wait 
+apt-key -y adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 7EA0A9C3F273FCD8
+wait
 sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 wait
-
 echo "\r\n \r\n"
-echo "Installing Docker... "
-sudo apt-get update && sudo apt-get -y install docker-ce
+echo "Installing Docker... \r\n \r\n"
+sudo -E apt-get update
 wait
-
+sudo -E apt-get -y install apt-transport-https ca-certificates curl software-properties-common
+wait
+sudo -E apt-get -y install docker-ce
+wait
 apt-cache madison docker-ce
 wait
-
-sudo apt-get -y install docker-ce
-wait
-
 echo "Done!"
-
 echo "\r\n \r\n"
-
 echo "----------------------------  "
 # from: https://github.com/hobby-kube/guide
-
 echo "Installing Kubernetes....  \r\n"
 echo "Learning more about it here: https://kubernetes.io/docs/tutorials/kubernetes-basics/" 
 echo "\r\n \r\n"
@@ -79,9 +72,9 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial-unstable main
 EOF
-apt-get update
-apt-get install -y kubelet kubeadm kubectl kubernetes-cni
-
+sudo -E apt-get update
+sudo -E apt-get install -y kubelet kubeadm kubectl kubernetes-cni
+wait
 echo "\r\n \r\n"
 
 echo "Running sample container"
@@ -89,9 +82,11 @@ sudo docker run hello-world
 wait
 echo "\r\n \r\n -------------------------------------------------------------- \r\n \r\n"
 
-echo "Downloading a better way to manage containers... container! \r\n.."
+echo "Downloading a better way to manage containers... \r\n.."
 echo " PORTAINER! - https://github.com/portainer/portainer \r\n "
 
+echo "Running command: sudo docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer"
+echo "\r\n \r\n"
 sudo docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer
 echo "Visit http://127.0.0.1:9000/ in chrome / firefox"
 echo "\r\n \r\n Docker deployment complete!!! \r\n \r\n"
