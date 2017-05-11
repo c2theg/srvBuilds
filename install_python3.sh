4882f3fbd0352f096a9e6467557e93f2f544509c
Chris Gray <christophermjgray@gmail.com>
2017-05-11 19:13:49 -0400

    Update install_python3.sh

--- install_python3.sh
+++ install_python3.sh
@@ -1,10 +1,34 @@
 #!/bin/sh
 #    If you update this from Windows, using Notepad ++, do the following:
-#       apt-get install dos2unix
-#       dos2unix install_redis.sh
-#       chmod u+x install_redis.sh
+#       sudo apt-get -y install dos2unix
+#       dos2unix <FILE>
+#       chmod u+x <FILE>
 #
 clear
+echo "
+ _____             _         _    _          _                                   
+|     |___ ___ ___| |_ ___ _| |  | |_ _ _   |_|                                  
+|   --|  _| -_| .'|  _| -_| . |  | . | | |   _                                   
+|_____|_| |___|__,|_| |___|___|  |___|_  |  |_|                                  
+                                     |___|                                       
+                                                                                 
+ _____ _       _     _           _              _____    __    _____             
+|     | |_ ___|_|___| |_ ___ ___| |_ ___ ___   |     |__|  |  |   __|___ ___ _ _ 
+|   --|   |  _| |_ -|  _| . | . |   | -_|  _|  | | | |  |  |  |  |  |  _| .'| | |
+|_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
+                            |_|                                             |___|
+
+\r\n \r\n
+Version:  1.0                             \r\n
+Last Updated:  5/7/2017
+\r\n \r\n
+Updating system first..."
+sudo -E apt-get update
+wait
+sudo -E apt-get upgrade -y
+wait
+echo "Downloading required dependencies...\r\n\r\n"
+#--------------------------------------------------------------------------------------------
 
 echo "Installing Python 3.+ latest.... "
 sudo apt-get -y update 
@@ -12,9 +36,26 @@ wait
 sudo apt-get -y upgrade
 wait
 #---------- PYTHON STUFF ----------------------------------
-sudo apt-get install -y python3-virtualenv libicu-dev python-software-properties python python-pip python-dev python3-setuptools
+sudo apt-get install -y libicu-dev python-software-properties python python-pip python-dev python3-setuptools
+wait
+sudo apt install python3-pip
 wait
 sudo easy_install3 pip
 wait
 
+sudo -H pip3 install --upgrade pip 
+sudo -H pip install --upgrade pip
+
+pip install virtualenv
+pip install python3-virtualenv
+#--------------------------------------------
+sudo pip3 install --upgrade requests
+
+#--------------------------------------------
+echo "\r\n \r\n "
 echo "Done installing Python3 "
+pip -V; pip3 -V
+
+echo "\r\n \r\n "
+
+
