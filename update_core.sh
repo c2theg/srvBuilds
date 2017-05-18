902f7c4274d88a8080f1bab45d6c5feb10408ee0
Chris Gray <christophermjgray@gmail.com>
2017-05-18 11:27:13 -0400

    Add files via upload

---
+++ update_core.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+#    If you update this from Windows, using Notepad ++, do the following:
+#       sudo apt-get -y install dos2unix
+#       dos2unix <FILE>
+#       chmod u+x <FILE>
+#
+clear
+echo "
+ _____             _         _    _          _                                   
+|     |___ ___ ___| |_ ___ _| |  | |_ _ _   |_|                                  
+|   --|  _| -_| .'|  _| -_| . |  | . | | |   _                                   
+|_____|_| |___|__,|_| |___|___|  |___|_  |  |_|                                  
+                                     |___|                                       
+                                                                                 
+ _____ _       _     _           _              _____    __    _____             
+|     | |_ ___|_|___| |_ ___ ___| |_ ___ ___   |     |__|  |  |   __|___ ___ _ _ 
+|   --|   |  _| |_ -|  _| . | . |   | -_|  _|  | | | |  |  |  |  |  |  _| .'| | |
+|_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
+                            |_|                                             |___|
+
+\r\n \r\n
+Version:  1.0                             \r\n
+Last Updated:  5/18/2017
+\r\n \r\n
+Updating system first..."
+sudo -E apt-get update
+wait
+sudo -E apt-get upgrade -y
+wait
+echo "Freeing up space"
+sudo apt-get autoremove -y
+wait
+echo "Downloading required dependencies...\r\n\r\n"
+#--------------------------------------------------------------------------------------------
+echo "Downloading files..."
+if [ -s "sys_cleanup.sh" ] 
+then
+	echo "Deleting files"
+	rm sys_cleanup.sh
+ 	rm update_ubuntu14.04.sh
+ 	rm install_common.sh
+fi
+
+echo "Downloading latest versions..."
+
+wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/sys_cleanup.sh
+wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/update_ubuntu14.04.sh
+wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/install_common.sh
+
+
+echo "done! \r\n \r\n"
