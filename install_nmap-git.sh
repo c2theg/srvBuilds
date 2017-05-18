3a2bb573c605e22fca039c5016e4b4ff0816c1bb
Chris Gray <christophermjgray@gmail.com>
2017-05-18 00:26:42 -0400

    Add files via upload

---
+++ install_nmap-git.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+#    If you update this from Windows, using Notepad ++, do the following:
+#       sudo apt-get -y install dos2unix
+#       dos2unix <FILE>
+#       chmod u+x <FILE>
+#
+clear
+echo "
+ _____             _         _    _          _                                   
+|     |___ ___ ___| |_ ___ _| |  | |_ _ _   |_|                                  
+|   --|  _| -_| .'|  _| -_| . |  | . | | |   _                                   
+|_____|_| |___|__,|_| |___|___|  |___|_  |  |_|                                  
+                                     |___|                                       
+                                                                                 
+ _____ _       _     _           _              _____    __    _____             
+|     | |_ ___|_|___| |_ ___ ___| |_ ___ ___   |     |__|  |  |   __|___ ___ _ _ 
+|   --|   |  _| |_ -|  _| . | . |   | -_|  _|  | | | |  |  |  |  |  |  _| .'| | |
+|_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
+                            |_|                                             |___|
+
+\r\n \r\n
+Version:  1.0                             \r\n
+Last Updated:  5/18/2017
+\r\n \r\n
+#Updating system first..."
+sudo -E apt-get update
+wait
+sudo -E apt-get upgrade -y
+wait
+echo "Downloading required dependencies...\r\n\r\n"
+#--------------------------------------------------------------------------------------------
+sudo apt-get install -y git build-essential netcat zmap 
+wait
+mkdir nmap-git
+cd nmap-git/
+git clone https://github.com/nmap/nmap.git
+wait
+cd nmap
+sudo ./configure
+wait
+sudo make
+wait
+sudo make install
+wait
+echo "\r\n \r\n"
+
+
+nmap -v
+
+echo "Done!  \r\n"
+echo "Examples...  \r\n"
+echo " nmap -v -sU -sT -p U:53,111,137,T:21-25,80,139,8080 96.119.83.25  \r\n"
+echo " nmap -Pn --top-ports 25 96.119.83.25  \r\n"
+echo " nmap -sTU --top-ports 25 <IP> \r\n  \r\n"
+
+echo " ------------------------------------------------------------------------ \r\n "
+
+echo " -- to Update NMAP from Git (Latest) \r\n "
+echo " git pull origin master  \r\n"
+echo " sudo make clean \r\n"
+echo " sudo make \r\n"
+echo " sudo make install  \r\n"
+
+
+
+echo "\r\n \r\n"
