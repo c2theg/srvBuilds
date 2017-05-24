ca850d6705dfc358830e32420cbcded801ddf7b7
Chris Gray <christophermjgray@gmail.com>
2017-05-24 10:34:32 -0400

    Create update_lets_encrypt.sh

---
+++ update_lets_encrypt.sh
@@ -0,0 +1,16 @@
+sudo ~/.local/share/letsencrypt/bin/letsencrypt certonly \
+--agree-tos --email someone@gmail.com \
+--webroot \
+--webroot-path  /var/www \
+--domains <YOUR WEBSITE>
+
+
+if [ $? -ne 0 ]
+ then
+        ERRORLOG=`tail /var/log/letsencrypt/letsencrypt.log`
+        echo -e "The Lets Encrypt Cert has not been renewed! \n \n" $ERRORLOG | mail -s "Lets Encrypt Cert Alert" someone@gmail.com
+ else
+        service nginx restart
+fi
+
+exit 0
