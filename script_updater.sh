1de1f4338c177b07659816859d6ca881def91546
Chris Gray <christophermjgray@gmail.com>
2017-06-04 12:20:51 -0400

    Update script_updater.sh

--- script_updater.sh
+++ script_updater.sh
@@ -1,19 +1,22 @@
 #place holder for the real code
 
 cd ~
-
 #---------------------------------------------------------------------------------------------
-rm sys_cleanup.sh
-rm update_ubuntu14.04.sh
-rm update_core.sh
-
+sudo rm sys_cleanup.sh
+sudo rm update_ubuntu14.04.sh
+sudo rm update_core.sh
+sudo rm script_updater.sh
 #---------------------------------------------------------------------------------------------
-wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/sys_cleanup.sh
-wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/update_ubuntu14.04.sh
-wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/update_core.sh
+sudo wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/sys_cleanup.sh
+sudo wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/update_ubuntu14.04.sh
+sudo wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/update_core.sh
 
+sudo wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/script_updater.sh
 #---------------------------------------------------------------------------------------------
-chmod u+x sys_cleanup.sh 
-chmod u+x update_ubuntu14.04.sh
-chmod u+x update_core.sh
+sudo chmod u+x script_updater.sh
+sudo chmod u+x sys_cleanup.sh 
+sudo chmod u+x update_ubuntu14.04.sh
+sudo chmod u+x update_core.sh
 
+
+echo "DONE! \r\n \r\n";
