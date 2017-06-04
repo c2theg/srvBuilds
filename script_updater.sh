b7b2ac2d85ad4725e54e479446154e3536570cbf
Chris Gray <christophermjgray@gmail.com>
2017-06-04 12:16:23 -0400

    Update script_updater.sh

--- script_updater.sh
+++ script_updater.sh
@@ -1,1 +1,19 @@
 #place holder for the real code
+
+cd ~
+
+#---------------------------------------------------------------------------------------------
+rm sys_cleanup.sh
+rm update_ubuntu14.04.sh
+rm update_core.sh
+
+#---------------------------------------------------------------------------------------------
+wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/sys_cleanup.sh
+wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/update_ubuntu14.04.sh
+wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/update_core.sh
+
+#---------------------------------------------------------------------------------------------
+chmod u+x sys_cleanup.sh 
+chmod u+x update_ubuntu14.04.sh
+chmod u+x update_core.sh
+
