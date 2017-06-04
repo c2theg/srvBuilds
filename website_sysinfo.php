5475581eb6bccec5f9b606d42b4f42623ea7d687
Chris Gray <christophermjgray@gmail.com>
2017-06-04 12:10:57 -0400

    Update website_sysinfo.php

--- website_sysinfo.php
+++ website_sysinfo.php
@@ -2,5 +2,11 @@
         $my_current_ip=exec("ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'");
         echo "Server IP: ".$my_current_ip;
         echo "<br /><br /><br />";
+
+        echo "Links: <br /><br />
+                Cockpit <a href='https://127.0.0.1:9090'>https://127.0.0.1:9090</a>  <br />
+                Webmin <a href='https://127.0.0.1:10000'>https://127.0.0.1:10000</a>  <br /><br />
+                
+        <br /><br /><br />";
         phpinfo();
 ?>
