53ed301de3e7b28f9b9f650aa99d6b0b29c9ffd5
Chris Gray <christophermjgray@gmail.com>
2017-07-08 11:48:06 -0400

    Update install_nmap-git.sh

--- install_nmap-git.sh
+++ install_nmap-git.sh
@@ -19,8 +19,8 @@ echo "
                             |_|                                             |___|
 
 \r\n \r\n
-Version:  1.0                             \r\n
-Last Updated:  5/18/2017
+Version:  1.1                             \r\n
+Last Updated:  7/8/2017
 \r\n \r\n
 #Updating system first..."
 sudo -E apt-get update
