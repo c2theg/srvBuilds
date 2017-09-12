4076ce0452b25c37672c88d556d1a23a4c2130ae
Chris Gray <christophermjgray@gmail.com>
2017-09-12 10:24:33 -0400

    Create install_phantomjs.sh

---
+++ install_phantomjs.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+#    If you update this from Windows, using Notepad ++, do the following:
+#       sudo apt-get -y install dos2unix
+#       dos2unix <FILE>
+#       chmod u+x <FILE>
+#
+clear
+echo "
+ _____             _         _    _          _                                   
+|     |___ ___ ___| |_ ___ _| |  | |_ _ _   |_|                                  
+|   --|  _| -_| .'|  _| -_| . |  | . | | |   _                                   
+|_____|_| |___|__,|_| |___|___|  |___|_  |  |_|                                  
+                                     |___|                                       
+                                                                                 
+ _____ _       _     _           _              _____    __    _____             
+|     | |_ ___|_|___| |_ ___ ___| |_ ___ ___   |     |__|  |  |   __|___ ___ _ _ 
+|   --|   |  _| |_ -|  _| . | . |   | -_|  _|  | | | |  |  |  |  |  |  _| .'| | |
+|_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
+                            |_|                                             |___|
+\r\n \r\n
+https://raw.githubusercontent.com/c2theg/srvBuilds/master/install_phantomjs.sh
+This really is meant to be run under Ubuntu 14.04 LTS +
+\r\n \r\n
+Version:  0.0.1                             \r\n
+Last Updated:  9/12/2017
+\r\n \r\n
+Updating system first..."
+sudo -E apt-get update
+wait
+sudo -E apt-get upgrade -y
+wait
+echo "Freeing up space"
+sudo apt-get autoremove -y
+wait
+echo "Downloading required dependencies...\r\n\r\n"
+#--------------------------------------------------------------------------------------------
+sudo -E apt-get install -y build-essential chrpath libssl-dev libxft-dev libfreetype6-dev libfreetype6 libfontconfig1-dev libfontconfig1 -y
+wait
+
+sudo wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
+wait 
+
+sudo tar xvjf phantomjs-2.1.1-linux-x86_64.tar.bz2 -C /usr/local/share/
+wait
+sudo ln -s /usr/local/share/phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/local/bin/
+wait
+echo " \r\n \r\n"
+echo " \r\n \r\n"
+
+phantomjs --version
+
+echo " \r\n \r\n"
+
+echo "DONE! "
+echo " \r\n \r\n"
+echo " \r\n \r\n"
