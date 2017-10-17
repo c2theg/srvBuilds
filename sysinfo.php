71ceeeffddae37c44719ec39e9221a6cc0a14dc8
Chris Gray <christophermjgray@gmail.com>
2017-10-17 10:51:17 -0400

    Update sysinfo.php

--- sysinfo.php
+++ sysinfo.php
@@ -1,9 +1,11 @@
 <?php
   /*
-    This is for DEVELOPMENT ONLY! Do not deploy on a production server, as it will expose sensitive service info to the world
+  
+  This is for DEVELOPMENT ONLY! Do not deploy on a production server, as it will expose sensitive service info to the world
+  
   */
   $ServerIP = $_SERVER["SERVER_ADDR"];
-	echo 'ServerIP: '.$ServerIP.'<br /><br />';
+  echo 'ServerIP: '.$ServerIP.'<br /><br />';
 	
     
   $RemoteIP = $_SERVER["REMOTE_ADDR"];
