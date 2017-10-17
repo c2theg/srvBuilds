c01866214bd4d583d86d233cdddfcc56ed49e128
Chris Gray <christophermjgray@gmail.com>
2017-10-17 10:50:40 -0400

    Create sysinfo.php

---
+++ sysinfo.php
@@ -0,0 +1,13 @@
+<?php
+  /*
+    This is for DEVELOPMENT ONLY! Do not deploy on a production server, as it will expose sensitive service info to the world
+  */
+  $ServerIP = $_SERVER["SERVER_ADDR"];
+	echo 'ServerIP: '.$ServerIP.'<br /><br />';
+	
+    
+  $RemoteIP = $_SERVER["REMOTE_ADDR"];
+  echo 'Client: '.$RemoteIP.'<br /><br />';
+  
+  phpinfo();
+?>
