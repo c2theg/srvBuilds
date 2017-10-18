2dd7cc878a6ee3e0575adf1719babaed2aab9854
Chris Gray <christophermjgray@gmail.com>
2017-10-18 02:15:08 -0400

    Create fix_permissions.sh

---
+++ fix_permissions.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+ocpath='/var/www/nextcloud'
+htuser='www-data'
+htgroup='www-data'
+rootuser='root'
+
+find ${ocpath}/ -type f -print0 | xargs -0 chmod 0640
+find ${ocpath}/ -type d -print0 | xargs -0 chmod 0750
+
+chown -R ${rootuser}:${htgroup} ${ocpath}/
+chown -R ${htuser}:${htgroup} ${ocpath}/apps/
+chown -R ${htuser}:${htgroup} ${ocpath}/config/
+chown -R ${htuser}:${htgroup} ${ocpath}/data/
+chown -R ${htuser}:${htgroup} ${ocpath}/themes/
+
+chown ${rootuser}:${htgroup} ${ocpath}/.htaccess
+chown ${rootuser}:${htgroup} ${ocpath}/data/.htaccess
+
+chmod 0644 ${ocpath}/.htaccess
+chmod 0644 ${ocpath}/data/.htaccess
