65330d8a2573fa85a0ad63bd959c9f1f734d897b
Chris Gray <christophermjgray@gmail.com>
2017-10-18 08:51:41 -0400

    Update gen_ssl_cert.sh

--- gen_ssl_cert.sh
+++ gen_ssl_cert.sh
@@ -5,22 +5,14 @@ fqdn='cloud.site.com'
 certsize='2048'
 #----------------------------------------------------------------------------
 echo -e "\r\n \r\n"
-
 read -p "Enter your FQDN: "  fqdn
-echo -e "Welcome $fqdn!"
-
-read -p "Enter DH key size (2048 or 4096): "  certsize
+echo -e "You entered: $fqdn!"
+read -p "Enter Cert-key size (2048 or 4096): "  certsize
 echo -e "Cert Size: $certsize"
-
 echo -e "\r\n \r\n"
 #----------------------------------------------------------------------------
 cd ${path}
-
 openssl req -new -newkey rsa:$certsize -sha256 -nodes -keyout ${file_prefix}_${fqdn}.key -out ${file_prefix}_${fqdn}.csr -text -subj "/C=US/ST=NA/L=NA/O={fqdn}/OU=HQ/CN={fqdn}"
-
-openssl dhparam -out ${file_prefix}_${fqdn}.pem $certsize 
-
+openssl dhparam -out ${file_prefix}_${fqdn}.pem $certsize
 cat ${file_prefix}_${fqdn}.csr
-
-
 echo -e "\r\n \r\n SSL Cert Path: $path \r\n \r\n"
