f1c2eb7a2734d2dd8ec5284d68c372c5884b2e2f
Chris Gray <christophermjgray@gmail.com>
2017-10-18 10:50:08 -0400

    Update install_percona_5.7.sh

--- install_percona_5.7.sh
+++ install_percona_5.7.sh
@@ -18,7 +18,7 @@ echo "
 |_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
                             |_|                                             |___|
 \r\n \r\n
-Version:  1.0                             \r\n
+Version:  1.1                             \r\n
 Last Updated:  10/18/2017
 \r\n \r\n
 Updating system first..."
@@ -28,24 +28,23 @@ sudo -E apt-get upgrade -y
 wait
 echo "Downloading required dependencies...\r\n\r\n"
 #--------------------------------------------------------------------------------------------
-apt-key adv --keyserver keys.gnupg.net --recv-keys 8507EFA5
+apt-key -y adv --keyserver keys.gnupg.net --recv-keys 8507EFA5
 
 wget https://repo.percona.com/apt/percona-release_0.1-4.$(lsb_release -sc)_all.deb
 dpkg -i percona-release_0.1-4.$(lsb_release -sc)_all.deb
-sudo apt-get update && sudo apt-get install mysql-client-core-5.7 libmecab2 percona-server-client-5.7 percona-server-common-5.7 php7.0-mysql
+sudo -E apt-get update
+wait
+sudo apt-get install -y mysql-client-core-5.7 libmecab2 percona-server-client-5.7 percona-server-common-5.7 php7.0-mysql
 
-echo "Downloading Config"
-wget "my.cnf" "https://raw.githubusercontent.com/c2theg/srvBuilds/master/my.cnf"
+echo -e "\r\n Downloading Config \r\n"
+wget "mysqld.cnf" "https://raw.githubusercontent.com/c2theg/srvBuilds/master/mysqld.cnf"
 wait
-sudo cp "my.cnf" "/etc/mysql/my.cnf"
+sudo cp "mysqld.cnf" "/etc/mysql/percona-server.conf.d/mysqld.cnf"
 wait
-echo "Percona Config Download Complete"
+echo -e "\r\n Percona Config Download Complete \r\n"
 sudo /etc/init.d/mysql restart
-echo "\r\n \r\n \r\n \r\n"
+echo -e "\r\n \r\n \r\n \r\n"
 echo "To create a remote connection use the following: \r\n \r\n"
-echo " mysql --user=root --password=*** mysql  \r\n\r\n"
-echo " GRANT ALL PRIVILEGES ON *.* TO 'user1'@'%' IDENTIFIED BY '***' WITH GRANT OPTION; \r\n\r\n"
-echo " Done! \r\n\r\n"
-
-
-echo "DONE \r\n \r\n"
+echo -e " mysql --user=root --password=*** mysql  \r\n\r\n"
+echo -e " GRANT ALL PRIVILEGES ON *.* TO 'user1'@'%' IDENTIFIED BY '***' WITH GRANT OPTION; \r\n\r\n"
+echo -e " Done! \r\n\r\n"
