b34be4c1725829db9aeb1e354c7d8f944daa0234
Chris Gray <christophermjgray@gmail.com>
2017-10-19 14:58:11 -0400

    Update install_webmin.sh

--- install_webmin.sh
+++ install_webmin.sh
@@ -5,6 +5,10 @@
 #       chmod u+x <FILE>
 #
 clear
+
+Version='1.860'
+
+
 echo "
  _____             _         _    _          _                                   
 |     |___ ___ ___| |_ ___ _| |  | |_ _ _   |_|                                  
@@ -19,10 +23,13 @@ echo "
                             |_|                                             |___|
 
 \r\n \r\n
-Version:  1.2.1                             \r\n
+Version:  1.2.2                             \r\n
 Last Updated:  10/19/2017
 \r\n \r\n
+
+This will install version  $Version of Webmin \r\n \r\n
 Updating system first..."
+
 sudo -E apt-get update
 wait
 sudo -E apt-get upgrade -y
@@ -32,7 +39,7 @@ echo "Downloading required dependencies...\r\n\r\n"
 sudo apt-get -y install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python
 wait
 #sudo wget http://prdownloads.sourceforge.net/webadmin/webmin_1.860_all.deb 
-sudo wget https://downloads.sourceforge.net/project/webadmin/webmin/1.860/webmin_1.860_all.deb?r=http%3A%2F%2Fwww.webmin.com%2Fdownload.html&ts=1508439010&use_mirror=ayera
+sudo wget -O webmin.deb https://downloads.sourceforge.net/project/webadmin/webmin/$Version/webmin_$Version_all.deb?r=http%3A%2F%2Fwww.webmin.com%2Fdownload.html&ts=1508439010&use_mirror=ayera
 wait
-sudo dpkg --install webmin_1.860_all.deb
+sudo dpkg --install webmin_$Version_all.deb
 echo "Done! \r\n \r\n"
