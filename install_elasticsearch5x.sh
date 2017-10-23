43df2cf575c04703c030af44e67bee18cdefce3b
Chris Gray <christophermjgray@gmail.com>
2017-10-22 23:32:41 -0400

    Create install_elasticsearch5x.sh

---
+++ install_elasticsearch5x.sh
@@ -0,0 +1,44 @@
+
+#!/bin/sh
+#    If you update this from Windows, using Notepad ++, do the following:
+#       sudo apt-get -y install dos2unix
+#       dos2unix <FILE>
+#       chmod u+x <FILE>
+#
+clear
+echo "
+ _____             _         _    _          _                                   
+|     |___ ___ ___| |_ ___ _| |  | |_ _ _   |_|                                  
+|   --|  _| -_| .'|  _| -_| . |  | . | | |   _                                   
+|_____|_| |___|__,|_| |___|___|  |___|_  |  |_|                                  
+                                     |___|                                       
+                                                                                 
+ _____ _       _     _           _              _____    __    _____             
+|     | |_ ___|_|___| |_ ___ ___| |_ ___ ___   |     |__|  |  |   __|___ ___ _ _ 
+|   --|   |  _| |_ -|  _| . | . |   | -_|  _|  | | | |  |  |  |  |  |  _| .'| | |
+|_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
+                            |_|                                             |___|
+\r\n \r\n
+
+https://www.elastic.co/guide/index.html
+
+https://raw.githubusercontent.com/c2theg/srvBuilds/master/install_elasticsearch5x.sh
+This really is meant to be run under Ubuntu 14.04 - 16.04 LTS +
+\r\n \r\n
+Version:  0.0.1                             \r\n
+Last Updated:  10/22/2017
+\r\n \r\n"
+
+echo -e "Installing Elastic Search 5.X \r\n \r\n"
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+
+sudo apt-get install -y apt-transport-https
+
+echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-5.x.list
+
+sudo apt-get update
+
+sudo apt-get install -y elasticsearch
+
+
+echo "DONE! \r\n \r\n"
