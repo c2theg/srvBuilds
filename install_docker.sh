7e42c3da50a23edf8e68e0b0817923d2e128656c
Chris Gray <christophermjgray@gmail.com>
2017-10-24 20:08:16 -0400

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -19,8 +19,8 @@ echo "
                             |_|                                             |___|
 
 \r\n \r\n
-Version:  1.0                             \r\n
-Last Updated:  5/7/2017
+Version:  1.3                             \r\n
+Last Updated:  10/24/2017
 \r\n \r\n
 Updating system first..."
 #sudo -E apt-get update
