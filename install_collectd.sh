66734e66a59a399b1667b6424deec8f06eff581f
Chris Gray <christophermjgray@gmail.com>
2017-10-28 22:46:32 -0400

    Create install_collectd.sh

---
+++ install_collectd.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#    If you update this from Windows, using Notepad ++, do the following:
+#       sudo apt-get -y install dos2unix
+#       dos2unix <FILE>
+#       chmod u+x <FILE>
+#
+clear
+echo "
+ _____             _         _    _          _                                   
+|     |___ ___ ___| |_ ___ _| |  | |_ _ _   |_|                                  
+|   --|  _| -_| .'|  _| -_| . |  | . | | |   _                                   
+|_____|_| |___|__,|_| |___|___|  |___|_  |  |_|                                  
+                                     |___|                                       
+                                                                                 
+ _____ _       _     _           _              _____    __    _____             
+|     | |_ ___|_|___| |_ ___ ___| |_ ___ ___   |     |__|  |  |   __|___ ___ _ _ 
+|   --|   |  _| |_ -|  _| . | . |   | -_|  _|  | | | |  |  |  |  |  |  _| .'| | |
+|_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
+                            |_|                                             |___|
+\r\n \r\n
+https://raw.githubusercontent.com/c2theg/srvBuilds/master/install_collectd.sh
+This really is meant to be run under Ubuntu 14.04 - 16.04 LTS +
+\r\n \r\n
+Version:  0.0.1                             \r\n
+Last Updated:  10/28/2017
+\r\n \r\n
+Installing CollectD"
+
+sudo apt-get install collectd collectd-utils
+
+
+
