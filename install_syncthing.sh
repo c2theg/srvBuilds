47e315217ffa0383f6c130a3e8f274d3363a9a88
Chris Gray <christophermjgray@gmail.com>
2017-11-07 15:11:24 -0500

    Create install_syncthing.sh

---
+++ install_syncthing.sh
@@ -0,0 +1,104 @@
+#!/bin/sh
+#    If you update this from Windows, using Notepad ++, do the following:
+#       sudo apt-get -y install dos2unix
+#       dos2unix <FILE>
+#       chmod u+x <FILE>
+#
+clear
+echo "
+ _____             _         _    _          _                                   
+|     |___ ___ ___| |_ ___ _| |  | |_ _ _   |_|                                  
+|   --|  _| -_| .'|  _| -_| . |  | . | | |   _                                   
+|_____|_| |___|__,|_| |___|___|  |___|_  |  |_|                                  
+                                     |___|                                       
+                                                                                 
+ _____ _       _     _           _              _____    __    _____             
+|     | |_ ___|_|___| |_ ___ ___| |_ ___ ___   |     |__|  |  |   __|___ ___ _ _ 
+|   --|   |  _| |_ -|  _| . | . |   | -_|  _|  | | | |  |  |  |  |  |  _| .'| | |
+|_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
+                            |_|                                             |___|
+
+\r\n \r\n
+Version:  0.4                             \r\n
+Last Updated:  11/7/2017
+\r\n \r\n
+Updating system first..."
+sudo -E apt-get update
+wait
+sudo -E apt-get upgrade -y
+wait
+#
+#
+#  https://www.linuxbabe.com/ubuntu/install-syncthing-ubuntu-16-04-via-official-deb-repository
+#  
+#
+#
+#
+echo "Downloading required dependencies...\r\n\r\n"
+#--------------------------------------------------------------------------------------------
+
+#------------- Version Detection -------------
+if [ -f /etc/os-release ]; then
+    # freedesktop.org and systemd
+    . /etc/os-release
+    OS=$NAME
+    VER=$VERSION_ID
+elif type lsb_release >/dev/null 2>&1; then
+    # linuxbase.org
+    OS=$(lsb_release -si)
+    VER=$(lsb_release -sr)
+elif [ -f /etc/lsb-release ]; then
+    # For some versions of Debian/Ubuntu without lsb_release command
+    . /etc/lsb-release
+    OS=$DISTRIB_ID
+    VER=$DISTRIB_RELEASE
+elif [ -f /etc/debian_version ]; then
+    # Older Debian/Ubuntu/etc.
+    OS=Debian
+    VER=$(cat /etc/debian_version)
+elif [ -f /etc/SuSe-release ]; then
+    # Older SuSE/etc.
+    ...
+elif [ -f /etc/redhat-release ]; then
+    # Older Red Hat, CentOS, etc.
+    ...
+else
+    # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
+    OS=$(uname -s)
+    VER=$(uname -r)
+fi
+echo " Detected: OS: $OS, Version: $VER \r\n \r\n"
+#-----------------------------------------------
+sudo apt-get install curl apt-transport-https
+wait
+
+if [ $VER == '14.04' ]; then
+    #-------- Ubuntu 14.04 ------------------------
+    sudo add-apt-repository ppa:ytvwld/syncthing
+elif [ $VER == '16.06' ]; then
+    #-------- Ubuntu 16.04 ------------------------
+    curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
+    echo "deb http://apt.syncthing.net/ syncthing release" | sudo tee /etc/apt/sources.list.d/syncthing.list
+fi
+
+#---------------------------------------------------------------------
+sudo -E apt-get update
+wait
+sudo -E apt-get install -y syncthing
+#-------------- Configure and start it -------------------------------
+if [ $VER == '14.04' ]; then
+    #-------- Ubuntu 14.04 ------------------------
+    echo "Config: ~/.config/syncthing/config.xml  \r\n \r\n "
+    nano ~/.config/syncthing/config.xml
+    syncthing
+elif [ $VER == '16.06' ]; then
+    #-------- Ubuntu 16.04 ------------------------
+    sudo systemctl enable syncthing@
+    ubuntu.service
+    sudo systemctl start syncthing@ubuntu.service
+    systemctl status syncthing@ubuntu.service
+fi
+#----------------------------------------------------------------------
+
+
+echo "view: 127.0.0.1:8384  to access SyncThing  \r\n \r\n ";
