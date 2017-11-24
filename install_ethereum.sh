3620f6f84ff9d5f80ea2e0d89ef870cc69893a78
Chris Gray <christophermjgray@gmail.com>
2017-11-24 09:36:56 -0500

    Update install_ethereum.sh

--- install_ethereum.sh
+++ install_ethereum.sh
@@ -13,32 +13,27 @@ sudo apt-get install -y software-properties-common
 #sudo add-apt-repository -y ppa:ethereum/ethereum
 #sudo sed 's/jessie/vivid/' -i /etc/apt/sources.list.d/ethereum-ethereum-*.list
 
-wget "ethminer" "https://github.com/ethereum-mining/ethminer/releases/download/v0.12.0/ethminer-0.12.0-Linux.tar.gz"
-
+# wget "ethminer" "https://github.com/ethereum-mining/ethminer/releases/download/v0.12.0/ethminer-0.12.0-Linux.tar.gz"
 
 sudo add-apt-repository -y ppa:ethereum/ethereum-qt
 sudo add-apt-repository -y ppa:ethereum/ethereum
-sudo add-apt-repository ppa:ethereum/ethereum-dev
+sudo add-apt-repository -y ppa:ethereum/ethereum-dev
 sudo apt-get update
-sudo apt-get install ethereum
-sudo apt-get install cpp-ethereum
+sudo apt-get install -y ethereum
+sudo apt-get install -y cpp-ethereum
+sudo apt-get install -y ethminer
 
-
-
-#sudo apt-get update
-#sudo apt-get install -y ethereum ethminer
-geth account new
-geth --rpc --rpccorsdomain localhost 2>> geth.log &
+#geth account new
+#geth --rpc --rpccorsdomain localhost 2>> geth.log &
 #ethminer -G  // -G for GPU, -M for benchmark
 #tail -f geth.log
-
 
 #sudo apt-get install -y linux-headers-amd64 build-essential
 #ethminer -G -F http://us1.ethermine.org:14444/0x2acd4A4D3c71B9c81f5E48a76766c1860c650F6B --farm-recheck 200
 
 #geth --mine --minerthreads=4
 #geth --etherbase 1 --mine  2>> geth.log // 1 is index: second account by creation order OR
-geth --etherbase '0x2acd4A4D3c71B9c81f5E48a76766c1860c650F6B' --mine 2>> geth.log
+#geth --etherbase '0x2acd4A4D3c71B9c81f5E48a76766c1860c650F6B' --mine 2>> geth.log
 
 #ethminer --farm-recheck 200 -G -S us1.ethermine.org:14444 -FS us1.ethermine.org:4444 -O 0x2acd4A4D3c71B9c81f5E48a76766c1860c650F6B.server1
 
