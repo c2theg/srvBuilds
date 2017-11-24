bc7d8cab7c7c46a7971191ca12803ff30d998902
Chris Gray <christophermjgray@gmail.com>
2017-11-24 09:37:49 -0500

    Update install_ethereum.sh

--- install_ethereum.sh
+++ install_ethereum.sh
@@ -1,13 +1,5 @@
 #------------------------------------------
 #  http://ethpool.org/
-#
-#
-# setx GPU_FORCE_64BIT_PTR 0
-# setx GPU_MAX_HEAP_SIZE 100
-# setx GPU_USE_SYNC_OBJECTS 1
-# setx GPU_MAX_ALLOC_PERCENT 100
-# setx GPU_SINGLE_ALLOC_PERCENT 100
-# ethminer.exe --farm-recheck 200 -G -S eu1.ethpool.org:3333 -FS us1.ethpool.org:3333 -O <Your_Ethereum_Address>.<RigName>
 #------------------------------------------
 sudo apt-get install -y software-properties-common
 #sudo add-apt-repository -y ppa:ethereum/ethereum
