ab5b27f6321c21f94321394359e904efc4079704
Chris Gray <christophermjgray@gmail.com>
2017-12-12 15:09:07 -0500

    Update fix_mysql_replication.sh

--- fix_mysql_replication.sh
+++ fix_mysql_replication.sh
@@ -1,8 +1,1 @@
-mysql --user=root --password=<password> mysql
-wait;
-STOP SLAVE;
-wait;
-SET GLOBAL SQL_SLAVE_SKIP_COUNTER = 1;
-wait;
-START SLAVE; 
-SHOW SLAVE STATUS;
+mysql --user=root --password=<Password> -e "STOP SLAVE; SET GLOBAL SQL_SLAVE_SKIP_COUNTER = 1; START SLAVE; SHOW SLAVE STATUS;"
