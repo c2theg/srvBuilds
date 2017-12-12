57ac571ce1b12e6c78b47ee0abd8f75f74eadb81
Chris Gray <christophermjgray@gmail.com>
2017-12-12 15:15:43 -0500

    Update fix_mysql_replication.sh

--- fix_mysql_replication.sh
+++ fix_mysql_replication.sh
@@ -1,5 +1,9 @@
 mysql --user=root --password=<Password> -e "STOP SLAVE; SET GLOBAL SQL_SLAVE_SKIP_COUNTER = 1; START SLAVE; SHOW SLAVE STATUS;"
 
-echo "\r\n \r\n "
-echo "To fix this on startup, add the following to crontab \r\n \r\n "
-echo "@reboot /home/ubuntu/fix_mysql_replication.sh >> /var/log/fix_mysql_replication.log 2>&1 \r\n \r\n "
+echo " "
+echo " "
+echo "To fix this on startup, add the following to crontab"
+echo " "
+echo "@reboot /home/ubuntu/fix_mysql_replication.sh >> /var/log/fix_mysql_replication.log 2>&1"
+echo " "
+echo " "
