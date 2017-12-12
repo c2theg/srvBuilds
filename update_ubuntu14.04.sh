c1607baa94989f51d33807211d63d3541cabe7e2
Chris Gray <christophermjgray@gmail.com>
2017-12-12 15:21:56 -0500

    Update update_ubuntu14.04.sh

--- update_ubuntu14.04.sh
+++ update_ubuntu14.04.sh
@@ -21,8 +21,8 @@ echo "Running update_core.sh at $now
                             |_|                                             |___|
 
 \r\n \r\n
-Version:  1.6.1                             \r\n
-Last Updated:  10/12/2017
+Version:  1.6.2                             \r\n
+Last Updated:  12/12/2017
 \r\n \r\n"
 wait
 sudo -E apt-get update
@@ -53,6 +53,7 @@ echo " crontab -e \r\n"
 echo "10 3 * * * /home/ubuntu/update_core.sh >> /var/log/update_core.log 2>&1"
 echo "40 4 * * * /home/ubuntu/update_ubuntu14.04.sh >> /var/log/update_ubuntu.log 2>&1"
 echo "20 4 * * 7 /home/ubuntu/sys_cleanup.sh >> /var/log/sys_cleanup.log 2>&1"
+echo "@reboot /home/ubuntu/update_core.sh >> /var/log/update_core.log 2>&1"
 
 echo "\r\n \r\n "
 echo " /etc/init.d/cron restart "
