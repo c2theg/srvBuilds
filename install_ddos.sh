9bfb47b0169396d4ec45c3942879e01bcfefd15c
Chris Gray <christophermjgray@gmail.com>
2017-12-14 11:07:05 -0500

    Update install_ddos.sh

--- install_ddos.sh
+++ install_ddos.sh
@@ -9,7 +9,6 @@ wait
 wget "install_nmap-git.sh" "https://raw.githubusercontent.com/c2theg/srvBuilds/master/install_nmap-git.sh" && chmod u+x install_nmap-git.sh && ./install_nmap-git.sh
 wait
 
-
 #--- fastnetmon -----  https://github.com/pavel-odintsov/fastnetmon/blob/master/docs/INSTALL.md
 wget https://raw.githubusercontent.com/pavel-odintsov/fastnetmon/master/src/fastnetmon_install.pl -Ofastnetmon_install.pl
 wait 
