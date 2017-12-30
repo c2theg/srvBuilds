68ebaeb0cb639510220d924d69ae1aaf39d71c2a
Chris Gray <christophermjgray@gmail.com>
2017-12-30 11:58:04 -0500

    Update install_beats.sh

--- install_beats.sh
+++ install_beats.sh
@@ -5,6 +5,9 @@
 #       dos2unix <FILE>
 #       chmod u+x <FILE>
 #
+
+version=6.1.1
+
 clear
 echo "
  _____             _         _    _          _                                   
@@ -26,24 +29,28 @@ https://raw.githubusercontent.com/c2theg/srvBuilds/master/install_beats.sh
 
 This really is meant to be run under Ubuntu 14.04 - 16.04 LTS +
 \r\n \r\n
-Version:  0.0.3                             \r\n
-Last Updated:  10/20/2017
+Version:  0.0.5                             \r\n
+Last Updated:  12/30/2017
 \r\n \r\n"
 
 
 echo -e "Installing Packetbeat \r\n  https://www.elastic.co/guide/en/beats/packetbeat/current/packetbeat-getting-started.html  \r\n \r\n"
 sudo apt-get -y install libpcap0.8
-curl -L -O https://artifacts.elastic.co/downloads/beats/packetbeat/packetbeat-5.6.3-amd64.deb
-sudo dpkg -i packetbeat-5.6.3-amd64.deb
+curl -L -O https://artifacts.elastic.co/downloads/beats/packetbeat/packetbeat-$version-amd64.deb
+sudo dpkg -i packetbeat-$version-amd64.deb
 
 
 echo -e "Installing Filebeat \r\n https://www.elastic.co/guide/en/beats/filebeat/current/filebeat-getting-started.html \r\n \r\n"
-curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-5.6.3-amd64.deb
-sudo dpkg -i filebeat-5.6.3-amd64.deb
+curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-$version-amd64.deb
+sudo dpkg -i filebeat-$version-amd64.deb
 
 
 echo -e "Installing Metricbeat \r\n https://www.elastic.co/guide/en/beats/metricbeat/current/metricbeat-getting-started.html \r\n \r\n"
-curl -L -O https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-5.6.3-amd64.deb
-sudo dpkg -i metricbeat-5.6.3-amd64.deb
+curl -L -O https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-$version-amd64.deb
+sudo dpkg -i metricbeat-$version-amd64.deb
+
+#echo -e "Installing HeartBeat \r\n hhttps://www.elastic.co/guide/en/beats/heartbeat/current/heartbeat-installation.html \r\n \r\n"
+#curl -L -O https://artifacts.elastic.co/downloads/beats/heartbeat/heartbeat-$version-amd64.deb
+#sudo dpkg -i heartbeat-$version-amd64.deb
 
 echo "DONE! \r\n \r\n"
