e8ef90cd45fc96a958d7412b00316a12889832b0
Chris Gray <christophermjgray@gmail.com>
2018-02-13 09:04:26 -0500

    Create install_ubuntu_kvm.sh

---
+++ install_ubuntu_kvm.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+#    If you update this from Windows, using Notepad ++, do the following:
+#       sudo apt-get -y install dos2unix
+#       dos2unix <FILE>
+#       chmod u+x <FILE>
+#
+clear
+
+
+echo "
+ _____             _         _    _          _                                   
+|     |___ ___ ___| |_ ___ _| |  | |_ _ _   |_|                                  
+|   --|  _| -_| .'|  _| -_| . |  | . | | |   _                                   
+|_____|_| |___|__,|_| |___|___|  |___|_  |  |_|                                  
+                                     |___|                                       
+                                                                                 
+ _____ _       _     _           _              _____    __    _____             
+|     | |_ ___|_|___| |_ ___ ___| |_ ___ ___   |     |__|  |  |   __|___ ___ _ _ 
+|   --|   |  _| |_ -|  _| . | . |   | -_|  _|  | | | |  |  |  |  |  |  _| .'| | |
+|_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
+                            |_|                                             |___|
+
+\r\n \r\n
+Version:  0.0.1                             \r\n
+Last Updated:  2/13/2018
+\r\n \r\n
+
+This will install version  $Version of Webmin \r\n \r\n
+Updating system first..."
+
+sudo -E apt-get update
+wait
+sudo -E apt-get upgrade -y
+wait
+echo "Downloading required dependencies...\r\n\r\n"
+#--------------------------------------------------------------------------------------------
+sudo apt-get install qemu-kvm libvirt-bin ubuntu-vm-builder bridge-utils
+
+sudo adduser `id -un` libvirtd
+
+virsh -c qemu:///system list
+
+sudo apt-get install virt-manager
+
+
