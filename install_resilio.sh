56750e92cc51121db591c97412950505a419bf4c
Chris Gray <christophermjgray@gmail.com>
2018-02-25 16:25:02 -0500

    Update install_resilio.sh

--- install_resilio.sh
+++ install_resilio.sh
@@ -91,3 +91,5 @@ sudo chmod -R 755 /var/www/
 
 sudo chown -R www-data:www-data /var/www/
 sudo chown -R rslsync:rslsync /var/www/
+
+sudo chmod -R 755 /media/data/sync/ && sudo chown -R rslsync:rslsync /media/data/sync/
