53e4307a8ec8de9d7a88a2bdc198e550d9542f51
Chris Gray <christophermjgray@gmail.com>
2018-03-09 10:56:04 -0500

    Create test_es.sh

---
+++ test_es.sh
@@ -0,0 +1,15 @@
+echo "\r\n \r\n  Stats \r\n \r\n"
+curl -XGET 'localhost:9200/_nodes/stats?pretty'
+#curl -XGET 'localhost:9200/_nodes/nodeId1,nodeId2/stats?pretty'
+
+
+echo "\r\n \r\n  Processes \r\n \r\n"
+curl 127.0.0.1:9200/_nodes/process?pretty
+
+echo "\r\n \r\n  Cluster Health \r\n \r\n"
+curl -XGET 'localhost:9200/_cat/health?v&pretty'
+
+echo "\r\n \r\n  Cluster Indices \r\n \r\n"
+curl -XGET 'localhost:9200/_cat/indices?v&pretty'
+
+echo "\r\n \r\n  Cluster Health \r\n \r\n"
