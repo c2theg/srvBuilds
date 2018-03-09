b422ed4071077f02954c98e5f50e74c68526afac
Chris Gray <christophermjgray@gmail.com>
2018-03-09 15:41:37 -0500

    Update test_es.sh

--- test_es.sh
+++ test_es.sh
@@ -8,13 +8,16 @@ curl -XGET 'localhost:9200/_nodes/stats?pretty'
 echo "\r\n \r\n  Processes \r\n \r\n"
 curl 127.0.0.1:9200/_nodes/process?pretty
 
-echo "\r\n \r\n  Cluster Health \r\n \r\n"
-curl -XGET 'localhost:9200/_cat/health?v&pretty'
-
 echo "\r\n \r\n  Cluster Templates' \r\n \r\n"
 curl -XGET 'localhost:9200/_template/*?pretty'
 
+echo "\r\n \r\n  Cluster cat Health \r\n \r\n"
+curl -XGET 'localhost:9200/_cat/health?v&pretty'
+
 echo "\r\n \r\n  Cluster Indices \r\n \r\n"
 curl -XGET 'localhost:9200/_cat/indices?v&pretty'
+
+echo "\r\n \r\n  Cluster Health \r\n \r\n"
+curl -XGET http://localhost:9200/_cluster/health?pretty
 
 echo "\r\n \r\n DONE \r\n \r\n \r\n"
