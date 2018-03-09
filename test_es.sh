6055a8a977a8feb37137af7d7edaa14c51a83a6a
Chris Gray <christophermjgray@gmail.com>
2018-03-09 10:57:07 -0500

    Update test_es.sh

--- test_es.sh
+++ test_es.sh
@@ -12,4 +12,4 @@ curl -XGET 'localhost:9200/_cat/health?v&pretty'
 echo "\r\n \r\n  Cluster Indices \r\n \r\n"
 curl -XGET 'localhost:9200/_cat/indices?v&pretty'
 
-echo "\r\n \r\n  Cluster Health \r\n \r\n"
+echo "\r\n \r\n DONE \r\n \r\n \r\n"
