26485130d345c828fc1078cdbe7447c27d787402
Chris Gray <christophermjgray@gmail.com>
2018-03-09 15:54:02 -0500

    Update test_es.sh

--- test_es.sh
+++ test_es.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# - Christopher Gray - version 0.7 - 3/9/18
+# - Christopher Gray - version 0.7.1 - 3/9/18
 echo "\r\n \r\n "
 #netstat -a -n | grep tcp | grep 9200
 ps -ef | grep elasticsearch
@@ -25,5 +25,9 @@ curl -XGET 'localhost:9200/_cat/indices?v&pretty'
 
 echo "\r\n \r\n  Cluster Health \r\n \r\n"
 curl -XGET http://localhost:9200/_cluster/health?pretty
+
+echo "\r\n \r\n  Cluster Health - Shards \r\n \r\n"
+curl -XGET http://localhost:9200/_cat/shards
+
 
 echo "\r\n \r\n DONE \r\n \r\n \r\n"
