ce93b94cd0f53b205cc6657ad7fb1e31632759c0
Chris Gray <christophermjgray@gmail.com>
2018-03-12 13:31:09 -0400

    Update install_kibana6.sh

--- install_kibana6.sh
+++ install_kibana6.sh
@@ -18,8 +18,8 @@ echo "
 |_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
                             |_|                                             |___|
 \r\n \r\n
-Version:  0.1.2                            \r\n
-Last Updated:  12/30/2017
+Version:  0.1.3                            \r\n
+Last Updated:  3/12/2018
 \r\n \r\n"
 
 echo " Source: https://www.elastic.co/guide/en/kibana/current/deb.html \r\n \r\n "
@@ -54,4 +54,11 @@ sudo /etc/init.d/kibana restart
 
 sudo update-rc.d kibana defaults 95 10
 
+
 echo "DONE! \r\n \r\n Point your browser to:  http://localhost:5601  to view it  \r\n \r\n "
+
+echo " -- Secure Kibana behind Nginx Reverse proxy \r\n \r\n
+ echo "admin:$(openssl passwd -apr1 YourStrongPassword)" | sudo tee -a /etc/nginx/htpasswd.kibana
+
+check out the site here: https://www.rosehosting.com/blog/install-and-configure-the-elk-stack-on-ubuntu-16-04/  \r\n \r\n
+"
