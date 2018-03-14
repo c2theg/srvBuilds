f1c3ec11c44227c6faea10582c3141744667ec9e
Chris Gray <christophermjgray@gmail.com>
2018-03-14 09:13:38 -0400

    Update test_es.sh

--- test_es.sh
+++ test_es.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# - Christopher Gray - version 0.7.3 - 3/12/18
+# - Christopher Gray - version 0.7.4 - 3/14/18
 echo "\r\n \r\n "
 #netstat -a -n | grep tcp | grep 9200
 #ps -ef | grep elasticsearch
@@ -27,7 +27,10 @@ curl -XGET http://localhost:9200/_cluster/health?pretty
 echo "\r\n \r\n  Cluster Health - Shards \r\n \r\n"
 curl -XGET http://localhost:9200/_cat/shards
 
-echo "\r\n \r\n  Cluster Allocation \r\n \r\n"
+echo "\r\n \r\n  Cluster Allocation Details \r\n \r\n"
 curl -H 'Content-Type: application/json' -X GET 'localhost:9200/_cluster/allocation/explain?pretty'
+
+echo "\r\n \r\n  Cluster Allocation brief \r\n \r\n"
+curl -s 'localhost:9200/_cat/allocation?v'
 
 echo "\r\n \r\n DONE \r\n \r\n \r\n"
