e293297039bea0e9b51ea1cbec3f4a939d26f902
Chris Gray <christophermjgray@gmail.com>
2018-03-16 00:12:51 -0400

    Update install_beats.sh

--- install_beats.sh
+++ install_beats.sh
@@ -6,7 +6,7 @@
 #       chmod u+x <FILE>
 #
 
-version=6.1.1
+version=6.2.2
 
 clear
 echo "
@@ -29,8 +29,8 @@ https://raw.githubusercontent.com/c2theg/srvBuilds/master/install_beats.sh
 
 This really is meant to be run under Ubuntu 14.04 - 16.04 LTS +
 \r\n \r\n
-Version:  0.0.6                             \r\n
-Last Updated:  3/14/2018
+Version:  0.0.7                             \r\n
+Last Updated:  3/16/2018
 \r\n \r\n"
 
 
@@ -59,12 +59,19 @@ sudo /bin/systemctl stop kibana.service
 sudo /bin/systemctl start kibana.service
 
 
-echo -e "Installing Metricbeat \r\n https://www.elastic.co/guide/en/beats/metricbeat/current/metricbeat-getting-started.html \r\n \r\n"
+echo -e "Installing MetricBeat \r\n https://www.elastic.co/guide/en/beats/metricbeat/current/metricbeat-getting-started.html \r\n \r\n"
 curl -L -O https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-$version-amd64.deb
 sudo dpkg -i metricbeat-$version-amd64.deb
 
-#echo -e "Installing HeartBeat \r\n hhttps://www.elastic.co/guide/en/beats/heartbeat/current/heartbeat-installation.html \r\n \r\n"
-#curl -L -O https://artifacts.elastic.co/downloads/beats/heartbeat/heartbeat-$version-amd64.deb
-#sudo dpkg -i heartbeat-$version-amd64.deb
+echo -e "Installing HeartBeat \r\n hhttps://www.elastic.co/guide/en/beats/heartbeat/current/heartbeat-installation.html \r\n \r\n"
+curl -L -O https://artifacts.elastic.co/downloads/beats/heartbeat/heartbeat-$version-amd64.deb
+sudo dpkg -i heartbeat-$version-amd64.deb
+
+
+echo -e "Installing AuditBeat \r\n https://www.elastic.co/guide/en/beats/metricbeat/current/auditbeat-getting-started.html \r\n \r\n"
+curl -L -O https://artifacts.elastic.co/downloads/beats/auditbeat/auditbeat-$version-amd64.deb
+sudo dpkg -i auditbeat-$version-amd64.deb
+
+
 
 echo "DONE! \r\n \r\n"
