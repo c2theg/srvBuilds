c09a13d7b0ae02b58d5f468f828e3d56c0bdd410
Chris Gray <christophermjgray@gmail.com>
2018-03-23 20:43:27 -0400

    Update test_es.sh

--- test_es.sh
+++ test_es.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
-# - Christopher Gray - version 0.7.4 - 3/14/18
+# - Christopher Gray - version 0.7.5 - 3/23/18
 echo "\r\n \r\n "
-#netstat -a -n | grep tcp | grep 9200
-#ps -ef | grep elasticsearch
-
-netstat -tulnp
 #----------------------------------------------------------------------
 echo "\r\n \r\n  Processes \r\n \r\n"
 curl 127.0.0.1:9200/_nodes/process?pretty
@@ -32,5 +28,12 @@ curl -H 'Content-Type: application/json' -X GET 'localhost:9200/_cluster/allocat
 
 echo "\r\n \r\n  Cluster Allocation brief \r\n \r\n"
 curl -s 'localhost:9200/_cat/allocation?v'
+
+#----------------------------------------------------------------------
+
+#netstat -a -n | grep tcp | grep 9200
+#ps -ef | grep elasticsearch
+
+netstat -tulnp
 
 echo "\r\n \r\n DONE \r\n \r\n \r\n"
