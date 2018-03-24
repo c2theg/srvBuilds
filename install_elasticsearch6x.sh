681eb5e48320a88a992cc1e04cbac724c9ae23da
Chris Gray <christophermjgray@gmail.com>
2018-03-24 14:13:20 -0400

    Update install_elasticsearch6x.sh

--- install_elasticsearch6x.sh
+++ install_elasticsearch6x.sh
@@ -40,8 +40,10 @@ echo -e "Installing Elastic Search 6.X \r\n \r\n"
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 
 sudo apt-get install -y apt-transport-https
-
-echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
+if [ -s "/etc/apt/sources.list.d/elastic-6.x.list" ]
+then
+	echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
+fi
 
 sudo apt-get update
 
