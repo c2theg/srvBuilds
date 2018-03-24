c2f461c5afce483b1cc3fa0a8556612843a55682
Chris Gray <christophermjgray@gmail.com>
2018-03-24 14:17:24 -0400

    Create install_syslogng.sh

---
+++ install_syslogng.sh
@@ -0,0 +1,3 @@
+
+sudo -E apt-get install -y syslog-ng-core syslog-ng-mod-sql syslog-ng-mod-json syslog-ng-mod-geoip
+#sudo -E apt-get install -y syslog-ng-mod-mongodb syslog-ng-mod-smtp syslog-ng-mod-amqp syslog-ng-mod-redis syslog-ng-mod-stomp libdbd-mysql libdbd-pgsql libdbd-sqlite3
