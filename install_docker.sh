201bd5bfcd78e783ee82e5a579d5bac304a4dd37
Chris Gray <christophermjgray@gmail.com>
2018-04-02 16:46:37 -0400

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -19,7 +19,7 @@ echo "
                             |_|                                             |___|
 
 \r\n \r\n
-Version:  1.4.2                             \r\n
+Version:  1.4.3                             \r\n
 Last Updated:  4/2/2018
 \r\n \r\n
 Updating system first..."
@@ -72,14 +72,12 @@ wait
 sudo -E apt-get -y install apt-transport-https ca-certificates curl software-properties-common
 wait
 
-if [ $VER = '14.04' ]
-then
+if [ $VER = '14.04' ]; then
     #-------- Ubuntu 14.04 ------------------------
     sudo -E apt-get -y install docker.io
     wait
     apt-cache madison docker-ce
-elif [ $VER = '16.04' ]
-then
+elif [ $VER = '16.04' ] || [ $VER = '18.04' ]; then
     #-------- Ubuntu 16.04 ------------------------
     sudo apt-get update
     sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
