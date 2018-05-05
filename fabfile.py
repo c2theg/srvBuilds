5a9016c1ece2feaf77fccc6f70de8d46517791e4
Chris Gray <christophermjgray@gmail.com>
2018-05-05 16:22:06 -0400

    Update fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -10,3 +10,22 @@ env.password = 'Secr3t!'
 @parallel
 def cmd(command):
 	sudo(command)
+
+
+'''   How to use
+Enter the commands as follows:
+
+ubuntu@host:$  fab cmd:"echo pi:Secret! | chpasswd"
+
+fab cmd:"apt update"
+
+fab cmd:"apt-get upgrade -y"
+fab cmd:"apt dist-upgrade -y"
+
+
+-- for Raspberry Pi--
+
+fab cmd:"raspi-config --expand-rootfs"
+fab cmd:"reboot now"
+
+'''
