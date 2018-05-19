9df744d89e752ebac587ba6cb39fd6f1c48dc35f
Chris Gray <christophermjgray@gmail.com>
2018-05-19 12:45:44 -0400

    Create install_python2-min.sh

---
+++ install_python2-min.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+#    If you update this from Windows, using Notepad ++, do the following:
+#       sudo apt-get -y install dos2unix
+#       dos2unix <FILE>
+#       chmod u+x <FILE>
+#
+clear
+echo "
+ _____             _         _    _          _                                   
+|     |___ ___ ___| |_ ___ _| |  | |_ _ _   |_|                                  
+|   --|  _| -_| .'|  _| -_| . |  | . | | |   _                                   
+|_____|_| |___|__,|_| |___|___|  |___|_  |  |_|                                  
+                                     |___|                                       
+                                                                                 
+ _____ _       _     _           _              _____    __    _____             
+|     | |_ ___|_|___| |_ ___ ___| |_ ___ ___   |     |__|  |  |   __|___ ___ _ _ 
+|   --|   |  _| |_ -|  _| . | . |   | -_|  _|  | | | |  |  |  |  |  |  _| .'| | |
+|_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
+                            |_|                                             |___|
+
+\r\n \r\n
+Version:  0.1                             \r\n
+Last Updated:  5/19/2018
+\r\n \r\n
+Updating system first..."
+sudo -E apt-get update
+wait
+sudo -E apt-get upgrade -y
+wait
+echo "Downloading required dependencies...\r\n\r\n"
+#--------------------------------------------------------------------------------------------
+echo "Installing Python 2.7 latest.... "
+# -- add newer python 2.7.x repo --
+sudo -E add-apt-repository -y ppa:fkrull/deadsnakes-python2.7
+wait
+sudo -E apt-get update
+wait
+sudo -E apt-get install -y python-minimal -no-install-recommends python-pip
+wait
+#---- install python dependancies ----
+sudo -E apt install -y python-pip
+sudo pip install --upgrade virtualenv
+echo "DONE installing Python 2.7 min \r\n \r\n "
