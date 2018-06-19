115b1bc0fce668f85f3fadf2bb2ca185a75da783
Chris Gray <christophermjgray@gmail.com>
2018-06-19 12:59:53 -0400

    Update get_temps_basic.py

--- raspi/get_temps_basic.py
+++ raspi/get_temps_basic.py
@@ -2,14 +2,15 @@
 # -*- coding: utf-8 -*-
 # Original code from: https://tutorials-raspberrypi.com/raspberry-pi-measure-humidity-temperature-dht11-dht22/
 # Christopher Gray - v0.0.1 - 6/19/2018
-import Adafruit_DHT
 from datetime import datetime
+import Adafruit_DHT
 #-----------------------------
 sensor = Adafruit_DHT.DHT22
 pin = 4
-humidity, Celsius = Adafruit_DHT.read_retry(sensor, pin)
+Humidity, Celsius = Adafruit_DHT.read_retry(sensor, pin)
 Fahrenheit = round((9.0/5.0 * Celsius + 32), 2)
-humidity = round(humidity, 2)
+Celsius = round(Celsius, 2)
+Humidity = round(Humidity, 2)
 #-----------------------------
-var_Human_Output = "temperature: " + str(Fahrenheit) + "°F, Humidity: " + str(humidity) + "% at: " + datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+var_Human_Output = "Temperature: " + str(Fahrenheit) + "°F / " + str(Celsius) + "°C, Humidity: " + str(Humidity) + "% at: " + datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 print str(var_Human_Output)
