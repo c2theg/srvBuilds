14b973181fdb5679dce93945279dd6b978ca64d6
Chris Gray <christophermjgray@gmail.com>
2018-06-19 14:42:34 -0400

    Update mqtt_sender.py

--- raspi/mqtt_sender.py
+++ raspi/mqtt_sender.py
@@ -2,11 +2,11 @@
 # -*- coding: utf-8 -*-
 # Original code from: https://tutorials-raspberrypi.com/raspberry-pi-mqtt-broker-client-wireless-communication/
 # Christopher Gray - v0.0.1 - 6/19/2018
-
 import paho.mqtt.publish as publish
- 
+#---------------------------- 
 MQTT_SERVER = "192.168.1.10"
 MQTT_PATH = "test_channel"
- 
+#----------------------------
+print("sending message to " + MQTT_SERVER)
 publish.single(MQTT_PATH, "Hello World!", hostname=MQTT_SERVER)
 print("done")
