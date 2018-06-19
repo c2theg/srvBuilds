6ea66349f08c76a8eb52641ff770019cd76c93cd
Chris Gray <christophermjgray@gmail.com>
2018-06-19 12:51:45 -0400

    Create get_temps_mqtt.py

---
+++ raspi/get_temps_mqtt.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Original code from: https://tutorials-raspberrypi.com/raspberry-pi-measure-humidity-temperature-dht11-dht22/
+# Christopher Gray - v0.0.1 - 6/19/2018
+import sys, os
+from datetime import datetime
+import Adafruit_DHT
+#-----------------------------
+sensor = Adafruit_DHT.DHT22
+pin = 4
+humidity, Celsius = Adafruit_DHT.read_retry(sensor, pin)
+Fahrenheit = round((9.0/5.0 * Celsius + 32), 2)
+humidity = round(humidity, 2)
+#-----------------------------
+var_Human_Output = "temperature: " + str(Fahrenheit) + "°F, Humidity: " + str(humidity) + "% at: " + datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+print str(var_Human_Output)
