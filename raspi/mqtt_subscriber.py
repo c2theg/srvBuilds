584e8210a4b455ad66f71b2667cdaf84b1739a58
Chris Gray <christophermjgray@gmail.com>
2018-06-18 22:46:11 -0400

    Create mqtt_subscriber.py

---
+++ raspi/mqtt_subscriber.py
@@ -0,0 +1,32 @@
+import paho.mqtt.client as mqtt
+#
+# Original code: https://tutorials-raspberrypi.com/raspberry-pi-mqtt-broker-client-wireless-communication/
+#
+
+MQTT_SERVER = "localhost"
+MQTT_PATH = "test_channel"
+ 
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+ 
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe(MQTT_PATH)
+ 
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+    # more callbacks, etc
+ 
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+ 
+client.connect(MQTT_SERVER, 1883, 60)
+ 
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+client.loop_forever()
