fddaabb1c98d75106026223dc03221f879d1e384
Chris Gray <christophermjgray@gmail.com>
2018-06-18 22:54:38 -0400

    Update install_raspi_mqtt.sh

--- raspi/install_raspi_mqtt.sh
+++ raspi/install_raspi_mqtt.sh
@@ -18,7 +18,7 @@ echo "
 |_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
                             |_|                                             |___|
 \r\n \r\n
-Version:  0.0.1                             \r\n
+Version:  0.0.2                             \r\n
 Last Updated:  6/18/2018
 \r\n \r\n
 Updating system first..."
@@ -30,16 +30,25 @@ echo "Downloading required dependencies...\r\n\r\n"
 #--------------------------------------------------------------------------------------------
 sudo apt-get install -y mosquitto mosquitto-clients
 
+# for Python
+sudo pip install paho-mqtt
+#sudo pip3 install paho-mqtt
+
 #-------------------- info ----------------------------------------------------
 # https://tutorials-raspberrypi.com/raspberry-pi-mqtt-broker-client-wireless-communication/
 
 #--------------------------------------------------------------------------------------------
+wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/raspi/mqtt_sender.py
+wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/raspi/mqtt_subscriber.py
 wait
 wait
 
 mosquitto_sub -h 192.168.1.10 -v -t test_channel
-
+wait
 mosquitto_pub -h 192.168.1.10 -t test_channel -m "Hello MQTT -> from Raspberry Pi"
 
 #--------------------------------------------------------------------------------------------
+#sudo python mqtt_subscriber.py
+sudo python mqtt_sender.py
+
 echo " Done! \r\n\r\n"
