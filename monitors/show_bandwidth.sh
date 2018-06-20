2aef1afddf9da5244821f3dd34b5f967eb268fff
Chris Gray <christophermjgray@gmail.com>
2018-06-20 02:24:30 -0400

    Create show_bandwidth.sh

---
+++ monitors/show_bandwidth.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+INTERVAL="1"  # update interval in seconds
+
+if [ -z "$1" ]; then
+        echo
+        echo usage: $0 [network-interface]
+        echo
+        echo e.g. $0 eth0
+        echo
+        echo shows packets-per-second
+        exit
+fi
+
+IF=$1
+
+while true
+do
+        R1=`cat /sys/class/net/$1/statistics/rx_packets`
+        T1=`cat /sys/class/net/$1/statistics/tx_packets`
+        sleep $INTERVAL
+        R2=`cat /sys/class/net/$1/statistics/rx_packets`
+        T2=`cat /sys/class/net/$1/statistics/tx_packets`
+        #-------------------------------------------------
+
+        TBPS=`expr $T2 - $T1`
+        RBPS=`expr $R2 - $R1`
+        TKBPS=`expr $TBPS / 1024`
+        RKBPS=`expr $RBPS / 1024`
+        #-------------------------
+        TXPPS=`expr $T2 - $T1`
+        RXPPS=`expr $R2 - $R1`
+
+        echo "$1, TX $TKBPS kB/s / $TXPPS pkts/s  -  RX $RKBPS kB/s / $RXPPS pkts/s"
+done
