f94cd2c5b64c28176f0ce61453dcb09d4c8f2c43
Chris Gray <christophermjgray@gmail.com>
2018-06-26 10:21:38 -0400

    Update install_nodejs.sh

--- install_nodejs.sh
+++ install_nodejs.sh
@@ -19,8 +19,8 @@ echo "
                             |_|                                             |___|
 
 \r\n \r\n
-Version:  1.4                             \r\n
-Last Updated:  2/25/2018
+Version:  1.5                             \r\n
+Last Updated:  6/26/2018
 \r\n \r\n
 Updating system first..."
 sudo -E apt-get update
@@ -29,44 +29,46 @@ sudo -E apt-get upgrade -y
 wait
 echo "Downloading required dependencies...\r\n\r\n"
 #--------------------------------------------------------------------------------------------
-sudo apt-get install -y build-essential libssl-dev
+sudo -E apt-get install -y build-essential libssl-dev
 
 cd ~
 
+# https://nodejs.org/en/download/package-manager/#debian-and-ubuntu-based-linux-distributions
+
+#--- OLD versions -----
 #OR V6
 #curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 #OR V7
 #curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
-#OR V8
+
+#-------- Current ----------
+#V8 - LTS
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-#OR V9
-#curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
+# V10
+#curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+
+#--------------------------------------------------------------------------------------------
 
 wait
 sudo apt-get install -y nodejs
-#--------------------------------------------------------------------------------------------
 
-
-echo "\r\n \r\n Installing NPM \r\n "
-
+#echo "\r\n \r\n Installing NPM \r\n "
 #sudo apt-get install -y npm
-
-curl -sL https://deb.nodesource.com/setup_8.x -o nodesource_setup.sh
-wait
-sudo bash nodesource_setup.sh
-
-wait
+#curl -sL https://deb.nodesource.com/setup_8.x -o nodesource_setup.sh
+#wait
+#sudo bash nodesource_setup.sh
+#wait
 
 sudo npm update npm -g
 
-#echo "Installing NPM modules... \r\n "
-
-#sudo npm install ws --ws:native
-#sudo npm install supervisor -g
-#sudo npm install express connect request emailjs mysql memcache colors md5 forever redis cluster
-#sudo npm install socket.io socket.io-redis socket.io-adapter socket.io-emitter socket.io-parser
-#sudo npm install socket.io --save
-
+echo "Install popular NPM modules... \r\n "
+echo " 
+sudo npm install ws --ws:native \r\n \r\n
+sudo npm install supervisor -g  \r\n \r\n
+sudo npm install express connect request emailjs mysql memcache colors md5 forever redis cluster  \r\n \r\n
+sudo npm install socket.io socket.io-redis socket.io-adapter socket.io-emitter socket.io-parser  \r\n \r\n
+sudo npm install socket.io --save  \r\n \r\n
+"
 
 wait
 echo "Done installing Node.JS and NPM \r\n \r\n"
