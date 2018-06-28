6f25f5183be09b3d935dc2b908dbeb97318097e1
Chris Gray <christophermjgray@gmail.com>
2018-06-27 23:47:15 -0400

    Create install_mqtt.sh

---
+++ install_mqtt.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+#    If you update this from Windows, using Notepad ++, do the following:
+#       sudo apt-get -y install dos2unix
+#       dos2unix <FILE>
+#       chmod u+x <FILE>
+#
+clear
+echo "
+ _____             _         _    _          _                                   
+|     |___ ___ ___| |_ ___ _| |  | |_ _ _   |_|                                  
+|   --|  _| -_| .'|  _| -_| . |  | . | | |   _                                   
+|_____|_| |___|__,|_| |___|___|  |___|_  |  |_|                                  
+                                     |___|                                       
+                                                                                 
+ _____ _       _     _           _              _____    __    _____             
+|     | |_ ___|_|___| |_ ___ ___| |_ ___ ___   |     |__|  |  |   __|___ ___ _ _ 
+|   --|   |  _| |_ -|  _| . | . |   | -_|  _|  | | | |  |  |  |  |  |  _| .'| | |
+|_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
+                            |_|                                             |___|
+\r\n \r\n
+Version:  0.0.3                             \r\n
+Last Updated:  6/27/2018
+\r\n \r\n
+Updating system first..."
+sudo -E apt-get update
+wait
+sudo -E apt-get upgrade -y
+wait
+echo "Downloading required dependencies...\r\n\r\n"
+#--------------------------------------------------------------------------------------------
+sudo apt-get install -y mosquitto mosquitto-clients
+
+# for Python
+sudo pip install paho-mqtt
+#sudo pip3 install paho-mqtt
+
+#-------------------- info ----------------------------------------------------
+# https://tutorials-raspberrypi.com/raspberry-pi-mqtt-broker-client-wireless-communication/
+
+
+echo " Done! \r\n\r\n"
