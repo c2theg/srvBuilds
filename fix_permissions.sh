2989b155c79c9d6740f844e36919f7d666b869fb
Chris Gray <christophermjgray@gmail.com>
2018-07-01 14:59:59 -0400

    Update fix_permissions.sh

--- fix_permissions.sh
+++ fix_permissions.sh
@@ -1,23 +1,45 @@
 #!/bin/bash
-ocpath='/var/www/nextcloud'
+
 htuser='www-data'
 htgroup='www-data'
 rootuser='root'
+Web_path='/var/www'
 
-find ${ocpath}/ -type f -print0 | xargs -0 chmod 0640
-find ${ocpath}/ -type d -print0 | xargs -0 chmod 0750
+chmod 0644 ${Web_path}/.htaccess
+chmod 0644 ${Web_path}/data/.htaccess
 
-chown -R ${rootuser}:${htgroup} ${ocpath}/
-chown -R ${htuser}:${htgroup} ${ocpath}/apps/
-chown -R ${htuser}:${htgroup} ${ocpath}/config/
-chown -R ${htuser}:${htgroup} ${ocpath}/data/
-chown -R ${htuser}:${htgroup} ${ocpath}/themes/
+sudo chmod -R 755 ${Web_path}
+sudo chown -R www-data:www-data ${Web_path}
+chown -R ${rootuser}:${htgroup} ${Web_path}/
+chown -R 'ubuntu':${htgroup} ${Web_path}/
+   
+if [ -d "/var/www/nextcloud" ]
+then
+    ocpath='/var/www/nextcloud'
+    find ${ocpath}/ -type f -print0 | xargs -0 chmod 0640
+    find ${ocpath}/ -type d -print0 | xargs -0 chmod 0750
 
-chown ${rootuser}:${htgroup} ${ocpath}/.htaccess
-chown ${rootuser}:${htgroup} ${ocpath}/data/.htaccess
+    chown -R ${rootuser}:${htgroup} ${ocpath}/
+    chown -R ${htuser}:${htgroup} ${ocpath}/apps/
+    chown -R ${htuser}:${htgroup} ${ocpath}/config/
+    chown -R ${htuser}:${htgroup} ${ocpath}/data/
+    chown -R ${htuser}:${htgroup} ${ocpath}/themes/
 
-chmod 0644 ${ocpath}/.htaccess
-chmod 0644 ${ocpath}/data/.htaccess
+    chown ${rootuser}:${htgroup} ${ocpath}/.htaccess
+    chown ${rootuser}:${htgroup} ${ocpath}/data/.htaccess
 
-sudo chmod -R 755 ${ocpath}
-sudo chown -R www-data:www-data ${ocpath}
+    chmod 0644 ${ocpath}/.htaccess
+    chmod 0644 ${ocpath}/data/.htaccess
+
+    sudo chmod -R 755 ${ocpath}
+    sudo chown -R www-data:www-data ${ocpath}
+fi
+
+#--- General commands -----
+echo "\r\n \r\n"
+echo "All users on the system: \r\n "
+#awk -F: '{ print $1 }' /etc/passwd
+cat /etc/passwd
+
+echo "All Groups on the system: \r\n "
+cut -d: -f1 /etc/group | sort
