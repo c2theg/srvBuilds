d071d82f47ffafbe2c80fa3860ca3778deb8883d
Chris Gray <christophermjgray@gmail.com>
2018-07-01 14:26:05 -0400

    Update install_monitoring.sh

--- install_monitoring.sh
+++ install_monitoring.sh
@@ -30,11 +30,47 @@ wait
 echo "Downloading required dependencies...\r\n\r\n"
 #--------------------------------------------------------------------------------------------
 
+echo "Downloading files "
+wait
+wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/install_webmin.sh
+chmod u+x install_webmin.sh
+sudo -e ./install_webmin.sh
 
-
-
-
-
-
-
-
+#------------- OS Version Detection -------------
+if [ -f /etc/os-release ]; then
+	    # freedesktop.org and systemd
+	    . /etc/os-release
+	    OS=$NAME
+	    VER=$VERSION_ID
+elif type lsb_release >/dev/null 2>&1; then
+	    # linuxbase.org
+	    OS=$(lsb_release -si)
+	    VER=$(lsb_release -sr)
+elif [ -f /etc/lsb-release ]; then
+	    # For some versions of Debian/Ubuntu without lsb_release command
+	    . /etc/lsb-release
+	    OS=$DISTRIB_ID
+	    VER=$DISTRIB_RELEASE
+elif [ -f /etc/debian_version ]; then
+	    # Older Debian/Ubuntu/etc.
+	    OS=Debian
+	    VER=$(cat /etc/debian_version)
+elif [ -f /etc/SuSe-release ]; then
+	    # Older SuSE/etc.
+	    ...
+elif [ -f /etc/redhat-release ]; then
+	    # Older Red Hat, CentOS, etc.
+	    ...
+else
+	    # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
+	    OS=$(uname -s)
+	    VER=$(uname -r)
+fi
+echo " Detected OS: $OS, Version: $VER \r\n \r\n"
+#-----------------------------------------------
+if [ $VER = '16.04' ] || [ $VER = '16.10' ] || [ $VER = '17.04' ] || [ $VER = '18.04' ]; then
+	    echo "\r\n\r\n \r\n Add Cockpit! (Only for Ubuntu 16.04+) \r\n \r\n"	
+	    sudo add-apt-repository -y ppa:cockpit-project/cockpit && sudo -E apt-get install -y cockpit
+	    sudo systemctl start cockpit && sudo systemctl enable cockpit
+fi
+#-----------------------
