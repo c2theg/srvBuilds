ca9633b6084187e5708e0b6aa6333a3073409bee
Chris Gray <christophermjgray@gmail.com>
2018-07-03 19:07:08 -0400

    Update get_temps_ds18b20.py

--- raspi/get_temps_ds18b20.py
+++ raspi/get_temps_ds18b20.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import os
-import glob
-import time
- 
+# Source: https://pimylifeup.com/raspberry-pi-temperature-sensor/
+import os, glob, time
+
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
- 
+
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
- 
+
 def read_temp_raw():
     f = open(device_file, 'r')
     lines = f.readlines()
     f.close()
     return lines
- 
+
 def read_temp():
     lines = read_temp_raw()
     while lines[0].strip()[-3:] != 'YES':
@@ -25,10 +24,10 @@ def read_temp():
     equals_pos = lines[1].find('t=')
     if equals_pos != -1:
         temp_string = lines[1][equals_pos+2:]
-        temp_c = float(temp_string) / 1000.0
-        temp_f = temp_c * 9.0 / 5.0 + 32.0
+        temp_c = round(float(temp_string) / 1000.0,2)
+        temp_f = round(temp_c * 9.0 / 5.0 + 32.0, 2)
         return temp_c, temp_f
-        
+
 while True:
         print(read_temp())
         time.sleep(1)
