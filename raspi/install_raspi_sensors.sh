1fc5f695fed7117baa40e125076b0973863d6218
Chris Gray <christophermjgray@gmail.com>
2018-07-04 01:23:47 -0400

    Update install_raspi_sensors.sh

--- raspi/install_raspi_sensors.sh
+++ raspi/install_raspi_sensors.sh
@@ -18,8 +18,8 @@ echo "
 |_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
                             |_|                                             |___|
 \r\n \r\n
-Version:  0.0.1                             \r\n
-Last Updated:  6/18/2018
+Version:  0.0.3                             \r\n
+Last Updated:  7/4/2018
 \r\n \r\n
 Updating system first..."
 sudo -E apt-get update
@@ -44,8 +44,14 @@ echo " sudo ./AdafruitDHT.py 22 4"
 #git clone https://github.com/pimylifeup/temperature_sensor.git
 
 
-
-
+#----------------------------------------
+Cron_output=$(crontab -l | grep "startup_temp_sensors.sh")
+#echo "The output is: [ $Cron_output ]"
+if [ -z "$Cron_output" ]
+then
+    line="@reboot /root/startup_temp_sensors.sh >> /var/log/startup_temp_sensors.log 2>&1"
+    (crontab -u root -l; echo "$line" ) | crontab -u root -
+fi
 #--------------------------------------------------------------------------------------------
 echo "You will need to REBOOT the pi before using any sensors!!! \r\n "
 echo " Done! \r\n\r\n"
