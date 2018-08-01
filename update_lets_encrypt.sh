4400b1b7e43029cd9d5abdd20360a540991c7f04
Chris Gray <christophermjgray@gmail.com>
2018-08-01 14:51:32 -0400

    Update update_lets_encrypt.sh

--- update_lets_encrypt.sh
+++ update_lets_encrypt.sh
@@ -1,3 +1,5 @@
+# look at: https://www.onepagezen.com/letsencrypt-auto-renew-certbot-apache/#step1
+
 sudo ~/.local/share/letsencrypt/bin/letsencrypt certonly \
 --agree-tos --email someone@gmail.com \
 --webroot \
