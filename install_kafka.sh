0b8a1b951062f091647d0127f933691d5497cf60
Chris Gray <christophermjgray@gmail.com>
2018-08-09 01:15:20 -0400

    Update install_kafka.sh

--- install_kafka.sh
+++ install_kafka.sh
@@ -18,28 +18,41 @@ echo "
 |_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
                             |_|                                             |___|
 \r\n \r\n
-Version:  0.1.2                             \r\n
-Last Updated:  3/13/2018
+Version:  0.1.3                             \r\n
+Last Updated:  8/8/2018
 \r\n \r\n
-This is meant for Ubuntu 14.04+ \r\n \r\n
+This is meant for Ubuntu 16.04+ \r\n \r\n
 Updating system first..."
 sudo -E apt-get update
 wait
 sudo -E apt-get upgrade -y
 wait
-echo "Downloading required dependencies...\r\n\r\n"
+echo "Downloading required dependencies...  https://linuxhint.com/install-apache-kafka-ubuntu/ . \r\n\r\n"
 #--------------------------------------------------------------------------------------------
-sudo apt-get install -y zookeeperd
-
-echo "\r\n \r\n Downloading Kafka... \r\n \r\n"
-wget -O "kafka_current.tgz" "http://mirror.cc.columbia.edu/pub/software/apache/kafka/1.0.1/kafka_2.11-1.0.1.tgz"
+sudo -E apt-get install -y openjdk-8-jdk
 wait
-tar -xvf kafka_current.tgz
+
+sudo -E apt-get install -y zookeeperd
+wait
+sudo systemctl start zookeeper
+wait
+sudo systemctl status zookeeper
+wait
+#-- add to startup
+sudo systemctl enable zookeeper
+wait
+sudo -E apt-get install -y net-tools
 
 
+sudo mkdir kafka
+echo "\r\n \r\n Downloading Kafka... (https://www.apache.org/dyn/closer.cgi?path=/kafka/2.0.0/kafka_2.11-2.0.0.tgz)  \r\n \r\n"
+#wget -O "kafka_current.tgz" "http://mirror.cc.columbia.edu/pub/software/apache/kafka/1.0.1/kafka_2.11-1.0.1.tgz"
+wget -O "kafka_current.tgz" "https://www.apache.org/dyn/closer.cgi?path=/kafka/2.0.0/kafka_2.11-2.0.0.tgz"
+wait
+sudo tar xvzf kafka_current.tgz -C kafka/
 
-echo "\r\n \r\n You can now start Kafka with the following: \r\n \r\n
-  cd kafka_current/  \r\n \r\n
+
+echo "\r\n \r\n You can now start Kafka with the following: \r\n \r\n  \r\n \r\n
   sudo bin/kafka-server-start.sh config/server.properties
 
 \r\n \r\n"
