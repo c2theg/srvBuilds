0ea17c7863057e6d657b3e55d5104cee7e1c87ee
Chris Gray <christophermjgray@gmail.com>
2018-08-28 02:04:43 -0400

    Create update_blocklists_local_servers.sh

---
+++ update_blocklists_local_servers.sh
@@ -0,0 +1,27 @@
+
+
+#-- Sources: https://firebog.net/
+
+#-- https://hosts-file.net/?s=classifications
+# EMD - Sites engaged in malware distribution
+https://hosts-file.net/emd.txt
+
+# EXP - Sites engaged in the housing, development or distribution of exploits, including but not limited to exploitation of browser, software (inclusive of website software such as CMS), operating system exploits aswell as those engaged in exploits via social engineering.
+https://hosts-file.net/exp.txt
+
+# PSH - Sites engaged in Phishing
+https://hosts-file.net/psh.txt
+
+# GRM - Sites engaged in astroturfing (otherwise known as grass roots marketing) or spamming
+https://hosts-file.net/grm.txt
+
+# HJK - Sites engaged in browser hijacking or other forms of hijacking (OS services, bandwidth, DNS, etc.)
+https://hosts-file.net/hjk.txt
+
+# PUP - Sites engaged in the distribution of Potentially Unwanted Programs
+https://hosts-file.net/hjk.txt
+
+#-- other lists
+https://www.malwaredomainlist.com/hostslist/hosts.txt
+https://raw.githubusercontent.com/anudeepND/blacklist/master/adservers.txt
+
