3d7c217b0d6af500543b930d27069ffd78887a34
Chris Gray <christophermjgray@gmail.com>
2018-09-13 15:22:30 -0400

    Update pihole_allowlist.sh

--- raspi/pihole_allowlist.sh
+++ raspi/pihole_allowlist.sh
@@ -26,7 +26,7 @@ echo "
 This really is meant to be run under Ubuntu 16.04 LTS +
 
 \r\n \r\n
-Version:  0.0.4                            \r\n
+Version:  0.0.5                            \r\n
 Last Updated:  9/12/2018
 \r\n \r\n"
 #--------------------------
@@ -38,30 +38,21 @@ pihole -w s.youtube.com video-stats.l.google.com
 pihole -w connectivitycheck.android.com connectivitycheck.gstatic.com
 pihole -w ajax.googleapis.com
 
-#--- Microsoft - Windows uses this to verify connectivity to Internet
+#--- Microsoft
+# Windows uses this to verify connectivity to Internet
 pihole -w www.msftncsi.com
+# The rest
 pihole -w outlook.office365.com products.office.com c.s-microsoft.com i.s-microsoft.com login.live.com
-pihole -w clientconfig.passport.net 
-pihole -w v10.events.data.microsoft.com
+pihole -w clientconfig.passport.net officeclient.microsoft.com
+pihole -w v10.events.data.microsoft.com v10.vortex-win.data.microsoft.com settings-win.data.microsoft.com 
 pihole -w xbox.ipv6.microsoft.com device.auth.xboxlive.com www.msftncsi.com title.mgt.xboxlive.com xsts.auth.xboxlive.com title.auth.xboxlive.com ctldl.windowsupdate.com attestation.xboxlive.com xboxexperiencesprod.experimentation.xboxlive.com xflight.xboxlive.com cert.mgt.xboxlive.com xkms.xboxlive.com def-vef.xboxlive.com notify.xboxlive.com help.ui.xboxlive.com licensing.xboxlive.com eds.xboxlive.com www.xboxlive.com v10.vortex-win.data.microsoft.com settings-win.data.microsoft.com
 pihole -w s.gateway.messenger.live.com ui.skype.com pricelist.skype.com apps.skype.com m.hotmail.com s.gateway.messenger.live.com sa.symcb.com s{1..5}.symcb.com 
-pihole -w officeclient.microsoft.com
-pihole -w settings-win.data.microsoft.com
-pihole -w v10.vortex-win.data.microsoft.com
 
 #--- Facebook
 pihole -w creative.ak.fbcdn.net external-lhr0-1.xx.fbcdn.net external-lhr1-1.xx.fbcdn.net external-lhr10-1.xx.fbcdn.net external-lhr2-1.xx.fbcdn.net external-lhr3-1.xx.fbcdn.net external-lhr4-1.xx.fbcdn.net external-lhr5-1.xx.fbcdn.net external-lhr6-1.xx.fbcdn.net external-lhr7-1.xx.fbcdn.net external-lhr8-1.xx.fbcdn.net external-lhr9-1.xx.fbcdn.net fbcdn-creative-a.akamaihd.net scontent-lhr3-1.xx.fbcdn.net scontent.xx.fbcdn.net scontent.fgdl5-1.fna.fbcdn.net graph.facebook.com connect.facebook.com cdn.fbsbx.com
 
 #-- Plex
 pihole -w plex.tv tvdb2.plex.tv pubsub.plex.bz proxy.plex.bz proxy02.pop.ord.plex.bz cpms.spop10.ams.plex.bz meta-db-worker02.pop.ric.plex.bz meta.plex.bz tvthemes.plexapp.com.cdn.cloudflare.net tvthemes.plexapp.com 106c06cd218b007d-b1e8a1331f68446599e96a4b46a050f5.ams.plex.services meta.plex.tv cpms35.spop10.ams.plex.bz proxy.plex.tv metrics.plex.tv pubsub.plex.tv status.plex.tv www.plex.tv node.plexapp.com nine.plugins.plexapp.com staging.plex.tv app.plex.tv o1.email.plex.tv  o2.sg0.plex.tv dashboard.plex.tv
-
-#-- Others
-pihole -w gravatar.com
-pihole -w thetvdb.com
-pihole -w themoviedb.com
-pihole -w medium.com
-pihole -w highcharts.com
-pihole -w spclient.wg.spotify.com apresolve.spotify.com
 
 #-- Apple
 pihole -w itunes.apple.com appleid.apple.com
@@ -74,3 +65,11 @@ pihole -w app-analytics.snapchat.com sc-analytics.appspot.com cf-st.sc-cdn.net
 pihole -w weeklyad.target.com m.weeklyad.target.com weeklyad.target.com.edgesuite.net api.target.com redsky.target.com profile.target.com 
 pihole -w amazon.com fls-na.amazon.com
 
+#-- Others
+pihole -w gravatar.com
+pihole -w thetvdb.com
+pihole -w themoviedb.com
+pihole -w medium.com
+pihole -w highcharts.com www.highcharts.com code.highcharts.com
+pihole -w spclient.wg.spotify.com apresolve.spotify.com
+pihole -w cloudflare.com www.cloudflare.com cdnjs.cloudflare.com
