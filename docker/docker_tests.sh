1585cbd51379fb1f9cf61ee71c52d6fb4abb31b4
Chris Gray <christophermjgray@gmail.com>
2018-09-25 01:18:01 -0400

    Update docker_tests.sh

--- docker/docker_tests.sh
+++ docker/docker_tests.sh
@@ -4,9 +4,14 @@ echo "\r\n \r\n Docker Tests \r\n \r\n"
 echo "Docker version: (docker version) \r\n "
 docker version
 
+echo "Docker Info: (docker info) \r\n "
+docker info
+
 echo "Docker Images downloaded.. (docker images) \r\n "
 docker images
 
 echo "Docker Containers running: (docker ps) \r\n "
-docker images
+docker ps
 
+
+echo "\r\n \r\n"
