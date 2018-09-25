1ff7508db8b74471c062be28bcef009426160543
Chris Gray <christophermjgray@gmail.com>
2018-09-25 01:01:13 -0400

    Create docker_tests.sh

---
+++ docker/docker_tests.sh
@@ -0,0 +1,12 @@
+echo "\r\n \r\n Docker Tests \r\n \r\n"
+
+
+echo "Docker version: (docker version) \r\n "
+docker version
+
+echo "Docker Images downloaded.. (docker images) \r\n "
+docker images
+
+echo "Docker Containers running: (docker ps) \r\n "
+docker images
+
