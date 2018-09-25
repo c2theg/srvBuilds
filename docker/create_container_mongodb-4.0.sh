3868d213f5662a99fd522bb5a141c4226cc1feea
Chris Gray <christophermjgray@gmail.com>
2018-09-25 00:57:54 -0400

    Update create_container_mongodb-4.0.sh

--- docker/create_container_mongodb-4.0.sh
+++ docker/create_container_mongodb-4.0.sh
@@ -1,3 +1,20 @@
 # create directory structure
 
+if [ ! -d "/media/data" ]; then
+then
+  mkdir /media/data 
+fi
+
+if [ ! -d "/media/data/nosql" ]; then
+then
+  mkdir /media/data/nosql
+fi
+
+if [ ! -d "/media/data/nosql/mongo1" ]; then
+then
+  mkdir /media/data/nosql/mongo1
+fi
+
+echo "Starting container... \r\n \r\n "
+
 docker run -d -p 27017:27107 -v ~/data:/media/data/nosql/mongo1 mongo:4.0
