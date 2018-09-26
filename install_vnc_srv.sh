4a8e2114a8a93d39d76d062220564cac095be780
Chris Gray <christophermjgray@gmail.com>
2018-09-26 01:09:34 -0400

    Create install_vnc_srv.sh

---
+++ install_vnc_srv.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+#    If you update this from Windows, using Notepad ++, do the following:
+#       sudo apt-get -y install dos2unix
+#       dos2unix <FILE>
+#       chmod u+x <FILE>
+#
+
+clear
+echo "
+ _____             _         _    _          _                                   
+|     |___ ___ ___| |_ ___ _| |  | |_ _ _   |_|                                  
+|   --|  _| -_| .'|  _| -_| . |  | . | | |   _                                   
+|_____|_| |___|__,|_| |___|___|  |___|_  |  |_|                                  
+                                     |___|                                       
+                                                                                 
+ _____ _       _     _           _              _____    __    _____             
+|     | |_ ___|_|___| |_ ___ ___| |_ ___ ___   |     |__|  |  |   __|___ ___ _ _ 
+|   --|   |  _| |_ -|  _| . | . |   | -_|  _|  | | | |  |  |  |  |  |  _| .'| | |
+|_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
+                            |_|                                             |___|
+\r\n \r\n
+This really is meant to be run under Ubuntu 16.04 LTS +
+\r\n \r\n
+Version:  0.0.1                             \r\n
+Last Updated:  9/26/2018
+
+https://www.digitalocean.com/community/tutorials/how-to-install-and-configure-vnc-on-ubuntu-18-04
+
+\r\n \r\n"
+
+sudo apt install xfce4 xfce4-goodies
+sudo apt install tightvncserver
+vncserver
+
