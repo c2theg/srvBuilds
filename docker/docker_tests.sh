ca517078c5842e645d1e8184cb73994bb1568bdf
Chris Gray <christophermjgray@gmail.com>
2018-09-27 01:17:19 -0400

    Update docker_tests.sh

--- docker/docker_tests.sh
+++ docker/docker_tests.sh
@@ -7,7 +7,7 @@
 clear
 
 echo "\r\n \r\n Docker Tests \r\n \r\n"
-
+echo " Link: https://www.digitalocean.com/community/tutorials/how-to-remove-docker-images-containers-and-volumes  \r\n \r\n"
 
 echo "Docker version: (docker version) \r\n "
 docker version
@@ -15,11 +15,10 @@ docker version
 echo "\r\n Docker Info: (docker info) \r\n "
 docker info
 
-echo "\r\n Docker Images downloaded.. (docker images) \r\n "
-docker images
+echo "\r\n Docker Images downloaded.. (docker images -a) \r\n "
+docker images -a
 
-echo "\r\n Docker Containers running: (docker ps) \r\n "
-docker ps
-
+echo "\r\n Docker Containers running: (docker ps -a) \r\n "
+docker ps -a
 
 echo "\r\n \r\n"
