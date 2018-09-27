ee0c5e8ac1aa5fa22df965936bc0d2e4e6aca18b
Chris Gray <christophermjgray@gmail.com>
2018-09-27 17:01:12 -0400

    Update docker_volume.sh

--- docker/docker_volume.sh
+++ docker/docker_volume.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-docker volume create ./ContainerVolumes
+echo "\r\n Create volume 'ContainerVolumes' \r\n "
+docker volume create ContainerVolumes
+
+
+echo "\r\n Inspect the volume... \r\n \r\n"
+docker volume inspect ContainerVolumes
+
+
+echo "\r\n All Volumes... \r\n \r\n"
+docker volume ls
