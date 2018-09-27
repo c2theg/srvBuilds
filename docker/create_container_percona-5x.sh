7d6ad99506cadb0b1c4a8351ba6202c8b90b3e09
Chris Gray <christophermjgray@gmail.com>
2018-09-27 02:06:03 -0400

    Create create_container_percona-5x.sh

---
+++ docker/create_container_percona-5x.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#    If you update this from Windows, using Notepad ++, do the following:
+#       sudo apt-get -y install dos2unix
+#       dos2unix <FILE>
+#       chmod u+x <FILE>
+#
+clear
+
+# create directory structure
+
+if [ ! -d "/media/data" ]; then
+then
+  mkdir /media/data 
+fi
+
+if [ ! -d "/media/data/sql" ]; then
+then
+  mkdir /media/data/sql
+fi
+
+if [ ! -d "/media/data/sql/mysql" ]; then
+then
+  mkdir /media/data/sql/mysql
+fi
+
+echo "Starting container... \r\n \r\n "
+
+docker run -it --hostname MySQL --name=MySQL --net=bridge --expose=3306 -p 3306:3306 -v ~/data:/media/data/sql/mysql -e MYSQL_ROOT_PASSWORD=SecretP@ssw0rd!@# percona:5-stretch
+
+echo "\r\n \r\n DONE! \r\n \r\n"
+docker images
