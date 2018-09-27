dfe519e783f5034c767b88c3911ca9d501882db5
Chris Gray <christophermjgray@gmail.com>
2018-09-27 15:56:10 -0400

    Update create_container_percona-5x.sh

--- docker/create_container_percona-5x.sh
+++ docker/create_container_percona-5x.sh
@@ -1,31 +1,51 @@
 #!/bin/sh
-#    If you update this from Windows, using Notepad ++, do the following:
-#       sudo apt-get -y install dos2unix
-#       dos2unix <FILE>
-#       chmod u+x <FILE>
 #
+#   Christopher Gray
+#     Version 0.1.4
+#     Updated: 9/27/18
+#
+#   This file is meant for Mac OSX or Linux (Ubuntu / CentOS)
 clear
 
-# create directory structure
+echo "Checking LOCAL directory structure. (/ContainerVolumes/data/sql/mysql).. \r\n \r\n "
 
-if [ ! -d "/media/data" ]; then
+if [ ! -d "/ContainerVolumes" ];
 then
-  mkdir /media/data 
+  mkdir /ContainerVolumes
 fi
 
-if [ ! -d "/media/data/sql" ]; then
+if [ ! -d "/ContainerVolumes/data" ];
 then
-  mkdir /media/data/sql
+  mkdir /ContainerVolumes/data
 fi
 
-if [ ! -d "/media/data/sql/mysql" ]; then
+if [ ! -d "/ContainerVolumes/data/sql" ];
 then
-  mkdir /media/data/sql/mysql
+  mkdir /ContainerVolumes/data/sql
 fi
+
+if [ ! -d "/ContainerVolumes/data/sql/mysql" ];
+then
+  mkdir /ContainerVolumes/data/sql/mysql
+fi
+
+echo "\r\n Downloading config... \r\n \r\n"
+wget -O "/ContainerVolumes/data/sql/mysql/mysqld.cnf" "https://github.com/c2theg/srvBuilds/blob/master/configs/mysqld-container.cnf"
 
 echo "Starting container... \r\n \r\n "
 
-docker run -it --hostname MySQL --name=MySQL --net=bridge --expose=3306 -p 3306:3306 -v ~/data:/media/data/sql/mysql -e MYSQL_ROOT_PASSWORD=SecretP@ssw0rd!@# percona:5-stretch
+docker run -it \
+  --hostname MySQL \
+  --name=MySQL \
+  --net=bridge \
+  --expose=3306 \
+  -p 3306:3306 \
+  -v /ContainerVolumes/data/sql/mysql:/etc/mysql/conf.d \
+  -e MYSQL_ROOT_PASSWORD=SecretP@ssw0rd!@# \
+  percona:5-stretch \
+  --skip-symbolic-links --initialize-insecure --skip-name-resolve
+
 
 echo "\r\n \r\n DONE! \r\n \r\n"
 docker images
+docker ps
