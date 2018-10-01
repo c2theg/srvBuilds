0d682cad56dd4a36c495bc5c6ff7321246245373
Chris Gray <christophermjgray@gmail.com>
2018-10-01 11:55:45 -0400

    Create deploy_nginx-php.sh

---
+++ docker/deploy_nginx-php.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+#    If you update this from Windows, using Notepad ++, do the following:
+#       sudo apt-get -y install dos2unix
+#       dos2unix <FILE>
+#       chmod u+x <FILE>
+#
+clear
+now=$(date)
+echo "Running deploy_nginx-php.sh at $now 
+ _____             _         _    _          _                                   
+|     |___ ___ ___| |_ ___ _| |  | |_ _ _   |_|                                  
+|   --|  _| -_| .'|  _| -_| . |  | . | | |   _                                   
+|_____|_| |___|__,|_| |___|___|  |___|_  |  |_|                                  
+                                     |___|                                       
+                                                                                 
+ _____ _       _     _           _              _____    __    _____             
+|     | |_ ___|_|___| |_ ___ ___| |_ ___ ___   |     |__|  |  |   __|___ ___ _ _ 
+|   --|   |  _| |_ -|  _| . | . |   | -_|  _|  | | | |  |  |  |  |  |  _| .'| | |
+|_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
+                            |_|                                             |___|
+\r\n \r\n
+Version:  0.0.1                             \r\n
+Last Updated:  10/1/2018
+\r\n \r\n"
+wait
+#---------- Download config files ----------------
+#--- Nginx Configs
+wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/configs/container-nginx.conf
+wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/configs/site1.conf
+#--- PHP Config
+wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/configs/container-php-fpm.ini
+
+#--- Docker Deployment Config
+wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/docker/compose-configs/docker-compose-nginx_php.yml
+
+#--- App Code
+wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/configs/sysinfo.php
+#-----------------------------------------------------------------------------------------------------------------
+# create code dir and move code
+sudo mkdir code
+cp sysinfo.php ./code/index.php
+
+#--- rename config file to "docker-compose.yml"
+#mv docker-compose-nginx_php.yml docker-compose.yml
+#--- start up container
+#docker-compose up
+docker-compose -f docker-compose-nginx_php.yml -p Webapp-Nginx-PHP
+echo "Done! \r\n \r\n"
