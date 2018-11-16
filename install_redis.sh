bc8088780738e14899a9257667c9b47a5dd1f3d6
Chris Gray <christophermjgray@gmail.com>
2018-11-16 00:36:48 -0600

    Update install_redis.sh

--- install_redis.sh
+++ install_redis.sh
@@ -1,16 +1,48 @@
 #!/bin/sh
 #    If you update this from Windows, using Notepad ++, do the following:
-#       apt-get install dos2unix
-#       dos2unix install_redis.sh
-#       chmod u+x install_redis.sh
+#       sudo apt-get -y install dos2unix
+#       dos2unix <FILE>
+#       chmod u+x <FILE>
 #
 clear
+now=$(date)
+echo "Running update_ubuntu.sh at $now 
+
+ _____             _         _    _          _                                   
+|     |___ ___ ___| |_ ___ _| |  | |_ _ _   |_|                                  
+|   --|  _| -_| .'|  _| -_| . |  | . | | |   _                                   
+|_____|_| |___|__,|_| |___|___|  |___|_  |  |_|                                  
+                                     |___|                                       
+                                                                                 
+ _____ _       _     _           _              _____    __    _____             
+|     | |_ ___|_|___| |_ ___ ___| |_ ___ ___   |     |__|  |  |   __|___ ___ _ _ 
+|   --|   |  _| |_ -|  _| . | . |   | -_|  _|  | | | |  |  |  |  |  |  _| .'| | |
+|_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
+                            |_|                                             |___|
+
+\r\n \r\n
+Version:  0.0.2                             \r\n
+Last Updated:  11/16/2018
+\r\n \r\n"
+wait
+sudo -E apt-get update
+wait
+sudo -E apt-get upgrade -y --force-yes
+wait
+sudo -E apt-get install -f -y
+wait
+echo "Freeing up space"
+sudo apt-get autoremove -y
+wait
+echo "Downloading required dependencies...\r\n\r\n"
+#--------------------------------------------------------------------------------------------
+
 echo "This installs redis-server to your box..."
 sudo add-apt-repository -y ppa:chris-lea/redis-server
 wait
 sudo -E apt-get update
 wait
-sudo -E apt-get install -y tcl8.5
+sudo -E apt-get install -y build-essential tcl
 wait
 sudo -E apt-get install -y redis-server
 #pip install redis-trib
@@ -79,18 +111,17 @@ wait
 mv redis_slave.conf /etc/redis/redis_slave.conf
 wait
 sudo /etc/init.d/redis-server restart
-echo " "
-echo " "
-echo " "
+
+echo "\r\n \r\n"
+echo "\r\n \r\n"
+
 echo " Running benchmark again. https://redis.io/topics/benchmarks "
 echo " "
 echo " "
 redis-benchmark -q -n 1000 -c 10 -P 5 -p 46378 
 # -a password
 wait
-echo " "
-echo " "
-echo " "
+echo "\r\n \r\n"
 echo "--------------------------------------------------------------------"
 echo "To test, issue the following commands: "
 echo " redis-benchmark -q -n 1000 -c 10 -P 5 -p 46378 -a password "
@@ -100,9 +131,8 @@ echo "      >  info"
 echo " "
 echo " /usr/bin/redis-server /etc/redis/redis_slave.conf"
 echo "\r\n \r\n"
-wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/install_ruby.sh && chmod u+x install_ruby.sh && ./install_ruby.sh
-gem install redis
-
-wget http://download.redis.io/redis-stable/src/redis-trib.rb && chmod u+x redis-trib.rb && ./redis-trib.rb
+#wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/install_ruby.sh && chmod u+x install_ruby.sh && ./install_ruby.sh
+#gem install redis
+#wget http://download.redis.io/redis-stable/src/redis-trib.rb && chmod u+x redis-trib.rb && ./redis-trib.rb
 
 echo "Done!"
