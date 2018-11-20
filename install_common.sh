6ae1cb7f48352cd646f7ba534b21a24de61eaf4d
Chris Gray <christophermjgray@gmail.com>
2018-11-20 11:51:52 -0500

    Update install_common.sh

--- install_common.sh
+++ install_common.sh
@@ -24,8 +24,8 @@ https://raw.githubusercontent.com/c2theg/srvBuilds/master/install_common.sh
 This really is meant to be run under Ubuntu 14.04 / 16.04 LTS +
 
 \r\n \r\n
-Version:  1.6.14                             \r\n
-Last Updated:  11/10/2018
+Version:  1.6.15                             \r\n
+Last Updated:  11/20/2018
 \r\n \r\n"
 echo "Checking Internet status...   "
 #ping -q -c5 github.com > /dev/null
