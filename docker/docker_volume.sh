07c85f725250478b9d149c737564d1400b2c0812
Chris Gray <christophermjgray@gmail.com>
2018-11-20 20:39:47 -0500

    Update docker_volume.sh

--- docker/docker_volume.sh
+++ docker/docker_volume.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
+if [ ! -d "/media/data" ]; then
+     mkdir /media/data 
+fi
+
+if [ ! -d "/media/data/containers" ]; then
+     mkdir /media/data/containers/
+fi
+#-----------------------------------------------------
 echo "\r\n Create volume 'ContainerVolumes' \r\n "
 docker volume create ContainerVolumes
 
-
 echo "\r\n Inspect the volume... \r\n \r\n"
 docker volume inspect ContainerVolumes
-
 
 echo "\r\n All Volumes... \r\n \r\n"
 docker volume ls
