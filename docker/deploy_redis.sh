20ad5a48efc1c9772e76b266e3e0b3abaf38dc1b
Chris Gray <christophermjgray@gmail.com>
2018-11-21 21:31:07 -0500

    Create deploy_redis.sh

---
+++ docker/deploy_redis.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+clear
+now=$(date)
+echo "Running deploy_redis.sh at $now 
+  _____             _             
+ |  __ \           | |            
+ | |  | | ___   ___| | _____ _ __ 
+ | |  | |/ _ \ / __| |/ / _ \ '__|
+ | |__| | (_) | (__|   <  __/ |   
+ |_____/ \___/ \___|_|\_\___|_|   
+                                  
+Created By:
+ _____ _       _     _           _              _____    __    _____             
+|     | |_ ___|_|___| |_ ___ ___| |_ ___ ___   |     |__|  |  |   __|___ ___ _ _ 
+|   --|   |  _| |_ -|  _| . | . |   | -_|  _|  | | | |  |  |  |  |  |  _| .'| | |
+|_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
+                            |_|                                             |___|
+\r\n \r\n
+Version:  0.0.1                             \r\n
+Last Updated:  11/21/2018
+\r\n \r\n"
+wait
+#---------- Download config files ----------------
+if [ -s "docker-compose.yml" ]; then
+    echo "Deleting old files \r\n"
+    rm docker-compose.yml
+#   rm container-redis.conf
+    rm docker-compose_redis.yml
+fi
+#--- Config ---
+#wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/configs/redis.conf
+#--- Docker Deployment Config ---
+wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/docker/compose-configs/docker-compose_redis.yml
+#--------- Create Directory Structure -------------------------------------------------------------------------
+if [ ! -d "/media/data" ]; then
+     mkdir /media/data 
+fi
+
+if [ ! -d "/media/data/containers" ]; then
+     mkdir /media/data/containers/
+fi
+
+if [ ! -d "/media/data/containers/redis1" ]; then
+     mkdir /media/data/containers/redis1/
+fi
+
+#--- Create Docker Volume ---
+docker volume create redis1
+
+echo "Inspect volume \r\n "
+docker volume inspect redis1
+
+#--- rename config file to "docker-compose.yml" -----
+mv docker-compose_redis.yml /media/data/containers/redis1/docker-compose.yml
+
+#--- start up container ---
+cd /media/data/containers/redis1/
+docker-compose up
+
+echo "Done! \r\n \r\n"
