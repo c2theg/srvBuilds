bdfa626f9da3234804af7449f5e2ec991cc04359
Chris Gray <christophermjgray@gmail.com>
2018-11-21 21:15:14 -0500

    Update docker_base_images.sh

--- docker/docker_base_images.sh
+++ docker/docker_base_images.sh
@@ -1,26 +1,31 @@
 #!/bin/sh
 #   Christopher Gray
-#     version 0.0.3
-#    11/20/2018
+#     version 0.0.5
+#    11/21/2018
 clear
 
-echo "\r\n Docker Images downloaded.. (docker images -a) \r\n "
+echo "\r\n Preloading Docker Base Images.. \r\n "
 docker images -a
 #-----------------------------------------------------------------------
 #-- Ubuntu
 #---- 16.04
+#-- mongodb
 docker pull ubuntu:xenial
 #---- 18.04
-docker pull ubuntu:bionic
-
+#docker pull ubuntu:bionic
 #-- Debian
-docker pull debian:scratch
+#docker pull debian:scratch
+#docker pull debian:jessie
+#-- Redis & MySQL & Nginx & PHP 7.2 FPM
 docker pull debian:stretch-slim
-docker pull debian:jessie
 #----
-docker pull busybox:latest
-docker pull alpine:3.8
+#docker pull busybox:latest
+#docker pull alpine:3.8
+
+#-- Golang
 docker pull buildpack-deps:stretch-scm
+#-- Python3.7 & NodeJS Latest
+docker pull buildpack-deps:stretch
 
 #---- Microsoft
 #docker pull mcr.microsoft.com/windows/servercore:latest
