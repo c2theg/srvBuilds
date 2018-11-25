0166e5cd41b9efa8f19aa0f598a38a8ea4df9d88
Chris Gray <christophermjgray@gmail.com>
2018-11-24 22:27:49 -0500

    Update redis-init.sh

--- configs/redis-init.sh
+++ configs/redis-init.sh
@@ -1,7 +1,5 @@
 # the following script is for init of redis in a docker container
-
-sysctl vm.overcommit_memory=1
+#sysctl vm.overcommit_memory=1
 # max socket backlog (def 128)
-sysctl -w net.core.somaxconn=1024
-
-echo never > /sys/kernel/mm/transparent_hugepage/enabled
+#sysctl -w net.core.somaxconn=1024
+#echo never > /sys/kernel/mm/transparent_hugepage/enabled
