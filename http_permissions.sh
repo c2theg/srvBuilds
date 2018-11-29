9079feac92eccfe0f4bc1bd0f404684cc3b8799f
Chris Gray <christophermjgray@gmail.com>
2018-11-29 13:02:18 -0500

    Update http_permissions.sh

--- http_permissions.sh
+++ http_permissions.sh
@@ -1,5 +1,8 @@
 # fixes http permissions
 # 640 or 755
 sudo chmod -R 755 /var/www/
-
 sudo chown -R www-data:www-data /var/www/
+
+#--- resilio ---
+#sudo chown -R rslsync:rslsync /var/www/
+sudo chmod -R 755 /media/data/sync/ && sudo chown -R rslsync:rslsync /media/data/sync/
