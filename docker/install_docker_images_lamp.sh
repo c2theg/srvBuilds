0f3a6cf86751d777c7b3dad24db4adeb9d62454c
Chris Gray <christophermjgray@gmail.com>
2019-01-21 12:29:09 -0500

    Create install_docker_images_lamp.sh

---
+++ docker/install_docker_images_lamp.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+clear
+echo "
+ _____             _         _    _          _                                   
+|     |___ ___ ___| |_ ___ _| |  | |_ _ _   |_|                                  
+|   --|  _| -_| .'|  _| -_| . |  | . | | |   _                                   
+|_____|_| |___|__,|_| |___|___|  |___|_  |  |_|                                  
+                                     |___|                                       
+                                                                                 
+ _____ _       _     _           _              _____    __    _____             
+|     | |_ ___|_|___| |_ ___ ___| |_ ___ ___   |     |__|  |  |   __|___ ___ _ _ 
+|   --|   |  _| |_ -|  _| . | . |   | -_|  _|  | | | |  |  |  |  |  |  _| .'| | |
+|_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
+                            |_|                                             |___|
+\r\n \r\n
+Version:  0.0.1                             \r\n
+Last Updated:  1/9/2019
+\r\n \r\n"
+#--- download images------
+docker pull nginx:latest
+# Percona (MySQL 8.x)
+docker pull percona:8
+# Percona (MySQL 5.7 is latest)
+#docker pull percona:latest
+docker pull php:latest
+docker pull memcached:latest
+
+# Update to the latest images
+#docker images | grep -v REPOSITORY | awk '{print $1}' | xargs -L1 docker pull
+
+echo "DONE!"
