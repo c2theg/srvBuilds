662d3fa08dfa90d66238a54984f023cc1c1e5c14
Chris Gray <christophermjgray@gmail.com>
2019-01-21 11:59:20 -0500

    Create update_docker_images.sh

---
+++ docker/update_docker_images.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#    If you update this from Windows, using Notepad ++, do the following:
+#       sudo apt-get -y install dos2unix
+#       dos2unix <FILE>
+#       chmod u+x <FILE>
+#
+clear
+echo "Running update_core.sh at $now \r\n
+Current working dir: $SCRIPTPATH \r\n \r\n
+ _____             _         _    _          _                                   
+|     |___ ___ ___| |_ ___ _| |  | |_ _ _   |_|                                  
+|   --|  _| -_| .'|  _| -_| . |  | . | | |   _                                   
+|_____|_| |___|__,|_| |___|___|  |___|_  |  |_|                                  
+                                     |___|                                       
+                                                                                 
+ _____ _       _     _           _              _____    __    _____             
+|     | |_ ___|_|___| |_ ___ ___| |_ ___ ___   |     |__|  |  |   __|___ ___ _ _ 
+|   --|   |  _| |_ -|  _| . | . |   | -_|  _|  | | | |  |  |  |  |  |  _| .'| | |
+|_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
+                            |_|                                             |___|
+Version:  0.0.5                             \r\n
+Last Updated:  1/21/2019
+\r\n \r\n"
+sudo -E apt-get update
+wait
+sudo -E apt-get upgrade -y
+#wait
+
+echo "Updating all of docker images to the latest... \r\n \r\n"
+docker images | grep -v REPOSITORY | awk '{print $1}' | xargs -L1 docker pull
