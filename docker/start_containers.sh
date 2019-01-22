f3635eb300d1f7e5ceab0781afa10a7e4e0e9d38
Chris Gray <christophermjgray@gmail.com>
2019-01-22 16:55:49 -0500

    Create start_containers.sh

---
+++ docker/start_containers.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+clear
+now=$(date)
+echo "Running deploy_mongodb.sh at $now 
+  _____             _             
+ |  __ \           | |            
+ | |  | | ___   ___| | _____ _ __ 
+ | |  | |/ _ \ / __| |/ / _ \ '__|
+ | |__| | (_) | (__|   <  __/ |   
+ |_____/ \___/ \___|_|\_\___|_|   
+                                  
+Created By:
+ _____ _       _     _           _              _____    __    _____             
+|     | |_ ___|_|___| |_ ___ ___| |_ ___ ___   |     |__|  |  |   __|___ ___ _ _ 
+|   --|   |  _| |_ -|  _| . | . |   | -_|  _|  | | | |  |  |  |  |  |  _| .'| | |
+|_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
+                            |_|                                             |___|
+\r\n \r\n
+Version:  0.0.1                             \r\n
+Last Updated:  1/22/2019
+\r\n \r\n"
+
+# References
+# https://docs.docker.com/compose/reference/run/
+# https://docs.docker.com/config/containers/start-containers-automatically/
+#------- Add Containers here ----------
+
+# MongoDB
+cd /media/data/containers/mongodb1/ && docker-compose up -d
+
+# Nginx
+cd /media/data/containers/nginx/ && docker-compose up -d
