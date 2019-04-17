708091ed6e164780e1ccb8ed29250fd21e35b086
Chris Gray <christophermjgray@gmail.com>
2019-04-17 01:58:38 -0400

    Update install_time.sh

--- install_time.sh
+++ install_time.sh
@@ -21,7 +21,7 @@ echo "Running update_ubuntu.sh at $now
                             |_|                                             |___|
 
 \r\n \r\n
-Version:  0.0.1                             \r\n
+Version:  0.0.2                             \r\n
 Last Updated:  4/17/2019
 \r\n \r\n"
 wait
@@ -36,11 +36,26 @@ sudo apt-get autoremove -y
 wait
 echo "Downloading required dependencies...\r\n\r\n"
 #--------------------------------------------------------------------------------------------
-echo "Fix and update clock"
 timedatectl status
+cat /etc/timezone
+grep UTC /etc/default/rcS
+date
+# hardware clock
+sudo hwclock --show
 
+
+echo "Fix and update clock"
+sudo timedatectl set-timezone America/New_York
+sudo timedatectl set-ntp on
+sudo timedatectl set-ntp true
+sudo systemctl restart systemd-timesyncd.service
+
+sudo ntpdate pool.ntp.org
 sudo service ntp stop
 sudo ntpdate -s time.google.com
 sudo service ntp start
 
 timedatectl status
+
+# https://help.ubuntu.com/lts/serverguide/NTP.html.en
+# https://askubuntu.com/questions/27528/how-to-display-current-time-date-setting
