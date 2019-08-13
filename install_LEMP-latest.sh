839c4fc9f8601ef74dd13a6640454f62f3e3f980
Chris Gray <christophermjgray@gmail.com>
2019-08-13 14:17:33 -0400

    Update install_LEMP-latest.sh

--- install_LEMP-latest.sh
+++ install_LEMP-latest.sh
@@ -18,22 +18,44 @@ echo "
 |_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
                             |_|                                             |___|
 
+ 
+|￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣|
+|    SENIOR ENGINEERS    |
+|     DO NOT GROW ON     |
+|         TREES          |
+|__＿＿＿＿＿＿＿＿＿＿______|
+          (\_❀) ||
+          (•ㅅ•) ||
+          / 　 づ
+
 https://raw.githubusercontent.com/c2theg/srvBuilds/master/install_LEMP-latest.sh
 
 INSTALLS  LEMP (Linux* Nginx Memcache PHP (Latest - 7.3)
 
 \r\n \r\n
-Version:  1.4.2                             \r\n
-Last Updated:  8/12/2019
+Version:  1.4.3                             \r\n
+Last Updated:  8/13/2019
 \r\n \r\n
 Updating system first..."
-sudo -E apt-get update
-wait
-sudo -E apt-get upgrade -y
-wait
+
+#---- Add Repo's -----
+sudo add-apt-repository -y ppa:chris-lea/nginx-devel
+sudo LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php
+
+#--- Create Filesystem  ----
+mkdir "/media/data/"
+mkdir "/var/log/nginx/"
+
+sudo chmod -R 755 /media/data/ && sudo chown -R www-data:www-data /media/data/
+
+#----- Update ------
+sudo -E apt-get -y update
+#wait
+#sudo -E apt-get upgrade -y
+#wait
 echo "Downloading required dependencies...\r\n\r\n"
 #--------------------------------------------------------------------------------------------
-sudo apt-get -y dist-upgrade
+#sudo apt-get -y dist-upgrade
 wait
 sudo apt-get install -y ntp ntpdate ssh openssh-server screen whois traceroute htop sysstat iptraf iftop speedometer ncdu nload
 #---- Email -----
@@ -47,15 +69,10 @@ sudo apt-get install -y ntp ntpdate ssh openssh-server screen whois traceroute h
 #---------- PYTHON STUFF ----------------------------------
 #sudo apt-get install -y python2-virtualenv python3-virtualenv libicu-dev python-software-properties python python-pip python-dev python3-setuptools
 #wait
-#--- PHP 7.3 ---
-sudo LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php
-wait
-sudo apt-get -y update
-wait
+#--- PHP ---
 sudo apt-get install -y openssl-blacklist ssl-cert libmcrypt-dev mcrypt 
 sudo apt-get install -y php7.3 php7.3-cli php7.3-fpm php7.3-curl php7.3-json php7.3-gd php7.3-mysql php7.3-mbstring zip unzip
 sudo apt-get install -y php7.3-common php7.3-opcache php7.3-readline php7.3-soap php7.3-ldap php-pear php-xdebug php-apcu php-ssh2 php-geoip php7.3-bcmath php7.3-zip php7.3-xml php-mailparse php7.3-bz2 php7.3-xmlrpc php7.3-mcrypt
-
 wait
 sudo pear channel-update pear.php.net
 sudo pear install mail Net_SMTP Auth_SASL2-0.1.0 mail_mime
@@ -78,14 +95,12 @@ sudo cp "memcached.conf" "/etc/memcached.conf"
 wait
 echo "Memcache Config Download Complete"
 wait
-echo "Restarting service..."
-/etc/init.d/memcached restart
-
+echo "Restarting memcache service..."
+sudo /etc/init.d/memcached restart
+wait
 #------- NGINX --------
-sudo add-apt-repository -y ppa:chris-lea/nginx-devel
-sudo apt-get -y update && apt-get -y install nginx nginx-common nginx-full fcgiwrap gcc make libpcre3-dev zlib1g-dev
+sudo apt-get install -y nginx nginx-common nginx-full fcgiwrap gcc make libpcre3-dev zlib1g-dev
 #sudo apt-get -y install nginx-pagespeed 
-
 #-- Download PHP Configs ---
 #sudo chmod +x /etc/init.d/php-fastcgi && /etc/init.d/php-fastcgi start  && update-rc.d php-fastcgi defaults
 cd ~
@@ -96,9 +111,6 @@ sudo mv "php-fastcgi" "/etc/init.d/php-fastcgi"
 wait
 sudo chmod +x /etc/init.d/php-fastcgi && /etc/init.d/php-fastcgi start && update-rc.d php-fastcgi defaults
 
-mkdir "/media/data/"
-mkdir "/var/log/nginx/"
-
 if [ -s "/etc/php/7.3/fpm/php.ini" ]
 then
 	echo "Deleting file: PHP.ini config "
@@ -107,20 +119,18 @@ then
 	rm /media/data/php_browscap.ini
 fi
 wait
-
 echo "Downloading PHP-FPM Configs"
 wget -O  "php.ini" "https://raw.githubusercontent.com/c2theg/srvBuilds/master/configs/php.ini"
 sudo mv "php.ini" "/etc/php/7.3/fpm/php.ini"
 wait
 #--------------------------------------------------
 wget -O "php-fpm.conf" "https://raw.githubusercontent.com/c2theg/srvBuilds/master/configs/php-fpm-7.3.conf"
-sudo mv "php-fpm-7.3.conf" "/etc/php/7.3/fpm/php-fpm.conf"
+sudo mv "php-fpm.conf" "/etc/php/7.3/fpm/php-fpm.conf"
 wait
 #--------------------------------------------------
 wget -O "php_browscap.ini" "https://browscap.org/stream?q=PHP_BrowsCapINI"
 sudo mv "php_browscap.ini" "/media/data/php_browscap.ini"
 wait
-#--------------------------------------------------
 echo "PHP-FPM Configs download complete"
 #---------------------------------------------------------------------------------------------------------
 if [ -s "/etc/nginx/nginx.conf" ]
