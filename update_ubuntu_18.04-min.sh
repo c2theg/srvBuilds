965fea5189983f8faa29d3c87bf5270ca96d2d81
Chris Gray <christophermjgray@gmail.com>
2019-10-22 12:47:30 -0400

    Create update_ubuntu_18.04-min.sh

---
+++ update_ubuntu_18.04-min.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#    If you update this from Windows, using Notepad ++, do the following:
+#       sudo apt-get -y install dos2unix
+#       dos2unix <FILE>
+#       chmod u+x <FILE>
+#
+clear
+now=$(date)
+echo "Running update_ubuntu_18.04-min.sh at $now 
+ _____             _         _    _          _                                   
+|     |___ ___ ___| |_ ___ _| |  | |_ _ _   |_|                                  
+|   --|  _| -_| .'|  _| -_| . |  | . | | |   _                                   
+|_____|_| |___|__,|_| |___|___|  |___|_  |  |_|                                  
+                                     |___|                                       
+                                                                                 
+ _____ _       _     _           _              _____    __    _____             
+|     | |_ ___|_|___| |_ ___ ___| |_ ___ ___   |     |__|  |  |   __|___ ___ _ _ 
+|   --|   |  _| |_ -|  _| . | . |   | -_|  _|  | | | |  |  |  |  |  |  _| .'| | |
+|_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
+                            |_|                                             |___|
+\r\n \r\n
+Version:  0.0.1                             \r\n
+Last Updated:  10/22/2019
+\r\n \r\n"
+wait
+
+# Ubuntu 18.04 setup with mini install
+
+sudo apt-get update
+sudo apt install net-tools curl ssh
