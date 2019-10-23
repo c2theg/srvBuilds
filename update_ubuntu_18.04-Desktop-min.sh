8d3d590e7e82eb552d2600518b7fbf40176d1217
Chris Gray <christophermjgray@gmail.com>
2019-10-23 00:42:13 -0400

    Update update_ubuntu_18.04-Desktop-min.sh

--- update_ubuntu_18.04-Desktop-min.sh
+++ update_ubuntu_18.04-Desktop-min.sh
@@ -83,7 +83,7 @@ sudo apt-get install -y filezilla
 
 #--- Sublime Text ---
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-echo "deb https://download.sublimetext.com/apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
 sudo apt-get install -y sublime-text
 
