c237b0b0e53606758bef3eefa8e64f69e3c26347
Chris Gray <christophermjgray@gmail.com>
2019-11-30 18:30:06 -0500

    Update install_speedtest.sh

--- install_speedtest.sh
+++ install_speedtest.sh
@@ -1,20 +1,23 @@
 #!/bin/sh
 clear
+# Version: 0.0.5
+# Updated: 11/30/19
 
 # https://www.speedtest.net/apps/cli
-
-sudo apt-get install -y gnupg1 apt-transport-https dirmngr
+sudo -E apt-get install -y gnupg1 apt-transport-https dirmngr
 export INSTALL_KEY=379CE192D401AB61
 # Ubuntu versions supported: xenial, bionic
 # Debian versions supported: jessie, stretch, buster
 export DEB_DISTRO=$(lsb_release -sc)
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $INSTALL_KEY
+#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $INSTALL_KEY
+sudo gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys $INSTALL_KEY
+
 echo "deb https://ookla.bintray.com/debian ${DEB_DISTRO} main" | sudo tee  /etc/apt/sources.list.d/speedtest.list
 sudo apt-get update
 # Other non-official binaries will conflict with Speedtest CLI
 # Example how to remove using apt-get
 # sudo apt-get remove speedtest-cli
-sudo apt-get install -y speedtest
+sudo -E apt-get install -y --allow-unauthenticated speedtest
 wait
 #echo " "
 speedtest
