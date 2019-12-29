66684006eae8dbc6852d1b9b64fd149c57e0f1f3
Chris Gray <christophermjgray@gmail.com>
2019-12-29 17:28:56 -0500

    Create setup_bbr.sh

---
+++ setup_bbr.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+#    If you update this from Windows, using Notepad ++, do the following:
+#       sudo apt-get -y install dos2unix
+#       dos2unix <FILE>
+#       chmod u+x <FILE>
+#
+clear
+echo "Running setup_bbr.sh 
+
+ _____             _         _    _          _                                   
+|     |___ ___ ___| |_ ___ _| |  | |_ _ _   |_|                                  
+|   --|  _| -_| .'|  _| -_| . |  | . | | |   _                                   
+|_____|_| |___|__,|_| |___|___|  |___|_  |  |_|                                  
+                                     |___|                                       
+                                                                                 
+ _____ _       _     _           _              _____    __    _____             
+|     | |_ ___|_|___| |_ ___ ___| |_ ___ ___   |     |__|  |  |   __|___ ___ _ _ 
+|   --|   |  _| |_ -|  _| . | . |   | -_|  _|  | | | |  |  |  |  |  |  _| .'| | |
+|_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
+                            |_|                                             |___|
+
+\r\n \r\n
+Version:  0.0.1                             \r\n
+Last Updated:  12/29/2019
+\r\n \r\n"
+wait
+# https://gist.github.com/xterat/cefccb42f7d4b2055368ebb00454861f
+
+#--------------------------------------------------------------------------------------------
+
+uname -r
+echo "Make sure kernel version is 4.9 or newer. if not, press Ctrl+c now! "
+wait 10
+
+apt install --install-recommends linux-generic-hwe-16.04
+modprobe tcp_bbr
+echo "tcp_bbr" >> /etc/modules-load.d/modules.conf
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+sysctl -p
+
+echo "DONE! \r\n "
+echo "Check if its working... "
+
+sysctl net.ipv4.tcp_available_congestion_control
+sysctl net.ipv4.tcp_congestion_control
+lsmod | grep bbr
