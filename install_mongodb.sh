d954a7b1f3d60f3f5a023bec305295b6950e351c
Chris Gray <christophermjgray@gmail.com>
2020-01-05 19:49:54 -0500

    Update install_mongodb.sh

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -18,33 +18,65 @@ echo "
 |_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
                             |_|                                             |___|
 \r\n \r\n
-Version:  0.0.3                             \r\n
-Last Updated:  1/8/2019
-\r\n \r\n
-Updating system first..."
+Version:  0.0.4                             \r\n
+Last Updated:  1/5/2020
+\r\n \r\n"
 #sudo -E apt-get update
 #wait
 #sudo -E apt-get upgrade -y
 #wait
+
+#echo "Source: https://www.howtoforge.com/tutorial/install-mongodb-on-ubuntu/  \r\n ";
+
 echo "Downloading required dependencies...\r\n\r\n"
 #--------------------------------------------------------------------------------------------
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
-touch /etc/apt/sources.list.d/mongodb-org-4.0.list
-#-- 16.04
-#echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
-#-- 18.04
-#echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+echo "Installing gnupg.. "
+sudo apt-get install -y gnupg openssl
 
-sudo -E apt-get update
-sudo apt-get install -y mongodb-org --allow-unauthenticated
+#--- Ubuntu 16.04 ---
+sudo apt-get install -y libcurl3
+#--- Ubuntu 18.04 ---
+#sudo apt-get install libcurl4
+
+echo "Adding Key... "
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+#--- Ubuntu 16.04 (Xenial) ---
+# /etc/apt/sources.list.d/mongodb-org-4.2.list
+echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+#--- Ubuntu 18.04 (Bionic) ----
+#echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+
+#-------------------------------------
+echo "Updating repo's... \r\n \r\n "
+sudo apt-get update
+
+echo "Install Mongodb... "
+sudo apt-get install -y mongodb-org=4.2.2 mongodb-org-server=4.2.2 mongodb-org-shell=4.2.2 mongodb-org-mongos=4.2.2 mongodb-org-tools=4.2.2
+
+#----------------------------------
+ps -e | grep mongo
+
+#sudo -E apt-get update
+#sudo apt-get install -y mongodb-org --allow-unauthenticated
 
 systemctl start mongod
 systemctl enable mongod
-
 netstat -plntu
 
-echo "Source: https://www.howtoforge.com/tutorial/install-mongodb-on-ubuntu/  \r\n ";
-echo " Done! \r\n\r\n"
+echo "Done. Starting Mongo..."
+#sudo service mongod start
+sudo -u mongodb mongod --config /etc/mongod.conf
 
-mongo
+sudo service mongod status
+
+echo " Use any of the following to get info from Mongo: \r\n
+mongo -port 27017   \r\n \r\n
+ 
+db.version()     \r\n
+rs.status()     \r\n
+rs.conf()     \r\n
+db.isMaster()     \r\n \r\n
+"
+
+#connect to mongo
+mongo -port 27017
