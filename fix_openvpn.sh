d057888f7f005ac2d4557bb2f3ea1eb8d3696285
Chris Gray <christophermjgray@gmail.com>
2020-02-01 22:38:39 -0500

    Update fix_openvpn.sh

--- fix_openvpn.sh
+++ fix_openvpn.sh
@@ -5,11 +5,12 @@ iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE
 ping 8.8.8.8 -c 5
 ping google.com -c 5
 
+cp fix_openvpn.sh /root/fix_openvpn.sh
+
 echo "\r\n \r\n 
 Make sure to add this to crontab: \r\n \r\n
 @reboot /home/ubuntu/fix_openvpn.sh >> /var/log/fix_openvpn.sh
-
-
+1 */1 * * *  /root/fix_openvpn.sh >> /var/log/fix_openvpn.sh
 "
 
 
