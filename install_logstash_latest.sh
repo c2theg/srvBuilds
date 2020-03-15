d786e991e69fd02de4ee1f6b55dc509b9ab0681a
Chris Gray <christophermjgray@gmail.com>
2020-03-15 00:05:02 -0400

    Update and rename install_logstash6.sh to install_logstash_latest.sh

--- install_logstash_latest.sh
+++ install_logstash_latest.sh
@@ -21,11 +21,10 @@ echo "
 https://raw.githubusercontent.com/c2theg/srvBuilds/master/install_logstash6.sh
 This really is meant to be run under Ubuntu 14.04 - 16.04 LTS +
 \r\n \r\n
-Version:  0.0.7                             \r\n
-Last Updated:  3/24/2018
+Version:  0.0.8                             \r\n
+Last Updated:  3/15/2020
 
 \r\n \r\n
-
 
 Updating system first..."
 sudo -E apt-get update
@@ -37,29 +36,28 @@ sudo apt-get autoremove -y
 wait
 echo "Downloading required dependencies...\r\n\r\n"
 #--------------------------------------------------------------------------------------------
-if [ ! -s "/etc/apt/sources.list.d/elastic-6.x.list" ]
+if [ ! -s "/etc/apt/sources.list.d/elastic-7.x.list" ]
 then
     wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
     sudo -E apt-get install -y apt-transport-https
-    echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
+    echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
 fi
 
 sudo -E apt-get update
 wait
-sudo -E apt-get install -y logstash ruby ruby-bundler
+sudo -E apt-get install -y logstash
+#sudo -E apt-get install -y ruby ruby-bundler
 
 sudo update-rc.d logstash defaults 95 10
 
 #----- Install Plugins ----
-sudo /usr/share/logstash/bin/logstash-plugin install logstash-filter-geoip
-sudo /usr/share/logstash/bin/logstash-plugin install logstash-filter-dns
+#sudo /usr/share/logstash/bin/logstash-plugin install logstash-filter-geoip
+#sudo /usr/share/logstash/bin/logstash-plugin install logstash-filter-dns
 
 #sudo /usr/share/logstash/bin/logstash-plugin install logstash-input-websocket
 #sudo /usr/share/logstash/bin/logstash-plugin install logstash-input-rss
 #sudo /usr/share/logstash/bin/logstash-plugin install logstash-input-xmpp
 #-------------------------- 
-
-
 echo "\r\n \r\n DONE! \r\n \r\n"
 
 echo "To TEST it out!... \r\n \r\n "
