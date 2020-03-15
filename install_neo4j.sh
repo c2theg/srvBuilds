dfdd93e951f2d33ad59d45d389d1fc641c81881f
Chris Gray <christophermjgray@gmail.com>
2020-03-15 16:43:58 -0400

    Create install_neo4j.sh

---
+++ install_neo4j.sh
@@ -0,0 +1,38 @@
+
+#!/bin/sh
+#
+
+clear
+echo "
+ _____             _         _    _          _                                   
+|     |___ ___ ___| |_ ___ _| |  | |_ _ _   |_|                                  
+|   --|  _| -_| .'|  _| -_| . |  | . | | |   _                                   
+|_____|_| |___|__,|_| |___|___|  |___|_  |  |_|                                  
+                                     |___|                                       
+                                                                                 
+ _____ _       _     _           _              _____    __    _____             
+|     | |_ ___|_|___| |_ ___ ___| |_ ___ ___   |     |__|  |  |   __|___ ___ _ _ 
+|   --|   |  _| |_ -|  _| . | . |   | -_|  _|  | | | |  |  |  |  |  |  _| .'| | |
+|_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
+                            |_|                                             |___|
+\r\n \r\n
+
+
+This really is meant to be run under Ubuntu 16.04 - 18.04 LTS +
+\r\n \r\n
+Version:  0.0.1                             \r\n
+Last Updated:  3/15/2020
+\r\n \r\n"
+
+wget --no-check-certificate -O - https://debian.neo4j.org/neotechnology.gpg.key | sudo apt-key add -
+echo 'deb http://debian.neo4j.org/repo stable/' > /etc/apt/sources.list.d/neo4j.list
+sudo -E apt update
+sudo -E apt install neo4j
+
+sudo -E systemctl start neo4j
+sudo -E systemctl enable neo4j
+
+sudo  systemctl status influxdb
+#-------------------------
+
+echo "DONE! \r\n \r\n"
