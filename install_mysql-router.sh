e28fd89f2b4f43ac8e12d3db233586c9638eed42
Chris Gray <christophermjgray@gmail.com>
2020-03-15 19:51:47 -0400

    Update install_mysql-router.sh

--- install_mysql-router.sh
+++ install_mysql-router.sh
@@ -18,25 +18,19 @@ echo "
 |_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
                             |_|                                             |___|
 \r\n \r\n
-Version:  0.0.2                             \r\n
-Last Updated:  8/25/2019
-\r\n \r\n
-Updating system first..."
-#sudo -E apt-get update
-wait
-#sudo -E apt-get upgrade -y
+Version:  0.0.3                             \r\n
+Last Updated:  3/15/2020
+\r\n \r\n"
 wait
 echo "Downloading required dependencies... \r\n\r\n "
 #--------------------------------------------------------------------------------------------
 cd ~
 echo "Download latest deb from: https://dev.mysql.com/downloads/repo/apt/  \r\n \r\n"
 
-
-wget –c https://dev.mysql.com/get/mysql-apt-config_0.8.11-1_all.deb
-sudo dpkg -i mysql-apt-config_0.8.8-1_all.deb
+wget -c https://dev.mysql.com/get/Downloads/MySQL-Router/mysql-router-community_8.0.19-1ubuntu18.04_amd64.deb
+sudo dpkg -i mysql-router-community_8.0.19-1ubuntu18.04_amd64.deb
 sudo apt-get update
 sudo apt-get install -y mysql-router
-
 
 echo "\r\n Downloading Config \r\n"
 wget "mysql_router.conf" "https://raw.githubusercontent.com/c2theg/srvBuilds/master/configs/mysql_router.conf"
@@ -44,7 +38,6 @@ wait
 sudo cp "mysql_router.conf" "/etc/mysqlrouter/mysql_router.conf"
 wait
 echo "\r\n Router Config Download Complete \r\n"
-
 
 #sudo /etc/init.d/mysql-router restart
 echo "\r\n \r\n \r\n \r\n"
