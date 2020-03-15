1d30ca3fcc4090d74bc7763074f078e6f9f1c3d4
Chris Gray <christophermjgray@gmail.com>
2020-03-15 12:34:05 -0400

    Update install_kibana_latest.sh

--- install_kibana_latest.sh
+++ install_kibana_latest.sh
@@ -24,19 +24,7 @@ Last Updated:  3/14/2020
 \r\n \r\n"
 
 echo " Source: https://www.elastic.co/guide/en/kibana/current/deb.html \r\n \r\n "
-
-sudo apt-get install -y apt-transport-https
-
-if [ ! -s "/etc/apt/sources.list.d/elastic-7.x.list" ]
-then
-   wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
-   echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
-fi
-
-sudo apt-get update && sudo apt-get install kibana
-
-#sudo -i service kibana start
-#sudo -i service kibana stop
+sudo apt-get install -y kibana
 
 #--- set permissions ---
 sudo chown -R kibana:kibana /usr/share/kibana/optimize/
@@ -56,11 +44,17 @@ wait
 
 sudo update-rc.d kibana defaults 95 10
 
-echo "DONE! \r\n \r\n Point your browser to:  http://localhost:5601  to view it  \r\n \r\n "
+echo "DONE! \r\n \r\n Point your browser to:  http://<Server IP>:5601  to view it  \r\n \r\n "
 
 #echo " -- Secure Kibana behind Nginx Reverse proxy \r\n \r\n
 #echo "admin:$(openssl passwd -apr1 YourStrongPassword)" | sudo tee -a /etc/nginx/htpasswd.kibana
 #check out the site here: https://www.rosehosting.com/blog/install-and-configure-the-elk-stack-on-ubuntu-16-04/  \r\n \r\n "
+
+echo "Starting service.. might take 1minute.. \r\n "
+sudo -i service kibana start
+sudo -i service kibana status
+wait
+sleep 10
 
 curl -X GET "localhost:5601/api/status"
 curl -u elastic:changeme http://localhost:9200/?pretty
