5a062615cac5d27980bc81af1afc0762963b3598
Chris Gray <christophermjgray@gmail.com>
2020-03-21 00:58:13 -0400

    Create network_check_18-04.sh

---
+++ network_check_18-04.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+#    If you update this from Windows, using Notepad ++, do the following:
+#       sudo apt-get -y install dos2unix
+#       dos2unix <FILE>
+#       chmod u+x <FILE>
+#
+clear
+now=$(date)
+echo "Running network_check_18-04.sh \r\n
+ _____             _         _    _          _                                   
+|     |___ ___ ___| |_ ___ _| |  | |_ _ _   |_|                                  
+|   --|  _| -_| .'|  _| -_| . |  | . | | |   _                                   
+|_____|_| |___|__,|_| |___|___|  |___|_  |  |_|                                  
+                                     |___|                                       
+                                                                                 
+ _____ _       _     _           _              _____    __    _____             
+|     | |_ ___|_|___| |_ ___ ___| |_ ___ ___   |     |__|  |  |   __|___ ___ _ _ 
+|   --|   |  _| |_ -|  _| . | . |   | -_|  _|  | | | |  |  |  |  |  |  _| .'| | |
+|_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
+                            |_|                                             |___|
+
+
+Version:  0.0.1                             \r\n
+Last Updated:  3/20/2020
+\r\n \r\n"
+wait
+#--------------------------------------------------------------------------------------------
+echo "Checking Internet status at $now ...\r\n\r\n"
+#ping -q -c3 github.com > /dev/null
+#if [ $? -eq 0 ]
+if nc -zw1 google.com 443; then
+	echo "Connected!!! Exiting  \r\n"
+else
+    echo "NOT CONNECTED! Reconnecting..."
+    netplan apply
+    echo "Done! \r\n"
+fi
