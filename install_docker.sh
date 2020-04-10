6cce4633177cbb6f5ad40a22410c50f010438ad8
Chris Gray <christophermjgray@gmail.com>
2020-04-09 21:51:50 -0400

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-#    If you update this from Windows, using Notepad ++, do the following:
-#       sudo apt-get -y install dos2unix
-#       dos2unix <FILE>
-#       chmod u+x <FILE>
 #
 clear
 echo "
@@ -19,100 +15,19 @@ echo "
                             |_|                                             |___|
 
 \r\n \r\n
-Version:  1.5.3                             \r\n
-Last Updated:  3/4/2020
-\r\n \r\n
-Updating system first..."
-sudo -E apt-get update
-#wait
-sudo -E apt-get upgrade -y
-#wait
+Version:  1.5.4                             \r\n
+Last Updated:  4/9/2020
+
+"
 echo "Downloading required dependencies...\r\n\r\n"
 #--------------------------------------------------------------------------------------------
-# - from:  https://docs.docker.com/engine/installation/linux/ubuntu/#os-requirements
+# Source: https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-16-04
 
-echo "Installing Docker... \r\n \r\n"
-sudo -E apt-get -y install apt-transport-https ca-certificates curl software-properties-common
-wait
-#-------- Ubuntu 16.04 ------------------------
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo -E apt-get update
+sudo apt-get update
 apt-cache policy docker-ce
-sudo -E apt-get install -y docker-ce
+sudo apt-get install -y docker-ce
 
-sudo systemctl start docker
-sudo systemctl enable docker
-#sudo systemctl status docker
 wait
-sudo usermod -aG docker $USER
-#sudo apt-get install docker.io
-
-#--- Install Docker Compose ---
-# Get Latest from: https://github.com/docker/compose/releases
-curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-docker-compose --version
-#------------------------------------
-echo "Installing Cockpit ... "
-#- give cockpit access to docker api
-sudo groupadd docker
-sudo usermod -aG docker $USER
-newgrp docker
-sudo -E apt-get install cockpit cockpit-docker -y
-echo "Done! - Visit: https://SERVER_IP:9090  "
-echo "\r\n \r\n"
-
-echo "Hello-World... ( sudo docker run -p 3000:80 tutum/hello-world  )"
-sudo docker pull hello-world
-wait
-
-echo "Nginx Container... ( https://hub.docker.com/_/nginx )
-
-"
-sudo docker pull nginx
-echo " docker run --name some-nginx -d -p 8080:80 some-content-nginx 
-
-
-"
-
-echo "Downloading a better way to manage containers... \r\n.."
-echo " PORTAINER! - https://github.com/portainer/portainer \r\n "
-
-docker pull portainer/portainer
-docker run -d -p 9000:9000 -v "/var/run/docker.sock:/var/run/docker.sock" portainer/portainer
-
-echo "Running command: sudo docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer"
-echo "\r\n \r\n"
-
-echo "Visit http://<Local_IP>:9000 in Chrome or Firefox \r\n \r\n"
-docker --version
-docker ps
-
-echo "Allowing remote access to Docker API... \r\n"
-# Source: https://success.docker.com/article/how-do-i-enable-the-remote-api-for-dockerd
-
-mkdir /etc/systemd/system/docker.service.d/
-touch /etc/systemd/system/docker.service.d/startup_options.conf
-echo [Service] >> /etc/systemd/system/docker.service.d/startup_options.conf
-echo ExecStart= >> /etc/systemd/system/docker.service.d/startup_options.conf
-echo ExecStart=/usr/bin/dockerd -H fd:// -H tcp://0.0.0.0:2376 >> /etc/systemd/system/docker.service.d/startup_options.conf
-
-sudo systemctl daemon-reload
-sudo systemctl restart docker.service
-wait
-#----------------------------
-echo "\r\n \r\n "
-ps aux | grep -i docker
-echo "\r\n \r\n "
-ss -ntl
-
-#dockerd --tlsverify --tlscacert=ca.pem --tlscert=server-cert.pem --tlskey=server-key.pem -H=0.0.0.0:2376
-#docker --tlsverify --tlscacert=ca.pem --tlscert=cert.pem --tlskey=key.pem -H=$HOST:2376 version
-  
-echo "View Docker commands here: https://docs.docker.com/engine/reference/commandline/container_ls/  \r\n \r\n"
-echo " Hello World Container:  sudo docker run -p 3000:80 tutum/hello-world  \r\n \r\n"
-echo "\r\n \r\n Docker deployment complete!!! \r\n \r\n"
-
-docker images
-docker ps
+sudo systemctl status docker
