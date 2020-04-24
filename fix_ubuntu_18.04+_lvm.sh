0cd8ce711e1c213a27df6a93babefb8177a387e5
Chris Gray <christophermjgray@gmail.com>
2020-04-24 02:03:56 -0400

    Update fix_ubuntu_18.04+_lvm.sh

--- fix_ubuntu_18.04+_lvm.sh
+++ fix_ubuntu_18.04+_lvm.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # From: https://askubuntu.com/questions/1106795/ubuntu-server-18-04-lvm-out-of-space-with-improper-default-partitioning
 # Update: 4-24-20
-#
+#   - works in 20.04 Server too
 #-------------------------------------------------
 lvm
 lvextend -l +100%FREE /dev/ubuntu-vg/ubuntu-lv
