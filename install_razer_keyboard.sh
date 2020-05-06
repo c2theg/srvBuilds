ccc7aeb9ffa570c0c9cafcee698d34e25d501954
Chris Gray <christophermjgray@gmail.com>
2020-05-06 19:37:16 -0400

    Update install_razer_keyboard.sh

--- install_razer_keyboard.sh
+++ install_razer_keyboard.sh
@@ -1,5 +1,12 @@
-# http://ubuntuhandbook.org/index.php/2019/04/configure-razer-peripherals-ubuntu-16-04-18-04/
 
+#!/bin/sh
+#
+clear
+echo "
+
+Version:  0.0.1
+Last Updated:  5/6/2020"
+# http://ubuntuhandbook.org/index.php/2019/04/configure-razer-peripherals-ubuntu-16-04-18-04/
 
 sudo add-apt-repository -y ppa:openrazer/stable
 sudo add-apt-repository -y ppa:polychromatic/stable
