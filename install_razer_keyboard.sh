75652315a0ee4ee361bddc3b35848ae62f736e28
Chris Gray <christophermjgray@gmail.com>
2020-05-06 19:36:22 -0400

    Create install_razer_keyboard.sh

---
+++ install_razer_keyboard.sh
@@ -0,0 +1,12 @@
+# http://ubuntuhandbook.org/index.php/2019/04/configure-razer-peripherals-ubuntu-16-04-18-04/
+
+
+sudo add-apt-repository -y ppa:openrazer/stable
+sudo add-apt-repository -y ppa:polychromatic/stable
+sudo apt update
+
+sudo apt install openrazer-meta polychromatic
+
+# Uninstall 
+# sudo apt remove --auto-remove openrazer-meta polychromatic
+
