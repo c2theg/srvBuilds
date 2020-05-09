4fd8efb22bb481d909334937644c5796c968f4e8
Chris Gray <christophermjgray@gmail.com>
2020-05-09 13:32:25 -0400

    Update setup_webdev_ubuntu.sh

--- setup_webdev_ubuntu.sh
+++ setup_webdev_ubuntu.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
+sudo apt update
+sudo apt install -y software-properties-common apt-transport-https wget
+
 
 apt install -y mongodb-clients
+sudo snap install --classic code
+
+
+wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/start_vscode_sudo.sh
+chmod u+x start_vscode_sudo.sh
+
