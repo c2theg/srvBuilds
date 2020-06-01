3ab942a80e333db21f6c2686373ce3b17203218f
Chris Gray <christophermjgray@gmail.com>
2020-06-01 14:13:39 -0400

    Create install_minecraft_pocketmine.sh

---
+++ games/install_minecraft_pocketmine.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#    If you update this from Windows, using Notepad ++, do the following:
+#       sudo apt-get -y install dos2unix
+#       dos2unix <FILE>
+#       chmod u+x <FILE>
+#
+clear
+echo "
+ _____             _         _    _          _                                   
+|     |___ ___ ___| |_ ___ _| |  | |_ _ _   |_|                                  
+|   --|  _| -_| .'|  _| -_| . |  | . | | |   _                                   
+|_____|_| |___|__,|_| |___|___|  |___|_  |  |_|                                  
+                                     |___|                                       
+                                                                                 
+ _____ _       _     _           _              _____    __    _____             
+|     | |_ ___|_|___| |_ ___ ___| |_ ___ ___   |     |__|  |  |   __|___ ___ _ _ 
+|   --|   |  _| |_ -|  _| . | . |   | -_|  _|  | | | |  |  |  |  |  |  _| .'| | |
+|_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
+                            |_|                                             |___|
+
+
+Version:  0.0.1
+Last Updated:  6/1/2020
+"
+sudo apt-get update
+wait
+sudo apt-get upgrade -y
+#--- Download Minecraft --- https://pmmp.readthedocs.io/en/rtfd/installation/get-dot-pmmp-dot-io.html
+
+mkdir pocketmine
+cd pocketmine
+
+wget -q -O - https://get.pmmp.io | bash -s -
