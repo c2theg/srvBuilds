e6c0dcbff41551eb0aaaecd6dca74e9413d757d3
Chris Gray <christophermjgray@gmail.com>
2020-06-02 17:09:26 -0400

    Update start_mcje.sh

--- games/minecraft/start_mcje.sh
+++ games/minecraft/start_mcje.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Crontab - Add to startup
-#     @reboot /home/ubuntu/start_mcje.sh >> /var/log/start_mcje.log 2>&1
+#     @reboot /home/ubuntu/minecraft-je/start_mcje.sh >> /var/log/start_mcje.log 2>&1
 #
 
-java -Xmx1024M -Xms1024M -jar server.jar nogui &
+java -Xmx1024M -Xms1024M -jar /home/ubuntu/minecraft-je/server.jar nogui &
