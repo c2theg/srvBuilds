c18941599dd104cdaeff2b368d3880d809575dca
Chris Gray <christophermjgray@gmail.com>
2020-09-20 22:33:05 -0400

    Update install_webmin.sh

--- install_webmin.sh
+++ install_webmin.sh
@@ -1,23 +1,10 @@
 #!/bin/bash
 clear
 
-Version='1.941'
+Version='1.953'
 echo "
- _____             _         _    _          _                                   
-|     |___ ___ ___| |_ ___ _| |  | |_ _ _   |_|                                  
-|   --|  _| -_| .'|  _| -_| . |  | . | | |   _                                   
-|_____|_| |___|__,|_| |___|___|  |___|_  |  |_|                                  
-                                     |___|                                       
-                                                                                 
- _____ _       _     _           _              _____    __    _____             
-|     | |_ ___|_|___| |_ ___ ___| |_ ___ ___   |     |__|  |  |   __|___ ___ _ _ 
-|   --|   |  _| |_ -|  _| . | . |   | -_|  _|  | | | |  |  |  |  |  |  _| .'| | |
-|_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
-                            |_|                                             |___|
-
-\r\n \r\n
-Version:  1.3.0                            \r\n
-Last Updated:  3/15/2020
+Version:  1.3.1    \r\n
+Last Updated:  9/20/2020
 \r\n \r\n
 
 This will install version  $Version of Webmin \r\n \r\n
@@ -29,9 +16,8 @@ sudo -E apt-get upgrade -y
 wait
 echo "Downloading required dependencies...\r\n\r\n"
 #--------------------------------------------------------------------------------------------
-sudo apt-get -y install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python
+sudo apt-get -y install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions
 wait
-
 wget "http://prdownloads.sourceforge.net/webadmin/webmin_${Version}_all.deb"
 wait
 sudo dpkg --install webmin_${Version}_all.deb
