4dc263760eb1fb5f73a6bfc920ca6006afa67786
Chris Gray <christophermjgray@gmail.com>
2020-09-20 22:31:19 -0400

    Update install_monitoring.sh

--- install_monitoring.sh
+++ install_monitoring.sh
@@ -19,8 +19,8 @@ echo "
                             |_|                                             |___|
 https://raw.githubusercontent.com/c2theg/srvBuilds/master/install_monitoring.sh
 \r\n \r\n
-Version:  0.0.2                            \r\n
-Last Updated:  7/1/2018
+Version:  0.0.3                            \r\n
+Last Updated:  9/20/2020
 \r\n \r\n
 Updating system first..."
 sudo -E apt-get update
@@ -34,7 +34,7 @@ echo "Downloading files "
 wait
 wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/install_webmin.sh
 chmod u+x install_webmin.sh
-sudo -e ./install_webmin.sh
+sudo ./install_webmin.sh
 
 #------------- OS Version Detection -------------
 if [ -f /etc/os-release ]; then
@@ -68,7 +68,7 @@ else
 fi
 echo " Detected OS: $OS, Version: $VER \r\n \r\n"
 #-----------------------------------------------
-if [ $VER = '16.04' ] || [ $VER = '16.10' ] || [ $VER = '17.04' ] || [ $VER = '18.04' ]; then
+if [ $VER = '16.04' ] || [ $VER = '16.10' ] || [ $VER = '17.04' ] || [ $VER = '18.04' ] || [ $VER = '20.04' ]; then
 	    echo "\r\n\r\n \r\n Add Cockpit! (Only for Ubuntu 16.04+) \r\n \r\n"	
 	    sudo add-apt-repository -y ppa:cockpit-project/cockpit && sudo -E apt-get install -y cockpit
 	    sudo systemctl start cockpit && sudo systemctl enable cockpit
