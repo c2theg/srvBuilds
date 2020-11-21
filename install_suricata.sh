3eaea3c5aa0262d9d816fd2d4cc08a72ee85c359
Chris Gray <christophermjgray@gmail.com>
2020-11-21 10:31:57 -0500

    Create install_suricata.sh

---
+++ install_suricata.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+#    If you update this from Windows, using Notepad ++, do the following:
+#       sudo apt-get -y install dos2unix
+#       dos2unix <FILE>
+#       chmod u+x <FILE>
+#
+clear
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+now=$(date)
+echo "Running update_core.sh at $now \r\n
+Current working dir: $SCRIPTPATH \r\n \r\n
+ _____             _         _    _          _                                   
+|     |___ ___ ___| |_ ___ _| |  | |_ _ _   |_|                                  
+|   --|  _| -_| .'|  _| -_| . |  | . | | |   _                                   
+|_____|_| |___|__,|_| |___|___|  |___|_  |  |_|                                  
+                                     |___|                                       
+                                                                                 
+ _____ _       _     _           _              _____    __    _____             
+|     | |_ ___|_|___| |_ ___ ___| |_ ___ ___   |     |__|  |  |   __|___ ___ _ _ 
+|   --|   |  _| |_ -|  _| . | . |   | -_|  _|  | | | |  |  |  |  |  |  _| .'| | |
+|_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
+                            |_|                                             |___|
+Version:  0.0.1                             \r\n
+Last Updated:  11/21/2020
+\r\n \r\n"
+#------------------------------------------------------------------------------------------------
+
+#https://redmine.openinfosecfoundation.org/projects/suricata/wiki/Ubuntu_Installation_-_Personal_Package_Archives_%28PPA%29
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository -y ppa:oisf/suricata-stable
+sudo apt-get update
+
+sudo apt-get install suricata 
+
+
