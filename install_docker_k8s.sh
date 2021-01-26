f76b4bf6379a38247bdff928be7883d0b79f9d7c
Chris Gray <christophermjgray@gmail.com>
2021-01-26 00:14:46 -0500

    Create install_docker_k8s.sh

---
+++ install_docker_k8s.sh
@@ -0,0 +1,75 @@
+#!/bin/sh
+#    If you update this from Windows, using Notepad ++, do the following:
+#       sudo apt-get -y install dos2unix
+#       dos2unix <FILE>
+#       chmod u+x <FILE>
+#
+#clear
+echo "\r\n \r\n \r\n 
+By: Christopher Gray
+
+
+Installing Docker for Kubernetes
+
+Version:  0.0.1             \r\n
+Last Updated:  1/25/2021
+\r\n \r\n"
+echo "Updating system first..."
+sudo -E apt-get update
+wait
+sudo -E apt-get upgrade -y
+wait
+#--------------------------------------------
+# (Install Docker CE)
+## Set up the repository:
+### Install packages to allow apt to use a repository over HTTPS
+sudo apt-get update && sudo apt-get install -y \
+  apt-transport-https ca-certificates curl software-properties-common gnupg2
+
+
+ # Add Docker's official GPG key:
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key --keyring /etc/apt/trusted.gpg.d/docker.gpg add -
+
+
+# Add the Docker apt repository:
+sudo add-apt-repository \
+  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) \
+  stable"
+
+
+# Install Docker CE
+sudo apt-get update && sudo apt-get install -y \
+  containerd.io=1.2.13-2 \
+  docker-ce=5:19.03.11~3-0~ubuntu-$(lsb_release -cs) \
+  docker-ce-cli=5:19.03.11~3-0~ubuntu-$(lsb_release -cs)
+
+
+## Create /etc/docker
+sudo mkdir /etc/docker
+
+
+# Set up the Docker daemon
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+
+# Create /etc/systemd/system/docker.service.d
+sudo mkdir -p /etc/systemd/system/docker.service.d
+
+
+# Restart Docker
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+
+
+sudo systemctl enable docker
+
