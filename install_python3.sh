43e7d61baedc1c16555f33007c909687c7bc8801
Chris Gray <christophermjgray@gmail.com>
2021-02-16 11:12:45 -0500

    Update install_python3.sh

--- install_python3.sh
+++ install_python3.sh
@@ -19,8 +19,8 @@ echo "
                             |_|                                             |___|
 
 \r\n \r\n
-Version:  1.4.11                             \r\n
-Last Updated:  9/10/2020
+Version:  1.4.12                             \r\n
+Last Updated:  2/16/2021
 \r\n \r\n"
 #sudo -E apt-get update
 #wait
@@ -28,22 +28,22 @@ Last Updated:  9/10/2020
 #wait
 #echo "Downloading required dependencies...\r\n\r\n"
 #--------------------------------------------------------------------------------------------
-echo "Installing Python 3.8+ latest... "
-
-# deadsnakes - Not needed in Ubuntu 20.04
-#sudo -E add-apt-repository -y ppa:deadsnakes/ppa
-#sudo -E apt-get update
+echo "Installing Python 3.9+ latest... "
+# deadsnakes
+sudo -E add-apt-repository -y ppa:deadsnakes/ppa
+sudo -E apt-get update
 #--------------------------------------------------------------------------------------------
 sudo apt --fix-broken install -y python-pycurl python-apt
-sudo apt-get install -y software-properties-common
+sudo -E apt-get install -y software-properties-common
 sudo -E apt-get install -y libtiff5-dev libjpeg8-dev zlib1g-dev liblcms2-dev libwebp-dev tcl8.6-dev tk8.6-dev python-tk
 sudo -E apt-get install -y libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
 sudo -E apt-get install -y build-essential checkinstall libgmp3-dev python-software-properties python3-yaml
-sudo -E apt-get install -y python3-dev python3.8-dev
-sudo -E apt-get install -y python3.8 python3.8-venv python3.8-doc binfmt-support
+sudo -E apt-get install -y python3.9 python3.9-dev python3.9-venv python3.9-doc binfmt-support
+sudo -E apt install -y tox
 
 #-- sometimes need pythonn 3.6 --
-sudo -E apt-get install -y libpython3.6-dev
+# sudo -E apt-get install -y libpython3.6-dev
+#sudo -E apt-get install -y python3-dev
 
 python3 -V
 #----------- if issues with PIP install -------------------
@@ -60,7 +60,6 @@ pip3 install --upgrade pip
 python3 -m pip install --upgrade pip
 #--------------------------------------------------------------------------------------------
 pip3 install pip
-
 #----- Install Flask ------------
 #echo "Installing Flask... \r\n "
 #pip3 install Flask flask_restful flask_apscheduler flask_marshmallow flask_migrate flask_socketio
@@ -70,7 +69,19 @@ pip3 install requests
 #pip3 install bleach
 #-----------------------
 echo "Installing other PIP modules... https://hugovk.github.io/top-pypi-packages/ \r\n "
+#--- crypto ---
+pip3 install PyNaCl
 pip3 install simp-AES
+pip3 install simple_aes
+pip3 install bcrypt
+pip3 install blake2b-py
+pip3 install blake2
+pip3 install chacha20poly1305
+pip3 install curve25519
+pip3 install siphashc
+pip3 install hkdf
+pip3 install ecdsa
+#--- others ---
 pip3 install pynacl
 pip3 install urllib3
 pip3 install requests
