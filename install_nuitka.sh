d9cbb396d9b9d8f0de897a028659a3ef95bca546
Chris Gray <christophermjgray@gmail.com>
2021-02-16 12:05:32 -0500

    Update install_nuitka.sh

--- install_nuitka.sh
+++ install_nuitka.sh
@@ -7,7 +7,7 @@
 #
 clear
 echo "
-Version:  0.0.1          \r\n
+Version:  0.0.2          \r\n
 Last Updated:  2/16/2021
 \r\n \r\n
 This is meant for Ubuntu 18.04+ \r\n \r\n
@@ -25,3 +25,7 @@ wget -O - http://nuitka.net/deb/archive.key.gpg | apt-key add -
 echo >/etc/apt/sources.list.d/nuitka.list "deb http://nuitka.net/deb/stable/$CODENAME $CODENAME main"
 sudo -E apt-get update
 apt-get install -y nuitka
+
+echo "Updating PyPI... "
+#pip install -U nuitka
+pip3 install -U nuitka
