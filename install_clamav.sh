b3f2ef62f0a05a77ceb463e4a94c69f91a4e5d25
Chris Gray <christophermjgray@gmail.com>
2021-04-21 01:19:32 -0400

    Create install_clamav.sh

---
+++ install_clamav.sh
@@ -0,0 +1,48 @@
+
+#!/bin/sh
+#    If you update this from Windows, using Notepad ++, do the following:
+#       sudo apt-get -y install dos2unix
+#       dos2unix <FILE>
+#       chmod u+x <FILE>
+#
+
+clear
+echo "
+ _____             _         _    _          _
+|     |___ ___ ___| |_ ___ _| |  | |_ _ _   |_|
+|   --|  _| -_| .'|  _| -_| . |  | . | | |   _
+|_____|_| |___|__,|_| |___|___|  |___|_  |  |_|
+                                     |___|
+
+ _____ _       _     _           _              _____    __    _____
+|     | |_ ___|_|___| |_ ___ ___| |_ ___ ___   |     |__|  |  |   __|___ ___ _ _
+|   --|   |  _| |_ -|  _| . | . |   | -_|  _|  | | | |  |  |  |  |  |  _| .'| | |
+|_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
+                            |_|                                             |___|
+\r\n \r\n
+
+
+This really is meant to be run under Ubuntu 20.04 LTS +
+\r\n \r\n
+Version:  0.0.1                             \r\n
+Last Updated:  4/21/2021
+\r\n \r\n"
+
+sudo apt-get update -y
+ß
+sudo apt-get install clamav clamav-daemon -y
+sudo systemctl stop clamav-freshclam
+sudo freshclam
+sudo systemctl start clamav-freshclam
+sudo systemctl enable clamav-freshclam
+
+
+# do a full scan!
+#sudo clamscan --infected --remove --recursive /
+sudo clamscan --infected --remove --recursive /home/ubuntu
+sudo clamscan --infected --remove --recursive /var/log/
+sudo clamscan --infected --remove --recursive /media/
+sudo clamscan --infected --remove --recursive /etc/
+
+
+echo "DONE! \r\n \r\n"
