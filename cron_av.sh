b10627450321af56461f336d7aa2de3e44b3ee42
Chris Gray <christophermjgray@gmail.com>
2021-04-21 09:38:08 -0400

    Update cron_av.sh

--- cron_av.sh
+++ cron_av.sh
@@ -4,6 +4,11 @@
 #       dos2unix <FILE>
 #       chmod u+x <FILE>
 #
+#
+#   Crontab: Run every Saturday morning at 2:30 AM
+#       30 2 * * 6 /root/cron_av.sh 2>&1 &
+#
+
 clear
 sudo systemctl stop clamav-freshclam
 echo "\r\n \r\n Updating ClamAV \r\n \r\n"
@@ -14,25 +19,34 @@ sudo systemctl enable clamav-freshclam
 # Do a full scan!
 #sudo clamscan --infected --remove --recursive /
 
-echo "\r\n \r\n Scanning: /home/ \r\n \r\n"
-sudo clamscan --infected --remove --recursive /home/
-echo "\r\n \r\n Scanning: /var/ \r\n \r\n"
-sudo clamscan --infected --remove --recursive /var/
-echo "\r\n \r\n Scanning: /media/ \r\n \r\n"
-sudo clamscan --infected --remove --recursive /media/
-echo "\r\n \r\n Scanning: /etc/ \r\n \r\n"
-sudo clamscan --infected --remove --recursive /etc/
-echo "\r\n \r\n Scanning: /tmp/ \r\n \r\n"
-sudo clamscan --infected --remove --recursive /tmp/
-echo "\r\n \r\n Scanning: /sys/ \r\n \r\n"
-sudo clamscan --infected --remove --recursive /sys/
 echo "\r\n \r\n Scanning: /root/ \r\n \r\n"
-sudo clamscan --infected --remove --recursive /root/
+sudo clamscan --max-filesize=3999M --max-scansize=3999M --infected --remove --recursive /root/
+
+echo "\r\n \r\n Scanning: /home/ \r\n \r\n"
+sudo clamscan --max-filesize=3999M --max-scansize=3999M --infected --remove --recursive /home/
+
+echo "\r\n \r\n Scanning: /tmp/ \r\n \r\n"
+sudo clamscan --max-filesize=3999M --max-scansize=3999M --infected --remove --recursive /tmp/
+
+echo "\r\n \r\n Scanning: /etc/ \r\n \r\n"
+sudo clamscan --max-filesize=3999M --max-scansize=3999M --infected --remove --recursive /etc/
+
+echo "\r\n \r\n Scanning: /media/ \r\n \r\n"
+sudo clamscan --max-filesize=3999M --max-scansize=3999M --infected --remove --recursive /media/
+
+echo "\r\n \r\n Scanning: /var/ \r\n \r\n"
+sudo clamscan --max-filesize=3999M --max-scansize=3999M --infected --remove --recursive /var/
+
+#echo "\r\n \r\n Scanning: /sys/ \r\n \r\n"
+#sudo clamscan --infected --remove --recursive /sys/
+
 echo "\r\n \r\n Scanning: /opt/ \r\n \r\n"
-sudo clamscan --infected --remove --recursive /opt/
+sudo clamscan --max-filesize=3999M --max-scansize=3999M --infected --remove --recursive /opt/
+
 echo "\r\n \r\n Scanning: /snap/ \r\n \r\n"
-sudo clamscan --infected --remove --recursive /snap/
+sudo clamscan --max-filesize=3999M --max-scansize=3999M --infected --remove --recursive /snap/
+
 echo "\r\n \r\n Scanning: /mnt/ \r\n \r\n"
-sudo clamscan --infected --remove --recursive /mnt/
+sudo clamscan --max-filesize=3999M --max-scansize=3999M --infected --remove --recursive /mnt/
 
 echo "DONE! \r\n \r\n"
