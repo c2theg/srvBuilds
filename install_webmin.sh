3941ee095376590390dc849849a4c5309c29bca4
Chris Gray <christophermjgray@gmail.com>
2021-08-11 23:23:28 -0400

    Update install_webmin.sh

--- install_webmin.sh
+++ install_webmin.sh
@@ -2,13 +2,12 @@
 clear
 
 
-Version='1.979'
 echo "
-Version:  1.3.3
+Version:  2.0.0
 Last Updated:  8/11/2021
 
 
-This will install version  $Version of Webmin \r\n \r\n
+This will install the latest version of Webmin
 Updating system first..."
 
 sudo -E apt-get update
@@ -19,7 +18,9 @@ echo "Downloading required dependencies...\r\n\r\n"
 #--------------------------------------------------------------------------------------------
 sudo apt-get -y install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions
 wait
-wget "http://prdownloads.sourceforge.net/webadmin/webmin_${Version}_all.deb"
+#wget "http://prdownloads.sourceforge.net/webadmin/webmin_${Version}_all.deb"
+wget https://www.webmin.com/download/deb/webmin-current.deb
 wait
-sudo dpkg --install webmin_${Version}_all.deb
+#sudo dpkg --install webmin_${Version}_all.deb
+sudo dpkg --install webmin-current.deb
 echo "Done! \r\n \r\n"
