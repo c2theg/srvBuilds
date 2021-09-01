00676225264bdeb3ff1799288b8bbe1d78d411cf
Chris Gray <christophermjgray@gmail.com>
2021-09-01 17:45:54 -0400

    Update install_monitoring.sh

--- install_monitoring.sh
+++ install_monitoring.sh
@@ -2,18 +2,26 @@
 #
 clear
 echo "
-Version:  0.0.6         \r\n
-Last Updated:  8/22/2021
+Version:  0.0.7         \r\n
+Last Updated:  9/1/2021
 \r\n \r\n
 Updating system first..."
 sudo -E apt-get update
 wait
 sudo -E apt-get upgrade -y
 wait
-sudo -E apt-get install -y cockpit
-sudo systemctl start cockpit && sudo systemctl enable cockpit
 
+
+#--- Cockpit ---
+#- give cockpit access to docker api
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+#----------------
+sudo -E apt-get -y install cockpit
+sudo systemctl start cockpit && sudo systemctl enable cockpit
 echo "Visit: https://<ServerIP>:9090 \r\n \r\n"
+
 #-----------------------
 echo "\r\n \r\n Install Webmin \r\n \r\n "
 wait
