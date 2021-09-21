e7c72e9030f092beeda87b2a2f33773f05cd64e2
Chris Gray <christophermjgray@gmail.com>
2021-09-21 13:42:40 -0400

    Update install_monitoring.sh

--- install_monitoring.sh
+++ install_monitoring.sh
@@ -16,7 +16,7 @@ wait
 #- give cockpit access to docker api
 sudo groupadd docker
 sudo usermod -aG docker $USER
-newgrp docker
+#newgrp docker
 #----------------
 sudo -E apt-get -y install cockpit
 sudo systemctl start cockpit && sudo systemctl enable cockpit
@@ -26,6 +26,6 @@ echo "Visit: https://<ServerIP>:9090 \r\n \r\n"
 echo "\r\n \r\n Install Webmin \r\n \r\n "
 wait
 wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/install_webmin.sh && chmod u+x install_webmin.sh && sudo ./install_webmin.sh
-sleep 10
+sleep 2
 wait
 rm install_webmin.sh
