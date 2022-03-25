03c08d75c276d5513963e5517bdd412f09c11d99
Chris Gray <christophermjgray@gmail.com>
2022-03-24 23:44:21 -0400

    Update install_openssl.sh

--- install_openssl.sh
+++ install_openssl.sh
@@ -4,51 +4,53 @@
 #
 clear
 echo "
-Version:  0.0.7          \r\n
-Last Updated:  9/17/2021
+Version:  0.0.8          \r\n
+Last Updated:  3/24/2022
 \r\n \r\n
 This is meant for Ubuntu 20.04+ \r\n \r\n
 Updating system first..."
 #sudo -E apt-get update
 #wait
 #sudo -E apt-get upgrade -y
-sudo apt-get install -y ca-certificates wget curl
+sudo apt-get install -y ca-certificates
 sudo update-ca-certificates
 
 #--- clean up any previous tries ---
-rm openssl-3.0.0.tar.gz.*
-rm openssl-3.0.0.tar.gz.sha256.*
+rm openssl-3.0.2.tar.gz.*
+rm openssl-3.0.2.tar.gz.sha256.*
 #--------------------------------------------------------------------------------------------
 # https://code.luasoftware.com/tutorials/linux/upgrade-openssl-on-ubuntu-20/
 openssl version -a
-sudo cp -R /usr/lib/ssl /usr/lib/ssl-1.1.1-bk
+sudo cp -R /usr/lib/ssl /usr/lib/ssl-prev-version-bk
 
 cd /home/ubuntu/
 
 # get the latest version
-wget --no-check-certificate https://www.openssl.org/source/openssl-3.0.0.tar.gz
-wget --no-check-certificate https://www.openssl.org/source/openssl-3.0.0.tar.gz.sha256
-echo "$(cat openssl-3.0.0.tar.gz.sha256) openssl-3.0.0.tar.gz" | sha256sum --check
+wget --no-check-certificate https://www.openssl.org/source/openssl-3.0.2.tar.gz
+wget --no-check-certificate https://www.openssl.org/source/openssl-3.0.2.tar.gz.sha256
+echo "$(cat openssl-3.0.2.tar.gz.sha256) openssl-3.0.2.tar.gz" | sha256sum --check
 
 echo "\r\n \r\n ---- Installing ---- \r\n \r\n "
 
-tar -zxf openssl-3.0.0.tar.gz
-cd openssl-3.0.0
+tar -zxf openssl-3.0.2.tar.gz
+cd openssl-3.0.2
 ./config
 make
 make test
 sudo make install
 
 #-- Change symbolic link
-sudo mv /usr/bin/openssl /usr/bin/openssl-3.0.0
+sudo mv /usr/bin/openssl /usr/bin/openssl-3.0.2
 sudo ln -s /usr/local/bin/openssl /usr/bin/openssl
 
 sudo ldconfig
 
 openssl version -a
 
-echo "\r\n \r\n DONE \r\n \r\n "
 
 cd ..
-rm openssl-3.0.0.tar.gz
-rm openssl-3.0.0.tar.gz.sha256
+rm openssl-3.0.2.tar.gz
+rm openssl-3.0.2.tar.gz.sha256
+
+
+echo "\r\n \r\n DONE \r\n \r\n "
