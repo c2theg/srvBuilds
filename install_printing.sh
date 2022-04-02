2fb30eff958b9d5e09fd81fd3c074387bc3107d7
Chris Gray <christophermjgray@gmail.com>
2022-04-02 01:54:20 -0400

    Create install_printing.sh

---
+++ install_printing.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+clear
+echo "
+ _____             _         _    _          _
+|     |___ ___ ___| |_ ___ _| |  | |_ _ _   |_|
+|   --|  _| -_| .'|  _| -_| . |  | . | | |   _
+|_____|_| |___|__,|_| |___|___|  |___|_  |  |_|
+                                     |___|
+
+ _____ _       _     _           _              _____    __    _____
+|     | |_ ___|_|___| |_ ___ ___| |_ ___ ___   |     |__|  |  |   __|___ ___ _ _
+|   --|   |  _| |_ -|  _| . | . |   | -_|  _|  | | | |  |  |  |  |  |  _| .'| | |
+|_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
+                            |_|                                             |___|
+\r\n \r\n
+
+
+This really is meant to be run under Ubuntu 20.04 LTS +
+\r\n \r\n
+Version:  0.0.2                             \r\n
+Last Updated:  4/2/2022
+\r\n \r\n"
+
+sudo apt-get update -y
+
+sudo apt-get install -y cups cups-bsd printer-driver-cups-pdf unpaper
+
+# zeroconf imagemagic
