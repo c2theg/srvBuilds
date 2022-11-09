c0c4757918ae38ab1a7585f85db767828b928b4f
Chris Gray <christophermjgray@gmail.com>
2022-11-08 16:43:12 -0800

    Update install_pihole.sh

--- install_pihole.sh
+++ install_pihole.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
-
+#
+#
+#
 # https://github.com/pi-hole/docker-pi-hole/blob/master/README.md
-# --- Modified DNS Servers - Chris Gray - 11/8/2022
+# https://github.com/pi-hole/docker-pi-hole/#running-pi-hole-docker
+#
+# --- Modified DNS Servers - Chris Gray - 11/8/2022 - version 0.04
 #       -> Cloudflare: https://blog.cloudflare.com/introducing-1-1-1-1-for-families/
 #       -> OpenDNS: https://support.opendns.com/hc/en-us/community/posts/360035396952-Family-Shield-IPv6-DNS-address-
+# --dns=127.0.0.1
+
+#--- On Ubuntu, disable caching DNS stub resolver
+sudo sed -r -i.orig 's/#?DNSStubListener=yes/DNSStubListener=no/g' /etc/systemd/resolved.conf
+sudo sh -c 'rm /etc/resolv.conf && ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf'
+systemctl restart systemd-resolved
+
+#----- Normal install below ----------
 
 PIHOLE_BASE="${PIHOLE_BASE:-$(pwd)}"
 [[ -d "$PIHOLE_BASE" ]] || mkdir -p "$PIHOLE_BASE" || { echo "Couldn't create storage directory: $PIHOLE_BASE"; exit 1; }
@@ -12,11 +24,11 @@ PIHOLE_BASE="${PIHOLE_BASE:-$(pwd)}"
 docker run -d \
     --name pihole \
     -p 53:53/tcp -p 53:53/udp \
-    -p 80:80 \
-    -e TZ="America/Chicago" \
+    -p 8080:80 \
+    -e TZ="America/New_York" \
     -v "${PIHOLE_BASE}/etc-pihole:/etc/pihole" \
     -v "${PIHOLE_BASE}/etc-dnsmasq.d:/etc/dnsmasq.d" \
-    --dns=127.0.0.1 --dns=1.1.1.2 --dns=208.67.222.222 --dns=2606:4700:4700::1113 --dns=::ffff:d043:dc7b \
+    --dns=1.1.1.2 --dns=208.67.222.222 --dns=2606:4700:4700::1113 --dns=::ffff:d043:dc7b \
     --restart=unless-stopped \
     --hostname pi.hole \
     -e VIRTUAL_HOST="pi.hole" \
