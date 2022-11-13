f70a6703f5ad8310293a644b87e1d4c117ea6635
Chris Gray <christophermjgray@gmail.com>
2022-11-13 11:34:12 -0500

    Update setup_security_firewall.sh

--- setup_security_firewall.sh
+++ setup_security_firewall.sh
@@ -1,53 +1,73 @@
+# Sources:
+#   https://www.digitalocean.com/community/tutorials/how-to-set-up-a-firewall-with-ufw-on-ubuntu-14-04
+#
+#
+#------------------------------------------------------------------------------------
+
 sudo apt-get update
 sudo apt-get install wget git nano ufw
 
-sudo ufw status
+sudo ufw status verbose
 sudo ufw app list
-#---------------------------------------------------------------------------------------------------------
+
+#sudo ufw default deny incoming
+#sudo ufw default allow outgoing
+#------------------------------------------------------------------------------------
 #--- SSH ---
 sudo ufw allow from 10.0.0.0/8 proto tcp to any port 22
 sudo ufw allow from 172.16.0.0/12 proto tcp to any port 22
 sudo ufw allow from 192.168.0.0/16 proto tcp to any port 22
-#sudo ufw allow OpenSSH
-#sudo ufw allow 22
-#sudo ufw allow “OpenSSH”
+
+
+#--- HTTP/s ---
+sudo ufw allow proto tcp from any to any port 80/tcp
+sudo ufw allow proto tcp from any to any port 443 # both UDP (HTTP3 / Quik) and TCP 
 
 
 #--- SNMP ---
-sudo ufw allow 161
-sudo ufw allow 162
+#sudo ufw allow from 10.0.0.0/8 proto tcp to any port 161,162
+#sudo ufw allow from 172.16.0.0/12 proto tcp to any port 161,162
+#sudo ufw allow from 192.168.0.0/16 proto tcp to any port 161,162
 
 
-#--- HTTP /s ---
-#sudo ufw allow "Nginx HTTPS"
-sudo ufw allow proto tcp from any to any port 80,443
-#sudo ufw allow 80
-#sudo ufw allow 443
-#sudo ufw allow 80/tcp
-#sudo ufw allow 80/udp
-#sudo ufw allow 443/tcp
-#sudo ufw allow 443/udp
+#-- NTP Server ---
+#sudo ufw allow from 10.0.0.0/8 proto tcp to any port 123
+#sudo ufw allow from 172.16.0.0/12 proto tcp to any port 123
+#sudo ufw allow from 192.168.0.0/16 proto tcp to any port 123
+#sudo ufw allow 123
 
 
-#--- BTSync ---
-#sudo ufw allow 8888
-#sudo ufw allow 8888/tcp
-#sudo ufw allow 8888/udp
+#--- Management (Webmin, Cockpit) ---
+#sudo ufw allow from 10.0.0.0/8 proto tcp to any port 10000,9090
+#sudo ufw allow from 172.16.0.0/12 proto tcp to any port 10000,9090
+#sudo ufw allow from 192.168.0.0/16 proto tcp to any port 10000,9090
+
+
+#--- Databases ----
+#- MySQL -
+#sudo ufw allow from 10.0.0.0/8 proto tcp to any port 3306
+#sudo ufw allow from 172.16.0.0/12 proto tcp to any port 3306
+#sudo ufw allow from 192.168.0.0/16 proto tcp to any port 3306
+
+#- MongoDB -
+#sudo ufw allow from 10.0.0.0/8 proto tcp to any port 27017,27018,27019
+#sudo ufw allow from 172.16.0.0/12 proto tcp to any port 27017,27018,27019
+#sudo ufw allow from 192.168.0.0/16 proto tcp to any port 27017,27018,27019
+
+#- Redis -
+#sudo ufw allow from 10.0.0.0/8 proto tcp to any port 6379,16379,26379
+#sudo ufw allow from 172.16.0.0/12 proto tcp to any port 6379,16379,26379
+#sudo ufw allow from 192.168.0.0/16 proto tcp to any port 6379,16379,26379
+
+
+#--- Resilio ---
+#sudo ufw allow from 10.0.0.0/8 proto tcp to any port 8888/tcp
+#sudo ufw allow from 172.16.0.0/12 proto tcp to any port 8888/tcp
+#sudo ufw allow from 192.168.0.0/16 proto tcp to any port 8888/tcp
 #sudo ufw allow 3000
 #sudo ufw allow 3000/tcp
 #sudo ufw allow 3000/udp
 
 
-#-- NTP ---
-#sudo ufw allow 123
-#sudo ufw allow 123/tcp
-#sudo ufw allow 123/udp
-
-
-#--- Databases ----
-sudo ufw allow from 10.0.0.0/8 proto tcp to any port 3306
-sudo ufw allow proto tcp from 10.0.0.0/8 to any port 27017,27018,27019
-sudo ufw allow proto tcp from 10.0.0.0/8 to any port 6379,16379,26379
-
-
+#--- Restart UFW ---
 sudo ufw reload
