8d1808cc86c163a4aabbc5a1a88cfc1fd2a77bac
Chris Gray <christophermjgray@gmail.com>
2022-11-14 14:01:25 -0800

    Update install_zfs.sh

--- install_zfs.sh
+++ install_zfs.sh
@@ -14,10 +14,17 @@ sudo -E apt-get upgrade -y
 wait
 echo "Downloading required dependencies...\r\n\r\n"
 #--------------------------------------------------------------------------------------------
-sudo apt install -y zfsutils-linux
+sudo apt install --yes zfsutils-linux
 
-sudo apt install -y zfs-initramfs
+sudo apt install --yes zfs-initramfs
 
+sudo apt install --yes debootstrap gdisk
+
+#-- Disable Autoshart of ZFS Volumes --
+sudo apt install --yes dbus-x11
+sudo dbus-launch gsettings set org.gnome.desktop.media-handling automount false
+
+#--- moving on ---
 clear
 echo "\r\n \r\n Done installing ZFS tools... \r\n \r\n "
 zpool list
