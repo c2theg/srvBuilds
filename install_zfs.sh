7874c7ec41e432228f51078babeeec04bbce7dfd
Chris Gray <christophermjgray@gmail.com>
2022-11-14 12:37:25 -0800

    Create install_zfs.sh

---
+++ install_zfs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+clear
+
+
+echo "
+Version:  0.0.8
+Last Updated:  11/14/2022
+This will install the latest version of Webmin
+Updating system first..."
+
+sudo -E apt-get update
+wait
+sudo -E apt-get upgrade -y
+wait
+echo "Downloading required dependencies...\r\n\r\n"
+#--------------------------------------------------------------------------------------------
+sudo apt install -y zfsutils-linux
+
+sudo apt install -y zfs-initramfs
+
+echo "\r\n \r\n Done installing ZFS tools... \r\n \r\n "
+
+zpool list
+
+sudo lsblk
+
+sudo fdisk -l
+
