10a6311079c464fe70dfd4bcb9e8618a2b164e96
Chris Gray <christophermjgray@gmail.com>
2022-12-22 20:42:09 -0500

    Update update_core.sh

--- update_core.sh
+++ update_core.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
-#    If you update this from Windows, using Notepad ++, do the following:
-#       sudo apt-get -y install dos2unix
-#       dos2unix <FILE>
-#       chmod u+x <FILE>
-#
-clear
+#clear
 # Absolute path to this script, e.g. /home/user/bin/foo.sh
 SCRIPT=$(readlink -f "$0")
 # Absolute path this script is in, thus /home/user/bin
@@ -26,17 +21,10 @@ Current working dir: $SCRIPTPATH \r\n \r\n
                             |_|                                             |___|
 
 
-Version:  1.6.6                             \r\n
-Last Updated:  8/22/2021
+Version:  1.6.7                             \r\n
+Last Updated:  12/22/2022
 \r\n \r\n"
-#sudo -E apt-get update
 wait
-#sudo -E apt-get upgrade -y
-#wait
-#echo "Freeing up space"
-#sudo apt-get autoremove -y
-wait
-#echo "Downloading required dependencies...\r\n\r\n"
 #--------------------------------------------------------------------------------------------
 echo "Checking Internet status...\r\n\r\n"
 #ping -q -c3 github.com > /dev/null
@@ -61,12 +49,12 @@ if nc -zw1 google.com 443; then
 		#------ under crontab -----
 		rm /root/sys_cleanup.sh
  		rm /root/update_ubuntu14.04.sh
- 		rm /root/install_common.sh
+ 		#rm /root/install_common.sh
 		rm /root/update_core.sh
-		rm /root/install_monitoring.*
+		#rm /root/install_monitoring.*
 		rm /root/sys_restart.*
-		rm /root/update_blocklists_local_servers.*
-		rm /root/update_time.sh
+		#rm /root/update_blocklists_local_servers.*
+		#rm /root/update_time.sh
 	fi
 	rm *.sh.1
 	echo "Downloading latest versions... \r\n\r\n"
@@ -74,33 +62,26 @@ if nc -zw1 google.com 443; then
 	sudo wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/update_ubuntu14.04.sh && chmod u+x update_ubuntu14.04.sh
 	sudo wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/sys_cleanup.sh && chmod u+x sys_cleanup.sh
 	sudo wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/sys_restart.sh && chmod u+x sys_restart.sh
-	sudo wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/install_common.sh && chmod u+x install_common.sh
-	sudo wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/install_monitoring.sh && chmod u+x install_monitoring.sh
-	sudo wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/update_blocklists_local_servers.sh && chmod u+x update_blocklists_local_servers.sh
-	sudo wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/update_time.sh && chmod u+x update_time.sh
-	sudo wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/update_kernel_u20-lt.sh && chmod u+x update_kernel_u20-lt.sh
+	#sudo wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/install_common.sh && chmod u+x install_common.sh
+	#sudo wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/install_monitoring.sh && chmod u+x install_monitoring.sh
+	#sudo wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/update_blocklists_local_servers.sh && chmod u+x update_blocklists_local_servers.sh
+	#sudo wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/update_time.sh && chmod u+x update_time.sh
+	#sudo wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/update_kernel_u20-lt.sh && chmod u+x update_kernel_u20-lt.sh
 	sudo wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/install_clamav.sh && chmod u+x install_clamav.sh
 	#wget -O - -q -t 1 --timeout=1 https://api.magnetoai.com/update_check.php?f=update_core > /dev/null
 	
-	if [ -s "install_docker.sh" ]; then
-		rm install_docker.sh
-		rm update_docker_images.sh
-		rm /root/update_docker_images.sh
-		sudo wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/docker/update_docker_images.sh && chmod u+x update_docker_images.sh
-		mv update_docker_images.sh /root/update_docker_images.sh
-	fi
 	#-----------------------------------------------
 	wait
 	if [ -d "/root/" ]; then
 		cp update_core.sh /root/update_core.sh
 		cp sys_cleanup.sh /root/sys_cleanup.sh
 		cp update_ubuntu14.04.sh /root/update_ubuntu14.04.sh
-		cp install_common.sh /root/install_common.sh
+#		cp install_common.sh /root/install_common.sh
 		cp sys_restart.sh /root/sys_restart.sh
-		cp install_monitoring.sh /root/install_monitoring.sh
-		cp update_blocklists_local_servers.sh /root/update_blocklists_local_servers.sh
-		cp update_time.sh /root/update_time.sh
-		cp install_clamav.sh /root/install_clamav.sh
+#		cp install_monitoring.sh /root/install_monitoring.sh
+#		cp update_blocklists_local_servers.sh /root/update_blocklists_local_servers.sh
+#		cp update_time.sh /root/update_time.sh
+#		cp install_clamav.sh /root/install_clamav.sh
 	fi
 	
 	wait
@@ -116,12 +97,6 @@ if nc -zw1 google.com 443; then
 	wait
 
 	sh /root/update_ubuntu14.04.sh
-#	sh /root/update_time.sh
-
-	if [ -s "/root/update_docker_images.sh" ]; then
-		echo "Updating all docker images.. "
-		sudo sh /root/update_docker_images.sh
-	fi
 else
 	echo "Not connected to the Internet. Fix that first and try again \r\n \r\n"
 fi
@@ -156,11 +131,4 @@ if [ -z "$Cron_output" ]; then
     (crontab -u root -l; echo "$line" ) | crontab -u root -
 fi
 
-Cron_output=$(crontab -l | grep "update_blocklists_local_servers.sh")
-#echo "The output is: [ $Cron_output ]"
-if [ -z "$Cron_output" ]; then
-    #-- Restart Server “At 03:13 on day-of-month 7.”
-    line="5 1 * * 6 /root/update_blocklists_local_servers.sh >> /var/log/update_blocklists_local_servers.log 2>&1"
-    (crontab -u root -l; echo "$line" ) | crontab -u root -
-fi
 echo "Done! \r\n \r\n"
