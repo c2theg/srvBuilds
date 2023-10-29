1b8777dd688976091f2d3cacb8cc5f61356f8f3e
Chris Gray <christophermjgray@gmail.com>
2023-10-29 10:01:35 -0400

    Update setup_mac_osx.sh

--- setup_mac_osx.sh
+++ setup_mac_osx.sh
@@ -17,6 +17,47 @@ echo "Running setup_mac_osx.sh
 \r\n \r\n
 Version:  0.0.1                            
 Last Updated:  10/29/2023
-\r\n \r\n"
+
+
+https://formulae.brew.sh/cask/
+
+
+"
 #--------------------------------------------------------------------------------------------
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+brew update --auto-update
+
+
+#--- install apps ---
+brew cask install docker
+brew install --cask docker-toolbox
+
+
+#--- CLI apps ---
 brew install gping
+brew install nload
+
+#--- GUI Apps ---
+
+#-- General --
+brew install --cask vlc
+brew install --cask macfuse
+brew install --cask veracrypt
+
+
+#--- Dev Apps ---
+brew install --cask postman
+brew install --cask sublime-text
+brew install --cask visual-studio-code
+brew install --cask cyberduck
+brew install tailscale
+brew install --cask transmission
+brew install nmap
+
+#-- databases --
+brew install --cask dbeaver-community
+brew install --cask sqlpro-for-sqlite
+
+#-- remove viewers --
+brew install --cask vnc-viewer
+brew install --cask microsoft-remote-desktop
