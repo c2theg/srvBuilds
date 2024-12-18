7baa08d53a85b0e7724cdf76c3b34760c04f7c1d
Chris Gray <christophermjgray@gmail.com>
2024-12-18 10:08:17 -0800

    Update install_tailscale.sh

--- install_tailscale.sh
+++ install_tailscale.sh
@@ -15,7 +15,7 @@ echo "
                             |_|                                             |___|
 
 \r\n \r\n
-Version:  0.0.10                            \r\n
+Version:  0.0.15                            \r\n
 Last Updated:  12/18/2024
 
 "
@@ -24,25 +24,21 @@ echo "Downloading required dependencies...\r\n\r\n"
 curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
 curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
 
-
 sudo -E apt-get update
 sudo -E apt-get install -y tailscale
-
 #-------------------------------------
-#sudo tailscale up
-sudo tailscale up --ssh
-
-echo "\r\n \r\n Your TailScale IP is: "
-tailscale ip -4
-tailscale ip -6
-tailscale netcheck
-tailscale status
-
 #--- Exit Node ---- https://tailscale.com/kb/1103/exit-nodes/?tab=linux
 echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
 echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
 sudo sysctl -p /etc/sysctl.d/99-tailscale.conf
 
-
+#-------------------------------------
+#sudo tailscale up --ssh
 sudo tailscale up --stateful-filtering=false --accept-routes --advertise-exit-node --advertise-routes=192.168.1.0/24 --ssh --accept-risk=lose-ssh
 tailscale set --auto-update
+
+echo "\r\n \r\n Your TailScale IP is:  \r\n"
+tailscale ip -4
+tailscale ip -6
+tailscale netcheck
+tailscale status
