4736be729cd1a971d4518c73c0455a398dca6137
Chris Gray <christophermjgray@gmail.com>
2025-01-20 09:50:12 -0500

    Create install_rust.sh

---
+++ install_rust.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+#
+clear
+echo "
+ _____             _         _    _          _                                   
+|     |___ ___ ___| |_ ___ _| |  | |_ _ _   |_|                                  
+|   --|  _| -_| .'|  _| -_| . |  | . | | |   _                                   
+|_____|_| |___|__,|_| |___|___|  |___|_  |  |_|                                  
+                                     |___|                                       
+                                                                                 
+ _____ _       _     _           _              _____    __    _____             
+|     | |_ ___|_|___| |_ ___ ___| |_ ___ ___   |     |__|  |  |   __|___ ___ _ _ 
+|   --|   |  _| |_ -|  _| . | . |   | -_|  _|  | | | |  |  |  |  |  |  _| .'| | |
+|_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
+                            |_|                                             |___|
+
+\r\n \r\n
+Version:  0.0.1                            \r\n
+Last Updated:  1/20/2025
+
+"
+echo "Downloading required dependencies...\r\n\r\n"
+#--------------------------------------------------------------------------------------------
+#curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+
+#sudo -E apt-get update
+#sudo -E apt-get install -y tailscale
+#-------------------------------------
+rustup update
+
+cargo build
+
+cargo run
+cargo test
+cargo doc
+cargo publish
+cargo --version
