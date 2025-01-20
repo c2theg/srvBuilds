9c01214939fcafedf76fd09c4688d09b9a01e2d3
Chris Gray <christophermjgray@gmail.com>
2025-01-20 11:40:00 -0500

    Update install_rust.sh

--- install_rust.sh
+++ install_rust.sh
@@ -19,21 +19,38 @@ Version:  0.0.1                            \r\n
 Last Updated:  1/20/2025
 
 "
+
+#-- update yourself! --
+rm install_rust.sh && wget https://raw.githubusercontent.com/c2theg/srvBuilds/refs/heads/master/install_rust.sh && chmod u+x install_rust.sh
+
+
 echo "Downloading required dependencies...\r\n\r\n"
 #--------------------------------------------------------------------------------------------
 #curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+rustc --version
 
-
-#sudo -E apt-get update
-#sudo -E apt-get install -y tailscale
+sudo -E apt-get update
 #-------------------------------------
 rustup update
-
 cargo build
-
 cargo run
 cargo test
 cargo doc
 cargo publish
 cargo --version
+
+cargo new hello_world
+cd hello_world
+
+#----- install libs ----------
+# https://tokio.rs/tokio/tutorial/setup
+cargo add tokio
+cargo add regex-syntax
+cargo add regex
+cargo add libc
+cargo add memchr
+
+#------ install programs ----------
+cargo install mini-redis
+
