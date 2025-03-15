f58f67ed610cb540e37c092ad5ae8550fb5795ca
Chris Gray <christophermjgray@gmail.com>
2025-03-15 14:05:25 -0700

    Create install_android_pi.sh

---
+++ install_android_pi.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+#
+clear
+echo "
+ _____             _         _    _          _                                   
+|     |___ ___ ___| |_ ___ _| |  | |_ _ _   |_|                                  
+|   --|  _| -_| .'|  _| -_| . |  | . | | |   _                                   
+|_____|_| |___|__,|_| |___|___|  |___|_  |  |_|                                  
+                                     |___|                                       
+                                                                                 
+ _____ _       _     _           _              _____    __    _____             
+|     | |_ ___|_|___| |_ ___ ___| |_ ___ ___   |     |__|  |  |   __|___ ___ _ _ 
+|   --|   |  _| |_ -|  _| . | . |   | -_|  _|  | | | |  |  |  |  |  |  _| .'| | |
+|_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
+                            |_|                                             |___|
+
+\r\n \r\n
+Version:  0.0.1                            \r\n
+Last Updated:  3/15/2025
+
+"
+#--------------------------------------------------------------------------------------------
+
+echo "
+First time setup / downloads 
+
+
+
+https://konstakang.com/devices/rpi5/
+https://konstakang.com/devices/rpi5/LineageOS22/
+
+
+
+Widevine L3 - for Netflix, Spotify etc.
+https://app.filen.io/#/d/46c3e9e7-a0d0-4248-aa2e-9a7646e77176%233yBNf8wSfI4HOrrdK8APuC2eF4Mjz3A3
+
+
+Google Apps
+https://github.com/MindTheGapps/15.0.0-arm64/releases/latest
+
+
+
+"
