ff23aa4f7a2d4288fb0d0ce5b228578bd0551434
Chris Gray <christophermjgray@gmail.com>
2025-05-27 13:06:11 -0400

    Update sys_cleanup.sh

--- sys_cleanup.sh
+++ sys_cleanup.sh
@@ -17,8 +17,8 @@ echo "
                             |_|                                             |___|
 
 \r\n \r\n
-Version:  1.9.9                             \r\n
-Last Updated:  5/16/2025
+Version:  1.9.12                             \r\n
+Last Updated:  5/27/2025
 --- Github: 
    wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/sys_cleanup.sh
 
@@ -36,8 +36,6 @@ If you get the error:  (raw.githubusercontent.com)... failed: Temporary failure 
 
 Do the following:
     cd / 
-
-
 "
 
 rm sys_cleanup.sh && wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/sys_cleanup.sh && chmod u+x sys_cleanup.sh
@@ -112,23 +110,37 @@ rm /var/log/aptitude.*
 rm /var/log/vmware-vmsvc.*
 rm /var/log/apt/term.log.*
 rm /var/log/apt/history.log.*
-rm /var/log/unattended-upgrades/unattended-upgrades-dpkg_*
-rm /var/log/unattended-upgrades/unattended-upgrades.log.*
+
+if [ -d "/var/log/unattended-upgrades/" ]; then
+    rm -rf /var/log/unattended-upgrades/*
+    #rm -rf /var/log/unattended-upgrades/unattended-upgrades-dpkg_*
+    #rm -rf /var/log/unattended-upgrades/unattended-upgrades.log.*
+fi
+
 rm -rf /var/log/upstart/*
 rm -rf /var/log/syslog
 rm -rf /var/log/messages
-rm /var/log/vmware-vmsvc-root.*
-rm -rf /var/log/unattended-upgrades/*
+
+
 rm /var/log/kern.log
 rm /var/log/ubuntu-advantage.log.*
 rm /var/log/ubuntu-advantage-timer.log.*
+
+rm /var/log/installer/*.log.*
+
 rm /var/log/vmware-vmsvc-root.*
 rm /var/log/vmware-vmtoolsd-root.*
+
+
 rm /var/log/dmesg.*
 rm /var/log/netserver.debug_*
-rm /var/log/samba/log.nmbd.*
-rm /var/log/samba/log.smbd.*
-rm /var/log/samba/log.*
+
+if [ -d "/var/log/samba/" ]; then
+    rm /var/log/samba/log.nmbd.*
+    rm /var/log/samba/log.smbd.*
+    rm /var/log/samba/log.*
+fi
+
 rm /var/log/apcupsd.events
 
 rm /var/log/install_clamav.log
@@ -142,111 +154,142 @@ rm /var/log/clamav/freshclam.log.*
 rm /var/log/fail2ban.log.*
 #rm /var/log/letsencrypt/letsencrypt.log.*
 #------ DBs ----------------------------------------------------------
-#rm -rf /var/log/mysql/*
-#rm /var/log/mysql.log.*
-#rm /var/log/mysql/mysql_error.log
-#rm /var/log/mysql/error.log
-#/etc/init.d/mysql restart
+if [ -d "/var/log/mongodb/" ]; then
+     rm -rf /var/log/mysql/*
+     rm /var/log/mysql.log.*
+     rm /var/log/mysql/mysql_error.log
+     rm /var/log/mysql/error.log
+     /etc/init.d/mysql restart
+fi
 
-rm /var/log/mongodb/*
-rm /var/log/mongdb/*
-rm /var/log/redis/*
+if [ -d "/var/log/mongodb/" ]; then
+    rm /var/log/mongodb/*
+fi
+
+if [ -d "/var/log/mongdb/" ]; then
+    rm /var/log/mongdb/*
+fi
+
+if [ -d "/var/log/redis/" ]; then
+    rm /var/log/redis/*
+    /etc/init.d/redis-server restart
+fi
 #rm /var/log/neo4j/*
 
-#-- Restart Services --
-# /etc/init.d/redis-server restart
 #------ ELK ----------------------------------------------------------
-#rm /var/log/kibana/*
-#systemctl restart kibana
+if [ -d "/var/log/kibana/" ]; then
+    rm /var/log/kibana/*
+    systemctl restart kibana
+fi
 
-#rm /var/log/logstash/*
-#rm /var/log/logstash/logstash-deprecation-*.log.gz
-#rm /var/log/logstash/logstash-plain-*.log.gz
-#systemctl restart logstash
+if [ -d "/var/log/logstash/" ]; then
+    rm /var/log/logstash/*
+    rm /var/log/logstash/logstash-deprecation-*.log.gz
+    rm /var/log/logstash/logstash-plain-*.log.gz
+    systemctl restart logstash
+fi
 
-#rm /var/log/elasticsearch/*
-#rm /var/log/elasticsearch/elasticsearch-*.log.gz
-#rm /var/log/elasticsearch/elasticsearch-*.json.gz
-#rm /var/log/elasticsearch/gc.log.*
-#rm /var/log/elasticsearch/elasticsearch_deprecation.log
-#rm /var/log/elasticsearch/elasticsearch_deprecation.json
-#/etc/init.d/elasticsearch restart
-#systemctl restart elasticsearch
-#systemctl status elasticsearch
+if [ -d "/var/log/elasticsearch/" ]; then
+    #-- Delete indexs---
+    #curl -X DELETE 'https://localhost:9200/_all'
+    #curl -X DELETE 'http://localhost:9200/*'
+    
+    #--  Delete  yesturday's  index --
+    #Yesterday=$(date -d "yesterday" '+%Y.%m.%d')
+    #echo "Deleting Yesterday's elasticsearch index...  http://localhost:9200/index-${Yesterday} ";
+    #curl -X DELETE "http://localhost:9200/index-${Yesterday}"
 
-#rm /var/log/metricbeat/*
+    rm /var/log/elasticsearch/*
+    rm /var/log/elasticsearch/elasticsearch-*.log.gz
+    rm /var/log/elasticsearch/elasticsearch-*.json.gz
+    rm /var/log/elasticsearch/gc.log.*
+    rm /var/log/elasticsearch/elasticsearch_deprecation.log
+    rm /var/log/elasticsearch/elasticsearch_deprecation.json
+    /etc/init.d/elasticsearch restart
+    systemctl restart elasticsearch
+    systemctl status elasticsearch
+    rm /var/log/metricbeat/*
+fi
 
-#-- Delete indexs---
-#curl -X DELETE 'https://localhost:9200/_all'
-#curl -X DELETE 'http://localhost:9200/*'
-
-#--  Delete  yesturday's  index --
-#Yesterday=$(date -d "yesterday" '+%Y.%m.%d')
-#echo "Deleting Yesterday's elasticsearch index...  http://localhost:9200/index-${Yesterday} ";
-#curl -X DELETE "http://localhost:9200/index-${Yesterday}"
 
 #------ Mail ----------------------------------------------------------
-rm /var/log/mail.log
-rm /var/log/mail.log.*
-rm /var/log/mail.err.*
-#rm -rf /var/mail/root/*
-#rm -rf /var/mail/www-data/*
-#rm -rf /var/mail/ubuntu/*
-#/etc/init.d/sendmail restart
-#--- wipe / purge all email que --
-postsuper -d ALL
-/etc/init.d/postfix restart
+if [ -d "/var/log/mail/" ]; then
+    rm /var/log/mail.log
+    rm /var/log/mail.log.*
+    rm /var/log/mail.err.*
+    #rm -rf /var/mail/root/*
+    #rm -rf /var/mail/www-data/*
+    #rm -rf /var/mail/ubuntu/*
+    #/etc/init.d/sendmail restart
+    #--- wipe / purge all email que --
+    postsuper -d ALL
+    /etc/init.d/postfix restart
+fi
 
-#------ Web / HTTP -------------------------------------------------------
-#rm /var/log/apache2/*
-#rm /var/log/lighttpd/*
+#------ Web / HTTP ---------------------------------------------
+if [ -d "/var/log/letsencrypt/" ]; then
+    rm -rf /var/log/letsencrypt/letsencrypt.log.*
+fi
+
+if [ -d "/var/log/apache2/" ]; then
+    rm /var/log/apache2/*
+fi
+if [ -d "/var/log/lighttpd/" ]; then   
+    rm /var/log/lighttpd/*
+fi
 #----------------------------------------------------------------
-echo "\r\n \r\n Removing Nginx and PHP logs, then restarting both services.. \r\n \r\n "
-rm -rf /var/log/nginx/*
-#rm /var/log/php5-fpm.log.*
-#rm /var/log/php7.4-fpm.log
-rm /var/log/php7.4-fpm.log.*
+if [ -d "/var/log/nginx/" ]; then
+    echo "\r\n \r\n Removing Nginx and PHP logs, then restarting both services.. \r\n \r\n "
+    rm -rf /var/log/nginx/*
+    #rm /var/log/php5-fpm.log.*
+    #rm /var/log/php7.4-fpm.log
+    rm /var/log/php7.4-fpm.log.*
+    
+    #rm /var/log/php8.0-fpm.log
+    rm /var/log/php8.0-fpm.log.*
+    
+    #rm /var/log/php8.1-fpm.log
+    rm /var/log/php8.1-fpm.log.*
 
-#rm /var/log/php8.0-fpm.log
-rm /var/log/php8.0-fpm.log.*
-
-#rm /var/log/php8.1-fpm.log
-rm /var/log/php8.1-fpm.log.*
-
-/etc/init.d/php7.4-fpm restart
-#/etc/init.d/php8.0-fpm restart
-/etc/init.d/php8.1-fpm restart
-
-rm -rf /var/log/letsencrypt/letsencrypt.log.*
-
-/etc/init.d/nginx restart
+    #--- restart services ---
+    /etc/init.d/php7.4-fpm restart
+    #/etc/init.d/php8.0-fpm restart
+    /etc/init.d/php8.1-fpm restart
+    /etc/init.d/nginx restart
+fi
 #------- PI-Hole / DNS releated ----------
-pihole -f
-wait
-sudo service pihole-FTL stop
-sudo service dnsmasq stop
-rm /var/log/dmesg.*
+if [ -d "/var/log/pihole/" ]; then
+    pihole -f
+    wait
+    sudo service pihole-FTL stop
+    sudo service dnsmasq stop
+    rm /var/log/dmesg.*
+    
+    rm /var/log/pihole/webserver.log.*
+    rm /var/log/pihole/pihole.log.*
+    rm /var/log/pihole/FTL.log.*
+    rm /var/log/pihole/pihole_updateGravity.log
+    
+    #kill $(lsof -t -i:53)
+    #wait
+    #/etc/init.d/lighttpd restart
+    /etc/init.d/dnsmasq restart
+    sudo service dnsmasq start
+    wait
+    sudo service pihole-FTL start
+    sudo systemctl restart pihole-FTL
+ 
+    #-------
+    #sudo service lighttpd status
+    #sudo service dnsmasq status
+    #sudo service pihole-FTL status
+    
+    rm /var/log/update_blocklists_local_servers.log
+    #rm /var/log/update_pihole_lists.log
 
-rm /var/log/pihole/webserver.log.*
-rm /var/log/pihole/pihole.log.*
-rm /var/log/pihole/FTL.log.*
-rm /var/log/pihole/pihole_updateGravity.log
+fi
 
-#kill $(lsof -t -i:53)
-#wait
-#/etc/init.d/lighttpd restart
-/etc/init.d/dnsmasq restart
-sudo service dnsmasq start
-wait
-sudo service pihole-FTL start
-sudo systemctl restart pihole-FTL
-#-------
-#sudo service lighttpd status
-#sudo service dnsmasq status
-#sudo service pihole-FTL status
 
-rm /var/log/update_blocklists_local_servers.log
-#rm /var/log/update_pihole_lists.log
 #---------- MISC ------------------------------------------------------
 rm /var/log/update_core.log
 rm /var/log/update_ubuntu.log
@@ -254,30 +297,22 @@ rm /var/log/sys_cleanup.log*
 
 rm /var/log/vmware-network.*
 rm /var/log/cloud-init.log
+
 #--- Resilio ---
-rm /var/lib/resilio-sync/sync.log
-rm /var/lib/resilio-sync/sync.log.*
+if [ -d "/var/lib/resilio-sync/" ]; then
+    rm /var/lib/resilio-sync/sync.log
+    rm /var/lib/resilio-sync/sync.log.*
+fi
+
 #----- DOCKER ------
-#echo " From: https://stackoverflow.com/questions/32723111/how-to-remove-old-and-unused-docker-images \r\n \r\n "
-# Clean up old containers before images..
-#docker ps --no-trunc -aqf "status=exited" | xargs docker rm
-#docker images --no-trunc -aqf "dangling=true" | xargs docker rmi
-#docker rm -v $(docker ps --filter status=exited -q 2>/dev/null) 2>/dev/null
-#docker rmi $(docker images --filter dangling=true -q 2>/dev/null) 2>/dev/null
-#echo "Delete all old images and any with a <none> tag. 
-#docker images | grep "<none>" | awk '{print $3}' | xargs -L1 docker rmi
-# This command is not good 
-#docker image prune -a
-#docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
-
-docker system prune -f
-docker image prune -f 
-docker volume rm $(docker volume ls -qf dangling=true)
-
-#-- docker logs --
-# cd /var/lib/docker/containers/
-# rm /var/lib/docker/containers/*/*.-json.log
-
+if [ -d "/var/lib/resilio-sync/" ]; then
+    # Clean up old containers before images..
+    docker system prune -f
+    docker image prune -f 
+    #-- docker logs --
+    # cd /var/lib/docker/containers/
+    rm /var/lib/docker/containers/*/*.log
+fi
 #----- End Docker ------
 history -c
 
