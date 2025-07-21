9009e507bfb6c542ea2fa5fdc6ef4c4f4276dfa0
Chris Gray <christophermjgray@gmail.com>
2025-07-21 13:14:28 -0400

    Create install_cockpit.sh

---
+++ install_cockpit.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+#
+clear
+echo "
+ _____             _         _    _          _                                   
+|     |___ ___ ___| |_ ___ _| |  | |_ _ _   |_|                                  
+|   --|  _| -_| .'|  _| -_| . |  | . | | |   _                                   
+|_____|_| |___|__,|_| |___|___|  |___|_  |  |_|                                  
+                                     |___|                                       
+                                                                                 
+ _____ _       _     _           _              _____    __    _____             
+|     | |_ ___|_|___| |_ ___ ___| |_ ___ ___   |     |__|  |  |   __|___ ___ _ _ 
+|   --|   |  _| |_ -|  _| . | . |   | -_|  _|  | | | |  |  |  |  |  |  _| .'| | |
+|_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
+                            |_|                                             |___|
+
+\r\n \r\n
+Version:  0.0.1
+Last Updated:  7/21/2025
+\r\n \r\n"
+#sudo -E apt-get update
+#wait
+#sudo -E apt-get upgrade -y
+#wait
+#echo "Downloading required dependencies...\r\n\r\n"
+#--------------------------------------------------------------------------------------------
+echo "Installing Cockpit
+
+"
+sudo apt install -t ${VERSION_CODENAME}-backports cockpit
+
+echo "
+
+
+Now visit:  https://IP_ADDRESS_OF_MACHINE:9090
+
+
+"
