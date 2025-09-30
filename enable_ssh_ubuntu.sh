03eeffe784b491e679106149e3e142d39db602e9
Chris Gray <christophermjgray@gmail.com>
2025-09-30 16:23:36 -0400

    Create enable_ssh_ubuntu.sh

---
+++ enable_ssh_ubuntu.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+#clear
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+#SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+#SCRIPTPATH=$(dirname "$SCRIPT")
+now=$(date)
+echo "Running enable_ssh_ubuntu.sh at $now \r\n
+Current working dir: $SCRIPTPATH \r\n \r\n
+ _____             _         _    _          _                                   
+|     |___ ___ ___| |_ ___ _| |  | |_ _ _   |_|                                  
+|   --|  _| -_| .'|  _| -_| . |  | . | | |   _                                   
+|_____|_| |___|__,|_| |___|___|  |___|_  |  |_|                                  
+                                     |___|                                       
+                                                                                 
+ _____ _       _     _           _              _____    __    _____             
+|     | |_ ___|_|___| |_ ___ ___| |_ ___ ___   |     |__|  |  |   __|___ ___ _ _ 
+|   --|   |  _| |_ -|  _| . | . |   | -_|  _|  | | | |  |  |  |  |  |  _| .'| | |
+|_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
+                            |_|                                             |___|
+
+
+Version:  0.0.1                             \r\n
+Last Updated:  9/30/2025
+\r\n \r\n"
+wait
+#--------------------------------------------------------------------------------------------
+#echo "Checking Internet status...\r\n\r\n"
+
+sudo apt update
+sudo apt install openssh-server -y    
+
+sudo systemctl enable ssh
+sudo systemctl start ssh
+
+sudo systemctl status ssh
+
+
+sudo ufw allow ssh
+sudo ufw reload
+
+
+ipconfig -a
+
+echo "Done. You should be able to ssh into this machine now ";
+    
