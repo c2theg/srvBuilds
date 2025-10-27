f1841dfc1b9e552759636513da6df2b06157e820
Chris Gray <christophermjgray@gmail.com>
2025-10-27 15:38:14 -0400

    Update version and enhance scanning script

--- install_clamav.sh
+++ install_clamav.sh
@@ -23,7 +23,7 @@ echo "
 
 This really is meant to be run under Ubuntu 20.04 LTS +
 \r\n \r\n
-Version:  0.0.26                             \r\n
+Version:  0.0.27                             \r\n
 Last Updated:  10/27/2025
 \r\n \r\n"
 
@@ -64,28 +64,56 @@ sudo rkhunter --check --skip-keypress
 #sudo clamscan --infected --remove --recursive /
 # clamscan --remove=yes -i -r ~/
 
-echo "\r\n \r\n Scanning: /home/ \r\n \r\n"
-sudo clamscan --infected --remove --recursive /home/
-echo "\r\n \r\n Scanning: /var/ \r\n \r\n"
-sudo clamscan --infected --remove --recursive /var/
-echo "\r\n \r\n Scanning: /media/ \r\n \r\n"
-sudo clamscan --infected --remove --recursive /media/
-echo "\r\n \r\n Scanning: /etc/ \r\n \r\n"
-sudo clamscan --infected --remove --recursive /etc/
+#--- public dirs ---
 echo "\r\n \r\n Scanning: /tmp/ \r\n \r\n"
 sudo clamscan --infected --remove --recursive /tmp/
-echo "\r\n \r\n Scanning: /sys/ \r\n \r\n"
-sudo clamscan --infected --remove --recursive /sys/
+
+#/var/tmp
+echo "\r\n \r\n Scanning: /var/ \r\n \r\n"
+sudo clamscan --infected --remove --recursive /var/
+
+# /dev/shm
+echo "\r\n \r\n Scanning: /dev \r\n \r\n"
+sudo clamscan --infected --remove --recursive /dev/
+
+
+#--- private dirs ---
+# /bin, /sbin, /usr/bin
+echo "\r\n \r\n Scanning: /bin \r\n \r\n"
+sudo clamscan --infected --remove --recursive /bin/
+
+echo "\r\n \r\n Scanning: /sbin/ \r\n \r\n"
+sudo clamscan --infected --remove --recursive /sbin/
+
+echo "\r\n \r\n Scanning: /usr/ \r\n \r\n"
+sudo clamscan --infected --remove --recursive /usr/
+
+echo "\r\n \r\n Scanning: /lib/ \r\n \r\n"
+sudo clamscan --infected --remove --recursive /lib/
+
+echo "\r\n \r\n Scanning: /etc/ \r\n \r\n"
+sudo clamscan --infected --remove --recursive /etc/
+
+#------- 
+echo "\r\n \r\n Scanning: /home/ \r\n \r\n"
+sudo clamscan --infected --remove --recursive /home/
+
 echo "\r\n \r\n Scanning: /root/ \r\n \r\n"
 sudo clamscan --infected --remove --recursive /root/
+
+echo "\r\n \r\n Scanning: /media/ \r\n \r\n"
+sudo clamscan --infected --remove --recursive /media/
+
+echo "\r\n \r\n Scanning: /sys/ \r\n \r\n"
+sudo clamscan --infected --remove --recursive /sys/
+
 echo "\r\n \r\n Scanning: /opt/ \r\n \r\n"
 sudo clamscan --infected --remove --recursive /opt/
+
 echo "\r\n \r\n Scanning: /snap/ \r\n \r\n"
 sudo clamscan --infected --remove --recursive /snap/
+
 echo "\r\n \r\n Scanning: /mnt/ \r\n \r\n"
 sudo clamscan --infected --remove --recursive /mnt/
 
 echo "DONE! \r\n \r\n"
-
-
-
