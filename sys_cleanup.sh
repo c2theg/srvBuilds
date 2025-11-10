c786c28dab836f4b2f33ca44c98396efb9192139
Chris Gray <christophermjgray@gmail.com>
2025-11-10 01:57:12 -0500

    Update version and enhance log cleanup script
    Updated version number from 1.9.22 to 1.9.23 and added cleanup for crypto.txt log file. Enhanced file size reporting for /var/log and home directories.

--- sys_cleanup.sh
+++ sys_cleanup.sh
@@ -17,7 +17,7 @@ echo "
                             |_|                                             |___|
 
 \r\n \r\n
-Version:  1.9.22                           \r\n
+Version:  1.9.23                           \r\n
 Last Updated:  11/9/2025
 --- Github: 
    wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/sys_cleanup.sh
@@ -149,6 +149,7 @@ rm /var/log/vmware-vmtoolsd-root.*
 
 rm /var/log/dmesg.*
 rm /var/log/netserver.debug_*
+rm /var/log/crypto.txt
 
 if [ -d "/var/log/samba/" ]; then
     rm /var/log/samba/log.nmbd.*
@@ -412,6 +413,25 @@ echo "\r\n \r\n Your best option is to restart the server to release these files
 
 echo "Find all files larger then 500mb... \r\n \r\n "
 find / -type f -size +500M
+
+
+echo "Finding largest files in /var/log 
+
+"
+sudo du -ah /var/log 2>/dev/null | sort -nr | head -n 10
+
+
+sudo du -ah / 2>/dev/null | sort -nr | head -n 10
+
+
+
+sudo du -sh /home/ubuntu
+
+
+sudo du -sh /root/
+
+
+sudo du -ah /root/ 2>/dev/null | sort -nr | head -n 10
 
 
 
