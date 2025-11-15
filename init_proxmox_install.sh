a25fe456da412fde86467fb7c6f06d9383bc32c7
Chris Gray <christophermjgray@gmail.com>
2025-11-15 12:30:37 -0500

    Create init_proxmox_install.sh for Proxmox setup
    Initial script to set up Proxmox installation and repositories.

---
+++ init_proxmox_install.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+#  Copyright © 2026 Christopher Gray 
+#--------------------------------------
+# Version:  0.0.1
+# Last Updated:  11/15/2025
+#--------------------------------------
+# download this file to /root/init_proxmox_install.sh
+# chmod +x /root/init_proxmox_install.sh
+# ./root/init_proxmox_install.sh
+
+
+
+# Username: root 
+# Password: The password you set during the Proxmox installation 
+# Access URL: https://<your-proxmox-ip>:8006 
+
+
+# https://pve.proxmox.com/wiki/Package_Repositories#sysadmin_no_subscription_repo
+
+
+# sudo nano /etc/apt/sources.list.d/proxmox.sources
+
+# add the following to the file
+echo "Types: deb
+URIs: http://download.proxmox.com/debian/pve
+Suites: trixie
+Components: pve-no-subscription
+Signed-By: /usr/share/keyrings/proxmox-archive-keyring.gpg" > /etc/apt/sources.list.d/proxmox.sources
+
+# cat > /etc/apt/sources.list.d/proxmox.sources <<EOF
+# Types: deb
+# URIs: http://download.proxmox.com/debian/pve
+# Suites: trixie
+# Components: pve-no-subscription
+# Signed-By: /usr/share/keyrings/proxmox-archive-keyring.gpg
+# EOF
+
+
+
+
+
+
+# add the following to the file
+# sudo nano /etc/apt/sources.list.d/ceph.sources 
+
+echo "Types: deb
+URIs: http://download.proxmox.com/debian/ceph-squid
+Suites: trixie
+Components: no-subscription
+Signed-By: /usr/share/keyrings/proxmox-archive-keyring.gpg" > /etc/apt/sources.list.d/ceph.sources
+
+
+
+
+
+#--- update system ---
+
+sudo apt update
+sudo apt-get install apt-transport-https
+sudo apt-get upgrade -y
+
+
+
+#https://github.com/CarmineCodes/Proxmox-No-Subscription-No-Problem
