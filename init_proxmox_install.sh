ec19abe0e924a0583e1b84ab32a1b28abf10185e
Chris Gray <christophermjgray@gmail.com>
2025-11-15 13:10:27 -0500

    Bump version to 0.0.10 and configure NTP servers
    Updated version number and added NTP server configurations.

--- init_proxmox_install.sh
+++ init_proxmox_install.sh
@@ -1,12 +1,63 @@
 #!/bin/bash
 #  Copyright © 2026 Christopher Gray 
 #--------------------------------------
-# Version:  0.0.2
+# Version:  0.0.10
 # Last Updated:  11/15/2025
 #--------------------------------------
 # wget https://raw.githubusercontent.com/c2theg/srvBuilds/refs/heads/master/init_proxmox_install.sh && chmod +x /root/init_proxmox_install.sh && /root/init_proxmox_install.sh
 
 
+wget https://raw.githubusercontent.com/c2theg/srvBuilds/refs/heads/master/sys_cleanup.sh && chmod +x /root/sys_cleanup.sh && /root/sys_cleanup.sh
+
+#-- Update Time (chronyd) --
+# https://raw.githubusercontent.com/c2theg/srvBuilds/refs/heads/master/configs/ntp.conf
+timedatectl set-timezone America/New_York
+#echo 'server 0.pool.ntp.org iburst' | sudo tee /etc/chrony/sources.d/nist.sources
+
+#--------- US based NTP servers ---------------------------
+#-- Cloudflare --
+echo 'server time.cloudflare.com iburst' | sudo tee /etc/chrony/sources.d/cloudflare.sources
+echo 'server 162.159.200.1 iburst' | sudo tee /etc/chrony/sources.d/cloudflare.sources
+echo 'server 162.159.200.123 iburst' | sudo tee /etc/chrony/sources.d/cloudflare.sources
+echo 'server 2606:4700:f1::1 iburst' | sudo tee /etc/chrony/sources.d/cloudflare.sources
+echo 'server 2606:4700:f1::123 iburst' | sudo tee /etc/chrony/sources.d/cloudflare.sources
+
+#-- Google --
+echo 'server time.google.com iburst' | sudo tee /etc/chrony/sources.d/google.sources
+echo 'server 216.239.35.4 iburst' | sudo tee /etc/chrony/sources.d/google.sources
+echo 'server 216.239.35.8 iburst' | sudo tee /etc/chrony/sources.d/google.sources
+echo 'server 2606:4700:f1::1 iburst' | sudo tee /etc/chrony/sources.d/google.sources
+echo 'server 2606:4700:f1::123 iburst' | sudo tee /etc/chrony/sources.d/google.sources
+
+#-- NIST --
+echo 'server time-d-g.nist.gov iburst' | sudo tee /etc/chrony/sources.d/nist.sources
+echo 'server time-d-wwv.nist.gov iburst' | sudo tee /etc/chrony/sources.d/nist.sources
+echo 'server time-d-b.nist.gov iburst' | sudo tee /etc/chrony/sources.d/nist.sources
+echo 'server time.nist.gov iburst' | sudo tee /etc/chrony/sources.d/nist.sources
+
+echo 'server 132.163.96.1 iburst' | sudo tee /etc/chrony/sources.d/nist.sources
+echo 'server 129.6.15.25 iburst' | sudo tee /etc/chrony/sources.d/nist.sources
+echo 'server 129.6.15.29 iburst' | sudo tee /etc/chrony/sources.d/nist.sources
+echo 'server 2610:20:6f97:97::4 iburst' | sudo tee /etc/chrony/sources.d/nist.sources
+echo 'server 2610:20:6f15:15::27 iburst' | sudo tee /etc/chrony/sources.d/nist.sources
+
+
+#---- Cloud NTP Servers ----
+#-- AWS --
+#echo 'server 169.254.169.123 iburst' | sudo tee /etc/chrony/sources.d/aws.sources
+#echo 'server fd00:ec2::123 iburst' | sudo tee /etc/chrony/sources.d/aws.sources
+#-- GCP --
+#echo 'server time.google.com iburst' | sudo tee /etc/chrony/sources.d/gcp.sources
+#echo 'server 216.239.32.15 iburst' | sudo tee /etc/chrony/sources.d/gcp.sources
+#-- Azure --
+#echo 'server time.windows.com iburst' | sudo tee /etc/chrony/sources.d/azure.sources
+
+
+systemctl restart chronyd
+# chronyc sources
+chronyc sources -v
+chronyc tracking
+chronyc activity
 #--------------------------------------
 # Username: root 
 # Password: The password you set during the Proxmox installation 
@@ -49,3 +100,13 @@ apt-get install apt-transport-https
 apt-get upgrade -y
 
 #https://github.com/CarmineCodes/Proxmox-No-Subscription-No-Problem
+
+apt-get -y install unattended-upgrades
+dpkg --configure -a
+sudo apt-get dist-upgrade -y
+
+
+#--- clean up ---
+apt-get autoclean -y
+apt-get autoremove -y
+apt autoremove -y
