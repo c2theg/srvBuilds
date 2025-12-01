84d79d811594897cd7fbc3ae3c883c1e00328e40
Chris Gray <christophermjgray@gmail.com>
2025-12-01 00:28:46 -0500

    Update install_ai.sh

--- install_ai.sh
+++ install_ai.sh
@@ -315,7 +315,19 @@ docker pull ghcr.io/open-webui/open-webui:ollama
 # docker run -d -p 3000:8080 -v ollama:/root/.ollama -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:ollama
 
 #-- GPU & CPU --
-# docker run -d -p 3000:8080 --gpus=all -v ollama:/root/.ollama -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:ollama
+# docker run -d -p 3000:8080 --gpus=all --add-host=host.docker.internal:host-gateway -v ollama:/root/.ollama -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:ollama
+
+
+#-- OpenUI - Models ---
+# https://ollama.com/library/gpt-oss
+# gpt-oss:20b
+
+# https://ollama.com/library/deepseek-r1
+# deepseek-r1:8b
+
+
+
+
 
 echo "
 
