658a2b48a463c8409d8ca33c48f6d158ec0551a9
Chris Gray <christophermjgray@gmail.com>
2025-12-03 00:14:21 -0500

    Create update_time2.sh

---
+++ update_time2.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+#clear
+now=$(date)
+echo "Running update_time.sh at $now
+
+ _____             _         _    _          _
+|     |___ ___ ___| |_ ___ _| |  | |_ _ _   |_|
+|   --|  _| -_| .'|  _| -_| . |  | . | | |   _
+|_____|_| |___|__,|_| |___|___|  |___|_  |  |_|
+                                     |___|
+
+ _____ _       _     _           _              _____    __    _____
+|     | |_ ___|_|___| |_ ___ ___| |_ ___ ___   |     |__|  |  |   __|___ ___ _ _
+|   --|   |  _| |_ -|  _| . | . |   | -_|  _|  | | | |  |  |  |  |  |  _| .'| | |
+|_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
+                            |_|                                             |___|
+
+
+
+
+Version:  0.0.1
+Last Updated:  12/3/2025
+
+
+"
+wait
+#--------------------------------------------------------------------------------------------
+sudo apt install chrony
+
+
+echo "
+
+Stopping Time Services...
+
+"
+
+#---- NTPDATE Service -------
+wget https://raw.githubusercontent.com/c2theg/srvBuilds/refs/heads/master/configs/chrony.conf && sudo cp chrony.conf /etc/chrony.conf && sudo systemctl restart chronyd
+
+
+systemctl restart chrony
+echo "Show status..
+
+"
+chronyc sources
+
+echo "
+
+Hardware Clock
+
+"
+sudo hwclock --show
+
+
+
+chronyc sources
