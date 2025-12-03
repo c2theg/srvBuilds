c22e562bb482121a7f8a2485249c0e2c387041cb
Chris Gray <christophermjgray@gmail.com>
2025-12-02 23:26:01 -0500

    Update install_common.sh

--- install_common.sh
+++ install_common.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#
 clear
 echo "
  _____             _         _    _          _                                   
@@ -17,10 +16,10 @@ echo "
 
 https://raw.githubusercontent.com/c2theg/srvBuilds/master/install_common.sh
 
-This really is meant to be run under Ubuntu 16.04 - 24.04 LTS
+This really is meant to be run under Ubuntu 16.04 - 24.04+ LTS
 
-Version:  1.7.24
-Last Updated:  11/3/2025
+Version:  1.7.26
+Last Updated:  12/2/2025
 
 Checking Internet status... 
 
@@ -47,7 +46,7 @@ if nc -zw1 google.com 443; then
 	#sudo -E apt-get install -y ifenslave # LACP - https://www.snel.com/support/how-to-set-up-lacp-bonding-on-ubuntu-18-04-with-netplan/
 
 	#--- Everything else ---
-	sudo -E apt-get install -y sysstat dos2unix
+	sudo -E apt-get install -y sysstat dos2unix cmake
 	sudo -E apt-get install -y ssh openssh-server openssl libssl-dev whois traceroute htop sshguard build-essential libffi-dev nfs-common nfs-kernel-server speedometer watchdog zip unzip
 	wait
 	sudo -E apt-get install -y ntp ntpdate linuxptp libicu-dev screen sysstat iptraf iperf3 iftop slurm tcptrack bmon nethogs nload parallel gnupg openssl libcurl4 curl net-tools
