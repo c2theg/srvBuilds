aac8151e39213dafe7858933df4f8facfa84fd92
Chris Gray <christophermjgray@gmail.com>
2025-12-02 23:40:19 -0500

    Update install_webmin.sh

--- install_webmin.sh
+++ install_webmin.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 clear
 
-
 echo "
-Version:  2.0.2
-Last Updated:  1/7/2024
+Version:  2.0.3
+Last Updated:  12/3/2025
+
+
+wget https://raw.githubusercontent.com/c2theg/srvBuilds/refs/heads/master/install_webmin.sh && chmod u+x install_webmin.sh
 
 
 This will install the latest version of Webmin
@@ -16,12 +18,16 @@ sudo -E apt-get upgrade -y
 wait
 echo "Downloading required dependencies...\r\n\r\n"
 #--------------------------------------------------------------------------------------------
+### -- https://webmin.com/download/#install
+# curl -o webmin-setup-repo.sh https://raw.githubusercontent.com/webmin/webmin/master/webmin-setup-repo.sh
+# sudo sh webmin-setup-repo.sh
+# sudo apt-get install webmin --install-recommends
+#
+#--------------------------------------------------------------------------------------------
 sudo apt-get -y install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions
 wait
-#wget "http://prdownloads.sourceforge.net/webadmin/webmin_${Version}_all.deb"
 wget https://www.webmin.com/download/deb/webmin-current.deb
 wait
-#sudo dpkg --install webmin_${Version}_all.deb
 sudo dpkg --install webmin-current.deb
 
 /etc/webmin/restart-by-force-kill
