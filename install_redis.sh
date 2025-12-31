4680ce4e1ee89ebf099523762b249bc903281ca8
Chris Gray <christophermjgray@gmail.com>
2025-12-31 10:43:46 -0500

    Update install_redis.sh

--- install_redis.sh
+++ install_redis.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-#    If you update this from Windows, using Notepad ++, do the following:
-#       sudo apt-get -y install dos2unix
-#       dos2unix <FILE>
-#       chmod u+x <FILE>
-#
 clear
 now=$(date)
 echo "Running install_redis.sh at $now 
@@ -20,44 +15,36 @@ echo "Running install_redis.sh at $now
 |_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
                             |_|                                             |___|
 
-\r\n \r\n
-Version:  0.1.8                             \r\n
-Last Updated:  12/19/2022
-\r\n \r\n"
-wait
-#sudo -E apt-get update
-#wait
-#sudo -E apt-get upgrade -y --force-yes
-wait
-#sudo -E apt-get install -f -y
-wait
-#echo "Freeing up space"
-#sudo apt-get autoremove -y
+
+Version:  0.1.10
+Last Updated:  12/30/2025
+
+
+DONT USE THIS ANYMORE! 
+
+A better way to do it is via a container!
+
+
+"
 wait
 echo "Downloading required dependencies...\r\n\r\n"
 #--------------------------------------------------------------------------------------------
 echo "This installs redis-server to your box... \r\n"
-#sudo add-apt-repository -y ppa:chris-lea/redis-server
-# Removing Chris-lea since he stopped working! - as of 8/1/2020 - BUM! https://launchpad.net/~chris-lea/+archive/ubuntu/redis-server
 sudo add-apt-repository -y ppa:redislabs/redis
-
 wait
 sudo -E apt-get update
 wait
 sudo -E apt-get install -y build-essential tcl
 wait
 sudo -E apt-get install -y redis-server ruby-redis redis-tools
-#pip install redis-trib
-wait
 clear
-echo "\r\n \r\n "
-echo "Fixing environment settings... \r\n \r\n"
-echo "fixing the: WARNING overcommit_memory is set to 0! Background save may fail under low memory condition. \r\n";
-#sudo sysctl vm.overcommit_memory=1
+echo "
+
+Fixing the: WARNING overcommit_memory is set to 0! Background save may fail under low memory condition. \r\n
+
+";
 sudo echo "vm.overcommit_memory = 1" >> /etc/sysctl.conf
 wait
-#echo "Fixing the: WARNING: The TCP backlog setting of 511 cannot be enforced because /proc/sys/net/core/somaxconn is set to the lower value of 128. \r\n"
-
 echo "Fixing WARNINGS / Issues with other platforms... \r\n \r\n"
 sudo echo "sysctl -w net.core.somaxconn=65535" >> /etc/rc.local
 sudo echo 65534 > /proc/sys/net/core/somaxconn
@@ -82,7 +69,9 @@ sudo echo 'stop on shutdown' >> /etc/init/redis-server.conf
 sudo echo 'exec sudo -u redis /usr/bin/redis-server /etc/redis/redis.conf' >> /etc/init/redis-server.conf
 sudo echo 'respawn' >> /etc/init/redis-server.conf
 echo "----------------------------------------------------------------------- \r\n"
-echo "Downloading latest custom config... \r\n "
+echo "Downloading latest custom config... 
+
+"
 wait
 rm redis_cluster.conf
 wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/configs/redis_cluster.conf
@@ -101,7 +90,9 @@ sudo adduser --system --group --no-create-home redis
 sudo mkdir /var/lib/redis
 sudo chown redis:redis /var/lib/redis && sudo chmod 770 /var/lib/redis
 
-echo "Making backups and copies.. \r\n \r\n"
+echo "Making backups and copies.. 
+
+"
 sudo mkdir /var/run/redis/
 sudo chown redis /var/run/redis/ && sudo chmod u+x /var/run/redis/
 
@@ -135,20 +126,25 @@ echo "To test, issue the following commands: "
 echo " redis-benchmark -q -n 1000 -c 10 -P 5 -p 46379 \r\n"
 echo " redis-cli -p 46379  \r\n"
 echo " Or auth with: \r\n "
-echo " redis-cli -p 46379 -a <password> \r\n \r\n"
-
-echo " /etc/init.d/redis-server stop  \r\n"
-echo " /etc/init.d/redis-server start  \r\n"
-echo "
-redis-cli info  \r\n 
-redis-cli info stats  \r\n
-redis-cli info server  \r\n
+echo " redis-cli -p 46379 -a <password>
 
 "
-echo "Setting up Redis to start on startup... \r\n "
-sudo systemctl enable redis-server.service
-echo " \r\n "
 
+echo " /etc/init.d/redis-server stop  
+
+"
+echo " /etc/init.d/redis-server start  
+
+"
+echo "
+redis-cli info
+redis-cli info stats
+redis-cli info server
+
+Setting up Redis to start on startup...
+
+"
+sudo systemctl enable redis-server.service
 #wget https://raw.githubusercontent.com/c2theg/srvBuilds/master/install_ruby.sh && chmod u+x install_ruby.sh && ./install_ruby.sh
 #gem install redis
 #wget http://download.redis.io/redis-stable/src/redis-trib.rb && chmod u+x redis-trib.rb && ./redis-trib.rb
@@ -156,25 +152,27 @@ echo "Done!"
 
 if [ -d "/etc/php" ]
 then
-    echo "installing PHP module... \r\n "
+    echo "Installing PHP module...
+    
+    "
     # sudo apt-get install -y php-redis
     sudo apt-get install -y php7.4-redis
     sudo bash -c "echo extension=redis.so > /etc/php/7.4/mods-available/redis.ini"
-    echo "Modify /etc/php/7.4/fpm/php.ini to configure redis servers... \r\n \r\n "
+    echo "Modify /etc/php/7.4/fpm/php.ini to configure redis servers...
+    
+    "
 fi
-
-echo "\r\n \r\n ---- Troubleshooting ---- \r\n \r\n 
+echo "
+---- Troubleshooting ---- 
 
 journalctl -xeu redis-server.service
-
 sudo systemctl restart redis-server
-
 sudo systemctl status redis-server
-
 ss -plnt4
 
-\r\n \r\n  Version: \r\n \r\n "
+Version:"
 
 redis-server --version
+echo "
 
-echo "\r\n \r\n"
+"
