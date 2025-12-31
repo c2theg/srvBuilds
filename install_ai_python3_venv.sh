f2e86724f8f67142361c62955fa9e22a999b0c97
Chris Gray <christophermjgray@gmail.com>
2025-12-31 12:24:01 -0500

    Update install_ai_python3_venv.sh

--- install_ai_python3_venv.sh
+++ install_ai_python3_venv.sh
@@ -32,7 +32,7 @@ Global Path:  $VENV_BASE/venv
 
 
 Install:
-    rm install_common_python3_venv.sh && wget https://raw.githubusercontent.com/c2theg/srvBuilds/refs/heads/master/install_common_python3_venv.sh && chmod u+x install_common_python3_venv.sh
+    rm install_ai_python3_venv.sh && wget https://raw.githubusercontent.com/c2theg/srvBuilds/refs/heads/master/install_ai_python3_venv.sh && chmod u+x install_ai_python3_venv.sh
 
 "
 
@@ -60,15 +60,85 @@ source "$VENV_DIR/bin/activate"
 # Upgrade core tooling
 pip install --upgrade pip setuptools wheel
 
-
 #--------------- Install shared packages ---------------
-pip install \
-    requests \
-    flask \
-    redis
+pip install requests flask redis
+pip3 install requests urllib3 beautifulsoup4 pymongo
+
+pip3 install ollama
+pip3 install pdfplumber
+pip3 install langchain langchain-core langchain-ollama langchain-community langchain_text_splitters
+pip3 install unstructured unstructured[all-docs]
+pip3 install fastembed
+pip3 install sentence-transformers
+pip3 install elevenlabs
+#--- Vector Databases ---
+# Milvus lite (10k - 100k vectors)
+#pip3 install milvus
+# Milvus Standalone (single machine (1M - 10M Vectors) / Milvus-cluster (10B vectors)
+pip3 install -U pymilvus
+#in memory vector database, single node
+pip3 install chromadb
+#------- Install Data Science libs -------
+pip3 install matplotlib
+pip3 install numpy
+pip3 install pandas
+pip3 install scipy
+python -m pip install "dask[complete]"
+
+#- visuals -
+pip3 install bokeh # https://bokeh.org/
+pip3 install seaborn # https://seaborn.pydata.org/installing.html
+pip3 install plotly # https://plotly.com/python/getting-started/
+
+# install NLP Libraries
+pip3 install textblob nltk spacy wordcloud
+
+# Machine Learning
+pip3 install scikit-learn joblib
+
+# Core email processing
+pip install html2text 
+
+# For Transformer models (GPU recommended)
+pip install torch transformers datasets
+
+# Download NLTK data
+python -m nltk.downloader punkt stopwords wordnet averaged_perceptron_tagger
+
+# Download spaCy model
+python -m spacy download en_core_web_sm
+
+#------- Install Machine Learning libs -------
+pip3 install torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip3 install tensorflow
+pip3 install scikit-learn
+pip3 install torchvision
+
+#pip3 install catboost
+# LightGBM -> https://lightgbm.readthedocs.io/en/stable/Installation-Guide.html
+pip3 install keras # Yolo3 requirement
+#pip3 install gym  # -> https://github.com/openai/gym
+#pip3 install xgboost # -> https://xgboost.readthedocs.io/en/stable/install.html
 
 
+#------- Generative AI -------
+pip3 install dalle2-pytorch # -> https://github.com/lucidrains/DALLE2-pytorch
+pip3 install pyro-ppl # -> https://pyro.ai/examples/intro_long.html
+#pip3 install glm_saga # -> https://pytorch.org/blog/empowering-models-performance/
+# pip install imageai --upgrade   |  ImageAI #-> https://imageai.readthedocs.io/en/latest/#:~:text=ImageAI%20is%20a%20python%20library,and%20few%20lines%20of%20code.
+# StyleGen #-> https://github.com/NVlabs/stylegan2
+#pip3 install flax # -> https://flax.readthedocs.io/en/latest/
+#pip3 install -U jax # -> Many Install options depending on hardware -> https://github.com/jax-ml/jax
+# NeRF -> https://github.com/bmild/nerf
 
+#------- Computer Vision / Real-Time Object Detection -------
+pip3 install opencv-python # https://docs.opencv.org/4.x/d6/d00/tutorial_py_root.html
+# Darknet # https://github.com/mdv3101/darknet-yolov3 |  https://pjreddie.com/darknet/
+# pip3 install YOLOv3 # -> https://pjreddie.com/darknet/yolo/ | https://viso.ai/deep-learning/yolov3-overview/
+# pip3 install yolo-v4 #-> https://github.com/philipperemy/python-darknet-yolo-v4
+
+#- https://huggingface.co/datasets?task_categories=task_categories%3Aimage-to-text
+# google/imageinwords  # https://huggingface.co/datasets/google/imageinwords
 
 #------------------- End of Shared ---------------------
 
