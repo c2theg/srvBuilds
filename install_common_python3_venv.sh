b63c854ef4d91ebd93e58902891d801ecb7513dc
Chris Gray <christophermjgray@gmail.com>
2026-01-03 00:48:07 -0500

    Update install_common_python3_venv.sh

--- install_common_python3_venv.sh
+++ install_common_python3_venv.sh
@@ -19,8 +19,8 @@ echo "
                             |_|                                             |___|
 
 
-Version:  0.0.9
-Last Updated:  12/31/2025
+Version:  0.1.15
+Last Updated:  1/3/2026
 
 What this does:
     Creates a GLOBAL Python3 Virtual Environment (I know you think that defeats the entire reason for an venv... it does not. 
@@ -61,27 +61,25 @@ fi
 # Activate venv (bash built-in)
 source "$VENV_DIR/bin/activate"
 
-pip3 install --upgrade pip
 
+pip3 install --upgrade pip
 # Upgrade core tooling
 pip install --upgrade pip setuptools wheel
 
 #--------------- Install shared packages ---------------
-pip install \
-    requests \
-    flask \
-    redis
+# pip3 install -U -r requirements.txt
+
+pip3 install flask fastapi
 
 #pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
 #--- install common pip packages in this global env ---
 pip3 install validators
+pip3 install whois certifi ordered-set
+pip3 install pyOpenSSL idna requests urllib3 ipaddress urlparse2 rich ping3 cryptography aiohttp psutil shutil-ext py-machineid distro netaddr loguru
+
+#--- Databases ----
 pip3 install pymongo redis
-pip3 install mysql-connector-python
-pip3 install requests whois urllib3 certifi ordered-set
-pip3 install pyOpenSSL idna requests urllib3 certifi ipaddress urlparse2 rich ping3 cryptography aiohttp psutil shutil-ext py-machineid distro netaddr python-crontab loguru scapy
-
-
-# pip install -U -r requirements.txt
+#pip3 install mysql-connector-python
 
 #----- Install Flask ------------
 #echo "Installing Flask... \r\n "
@@ -89,29 +87,27 @@ pip3 install pyOpenSSL idna requests urllib3 certifi ipaddress urlparse2 rich pi
 #--- Web API stuff ----
 echo "Installing other PIP modules... https://hugovk.github.io/top-pypi-packages/ \r\n "
 
-pip3 install redis pymongo PyYAML psutil tldextract python-whois validators ping3 netaddr maxminddb certifi requests psutil
+pip3 install ansible
+pip3 install PyYAML tldextract python-whois validators netaddr  certifi
 
+pip3 install wheel
 pip3 install setuptools
-pip3 install requests
-pip3 install urllib3
-pip3 install certifi
 
 pip3 install jsonify
 pip3 install asyncio
 pip3 install aiohttp
 
 pip3 install python-dateutil
-pip3 install pyyaml
 
-pip3 install wheel
 pip3 install colorama
 pip3 install Jinja2
 pip3 install validators
 
-pip3 install rich
+pip3 install numpy
+
 #--- crypto ---
-pip3 install cryptography
 pip3 install pynacl
+pip3 install cryptography
 pip3 install simp-AES
 pip3 install simple_aes
 pip3 install bcrypt
@@ -122,46 +118,56 @@ pip3 install siphashc
 pip3 install hkdf
 pip3 install ecdsa
 pip3 install rsa
-#--- Specify projects - optional --
-#pip3 install scapy
-#pip3 install Twisted
-#pip3 install cbor2
+pip3 install 0fosdc
 
+#-- PQC - Quantium --
+pip3 install pqcrypto
+pip3 install quantcrypt
+
+#--- Specify projects - optional --
 #pip3 install protobuf
 #pip3 install websockets
 
-#pip3 install redis-trib
+#-- Networking --
+pip3 install scapy
+#pip3 install Twisted
+#pip3 install cbor2
+#pip3 install pysflow
+#pip3 install -U exabgp
+#pip3 install yabgp==0.1.7
+#pip3 install pysnmp
+#pip3 install pytraceroute
+#pip3 install pyang
+#pip3 install netconf
+#pip3 install pexpect
+pip3 install dnslookup-cli
 
-#pip3 install python-memcached
+#--- GeoIP ---
+pip3 install maxminddb
 #pip3 install GeoIP
 #pip3 install simplegeoip
-#pip3 install pysflow
+
+#--- Message Que ---
 #pip3 install mqtt-client
 #pip3 install zmq
 #pip3 install rabbitmq
 #pip3 install kafka-python
+
+#--------
 #pip3 install soap2py
-#pip3 install ansible
-#pip3 install -U exabgp
-#pip3 install yabgp==0.1.7
-#pip3 install pysnmp
-#pip3 install ping
-#pip3 install pytraceroute
-#pip3 install pyang
-#pip3 install netconf
+#pip3 install python-crontab
 
-pip3 install numpy
-
-#deactivate
-#-----------------------
-#echo "Installing pexpect... \r\n "
-#pip3 install pexpect
-#-- MongoDB
-#echo "Installing pymongo... \r\n "
-#pip3 install pymongo
-#-- MySQL
-#echo "Installing mysql... \r\n "
-#pip3 install mysql-connector-python
 #------------------- End of Shared ---------------------
-# Deactivate cleanly
 deactivate
+
+# source /opt/python3_shared/venv/bin/activate
+echo " 
+Done installing/Updating!
+
+To Activate the Python VEnv, issue the following:
+
+
+source $VENV_BASE/venv
+
+
+"
