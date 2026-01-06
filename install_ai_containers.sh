ba5587fea11bc23f3f139cc1935dd3a0607df256
Chris Gray <christophermjgray@gmail.com>
2026-01-05 22:12:11 -0500

    Update install_ai_containers.sh

--- install_ai_containers.sh
+++ install_ai_containers.sh
@@ -1,5 +1,10 @@
 
 
-docker run -it --rm -p 8888:8888 jupyter/datascience-notebook
+docker run -it --rm -p 8888:8888 jupyter/datascience-notebook:latest
 
-docker run -it --rm -p 5678:5678 n8nio/n8n
+docker run -it --rm -p 5678:5678 n8nio/n8n:latest
+
+docker run -it --rm ollama/ollama:latest
+
+#--- vector databases ----
+docker run -it --rm -p 6333:6333 qdrant/qdrant
