ed7032b94042d694e1dfc6b0f414e54b7e793b39
Chris Gray <christophermjgray@gmail.com>
2026-05-04 23:15:57 -0400

    Update version and add fix_permissions script
    Updated version number to 0.1.2 and added download for fix_permissions.sh.

--- install_resilio_ubuntu_26.04.sh
+++ install_resilio_ubuntu_26.04.sh
@@ -15,7 +15,7 @@ echo "
 
 
 
-Version:  0.1.0
+Version:  0.1.2
 Last Updated:  5/4/2026
 This is really meant for 26.04+
 
@@ -28,10 +28,59 @@ wait
 sudo -E apt-get upgrade -y
 wait
 echo "Downloading required dependencies...\r\n\r\n"
+#---------------------------------------------------------------------------------------------------------
+#if [ -s "/etc/resilio-sync/config.json" ]
+#then
+#     echo "Deleting file Resilio config "
+#     rm /etc/resilio-sync/config.json
+#     rm resilio_config.json
+#     rm resilio-sync.service
+#fi
+echo "Downloading Resilio Config"
+wget -O "resilio_config.json" "https://raw.githubusercontent.com/c2theg/srvBuilds/master/configs/resilio_config.json"
+#wget -O "resilio-sync.service" "https://raw.githubusercontent.com/c2theg/srvBuilds/master/configs/resilio-sync.service"
+wget -O "fix_permissions.sh" "https://raw.githubusercontent.com/c2theg/srvBuilds/master/fix_permissions.sh"
+wait
+echo "Resilio Config Download Complete"
+
+sudo mv "resilio_config.json" "/etc/resilio-sync/config.json"
 #--------------------------------------------------------------------------------------------
+
 echo "Installing ... \r\n \r\n"
 
 sudo rm -f /etc/apt/trusted.gpg.d/resilio-sync.asc
 wget -qO- https://linux-packages.resilio.com/resilio-sync/key.asc | sudo tee /etc/apt/trusted.gpg.d/resilio-sync.asc >/dev/null
 sudo apt update
 sudo apt install -y resilio-sync
+
+#-------------------------------------------------
+sudo systemctl enable --now resilio-sync
+sudo systemctl status resilio-sync --no-pager
+
+sudo ss -ltnp | grep -E '8888|rslsync'
+
+sudo systemctl restart resilio-sync
+sudo journalctl -u resilio-sync -n 80 --no-pager
+sudo ss -ltnp | grep 8888
+
+
+sudo ufw allow 8888/tcp
+
+
+#-----------------------
+#sudo chmod -R 755 /var/www/
+#sudo chown -R www-data:www-data /var/www/
+#sudo chown -R rslsync:rslsync /var/www/
+
+mkdir -p /media/data/sync
+sudo chmod -R 755 /media/data/sync/ && sudo chown -R rslsync:rslsync /media/data/sync/
+
+sudo mv "fix_permissions.sh" "/media/data/sync/fix_permissions.sh"
+chmod u+x /media/data/sync/fix_permissions.sh
+
+cd /home/rslsync
+wget https://raw.githubusercontent.com/c2theg/srvBuilds/refs/heads/master/configs/btsync.btskey
+sudo chown -R rslsync:rslsync btsync.btskey
+
+echo "DONE. Now visit the server in your webbrowser at https://<SERVERIP>:8888"
+
