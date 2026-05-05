daa7a44ae0f7bc5232584a2818ffb59638de4f69
Chris Gray <christophermjgray@gmail.com>
2026-05-04 22:36:36 -0400

    Create install script for Resilio Sync on Ubuntu 26.04
    Added a script for installing Resilio Sync on Ubuntu 26.04 with system update and dependency installation.

--- install_resilio_ubuntu_26.04.sh
+++ install_resilio_ubuntu_26.04.sh
@@ -1,3 +1,36 @@
+#!/bin/sh
+clear
+echo "
+ _____             _         _    _          _                                   
+|     |___ ___ ___| |_ ___ _| |  | |_ _ _   |_|                                  
+|   --|  _| -_| .'|  _| -_| . |  | . | | |   _                                   
+|_____|_| |___|__,|_| |___|___|  |___|_  |  |_|                                  
+                                     |___|                                       
+                                                                                 
+ _____ _       _     _           _              _____    __    _____             
+|     | |_ ___|_|___| |_ ___ ___| |_ ___ ___   |     |__|  |  |   __|___ ___ _ _ 
+|   --|   |  _| |_ -|  _| . | . |   | -_|  _|  | | | |  |  |  |  |  |  _| .'| | |
+|_____|_|_|_| |_|___|_| |___|  _|_|_|___|_|    |_|_|_|_____|  |_____|_| |__,|_  |
+                            |_|                                             |___|
+
+
+
+Version:  0.1.0
+Last Updated:  5/4/2026
+This is really meant for 26.04+
+
+wget https://raw.githubusercontent.com/c2theg/srvBuilds/refs/heads/master/install_resilio_ubuntu_26.04.sh && chmod u+x install_resilio_ubuntu_26.04.sh
+
+Updating system first..."
+
+sudo -E apt-get update
+wait
+sudo -E apt-get upgrade -y
+wait
+echo "Downloading required dependencies...\r\n\r\n"
+#--------------------------------------------------------------------------------------------
+echo "Installing ... \r\n \r\n"
+
 echo "deb http://linux-packages.resilio.com/resilio-sync/deb resilio-sync non-free" | sudo tee /etc/apt/sources.list.d/resilio-sync.list
 curl -LO https://linux-packages.resilio.com/resilio-sync/key.asc
 sudo apt-key add key.asc
